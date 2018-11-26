d01158600ab4e8c43298014c3a67c870df35f338
KallynGowdyYetiCGI <kallyn.gowdy@yeticgi.com>
2018-11-26 13:07:52 -0500

    Work on adding a menu to the toolbar

--- WebClient/App/App.ts
+++ WebClient/App/App.ts
@@ -1,9 +1,18 @@
 import Vue, { ComponentOptions } from 'vue';
+import Component from 'vue-class-component';
 
-const app: ComponentOptions<Vue> = {
-    data() {
-        return {message: 'Hello, App!'};
+@Component({
+    components: {
+        'app': App
     }
-};
+})
 
-export default app;
+export default class App extends Vue {
+    showNavigation: boolean = false;
+    message: string = 'Hello, App!';
+
+    menuClicked() {
+        this.showNavigation = !this.showNavigation;
+        console.log("[App] Menu Clicked showNavigation === " + this.showNavigation);
+    }
+}
