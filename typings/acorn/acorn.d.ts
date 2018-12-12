f0b7acdd643b71453835ecd57d7beaa7bc020c7a
KallynGowdyYetiCGI <kallyn.gowdy@yeticgi.com>
2018-12-12 17:01:17 -0500

    Add Acorn
    - Its a JavaScript parser

---
+++ typings/acorn/acorn.d.ts
@@ -0,0 +1,19 @@
+import {Parser, TokenType, Node} from 'acorn';
+declare module 'acorn' {
+    interface Parser {
+        type: TokenType;
+        start: number;
+        startLoc: number;
+        value: string;
+        pos: number;
+        next(): void;
+        parseLiteral(value: string): Node;
+        parseIdent(): Node;
+        unexpected(): void;
+        startNodeAt(start: number, startLoc: number): Node;
+        readToken(code: number): any;
+        finishToken(token: TokenType): any;
+        finishNode(node: Node, type: string): Node;
+        parseExprAtom(refShortHandDefaultPos: any): Node;
+    }
+}
