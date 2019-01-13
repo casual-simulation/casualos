506daa1e60447433291fb29f6c947032f5b5bb52
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-01-12 22:07:22 -0500

    Add the most basic page possible for resolving merge conflicts

--- WebClient/MergeConflicts/MergeConflicts.ts
+++ WebClient/MergeConflicts/MergeConflicts.ts
@@ -2,9 +2,21 @@ import Vue, { ComponentOptions } from 'vue';
 import Component from 'vue-class-component';
 import {Prop, Inject} from 'vue-property-decorator';
 import { appManager } from '../AppManager';
+import { ConflictDetails, ResolvedConflict, first, second } from 'common/Files';
+import { groupBy, keys } from 'lodash';
+
+interface FileConflicts {
+    id: string;
+    conflicts: ConflictDetails[];
+}
 
 @Component({})
 export default class MergeConflicts extends Vue {
+
+    files: FileConflicts[] = [];
+    resolved: ResolvedConflict[] = [];
+    first = first;
+    second = second;
 
     get fileManager() {
         return appManager.fileManager;
@@ -12,5 +24,41 @@ export default class MergeConflicts extends Vue {
 
     constructor() {
         super();
+        this.files = [];
+        this.resolved = [];
+    }
+
+    created() {
+        const conflicts = appManager.fileManager.mergeStatus.remainingConflicts.slice();
+        const grouped = groupBy(conflicts, 'path[0]');
+        const ids = keys(grouped);
+
+        this.files = ids.map(id => ({
+            id: id,
+            conflicts: conflicts
+        }));
+    }
+
+    takeValue(file: FileConflicts, conflict: ConflictDetails, value: any) {
+        this.resolved.push({
+            value: value,
+            details: conflict
+        });
+
+        let index = file.conflicts.indexOf(conflict);
+        if (index >= 0) {
+            file.conflicts.splice(index, 1);
+            if (file.conflicts.length <= 0) {
+                index = this.files.indexOf(file);
+                if (index >= 0) {
+                    this.files.splice(0, 1);
+                }
+            }
+        }
+    }
+
+    finish() {
+        appManager.fileManager.resolveConflicts(this.resolved);
+        this.$router.push('/home');
     }
 };
--- WebClient/MergeConflicts/MergeConflicts.vue
+++ WebClient/MergeConflicts/MergeConflicts.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
         <h1>Fixing conflicts one at time!</h1>
+        <div v-for="file in files" :key="file.id">
+            <h2>{{file.id}}</h2>
+            <div v-for="(conflict, index) in file.conflicts" :key="index">
+                <h3>{{conflict.path.join('.')}}</h3>
+                <ul>
+                    <li><md-button @click="takeValue(file, conflict, conflict.conflict[first])">{{conflict.conflict[first]}}</md-button></li>
+                    <li><md-button @click="takeValue(file, conflict, conflict.conflict[second])">{{conflict.conflict[second]}}</md-button></li>
+                </ul>
+            </div>
+        </div>
+        <div v-if="files.length <= 0">
+            <md-button @click="finish()">Finish</md-button>
+        </div>
     </div>
 </template>
 <script src="./MergeConflicts.ts"></script>
