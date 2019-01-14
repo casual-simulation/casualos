721825328b66ff1d00da8cdb1fb4ad6bb347a53c
KallynGowdyYetiCGI <kallyn.gowdy@yeticgi.com>
2019-01-14 17:55:55 -0500

    Change the MergeConflicts page to use the FileTable

--- WebClient/MergeConflicts/MergeConflicts.ts
+++ WebClient/MergeConflicts/MergeConflicts.ts
@@ -2,15 +2,24 @@ import Vue, { ComponentOptions } from 'vue';
 import Component from 'vue-class-component';
 import {Prop, Inject} from 'vue-property-decorator';
 import { appManager } from '../AppManager';
-import { ConflictDetails, ResolvedConflict, first, second } from 'common/Files';
-import { groupBy, keys } from 'lodash';
+import { ConflictDetails, ResolvedConflict, first, second, File } from 'common/Files';
+import { groupBy, keys, uniq } from 'lodash';
+import FileTable from '../FileTable/FileTable';
+import { fileTags } from 'common/Files/FileCalculations';
 
 interface FileConflicts {
     id: string;
+    first: File,
+    second: File;
+    tags: string[];
     conflicts: ConflictDetails[];
 }
 
-@Component({})
+@Component({
+    components: {
+        'file-table': FileTable
+    }
+})
 export default class MergeConflicts extends Vue {
 
     files: FileConflicts[] = [];
@@ -28,15 +37,32 @@ export default class MergeConflicts extends Vue {
         this.resolved = [];
     }
 
+    conflictName(conflict: ConflictDetails) {
+        const index = conflict.path.indexOf('tags');
+        if (index >= 0) {
+            const tagName = conflict.path.slice(index);
+            return tagName.join('.');
+        } else {
+            return conflict.path.join('.');
+        }
+    }
+
     created() {
         const conflicts = appManager.fileManager.mergeStatus.remainingConflicts.slice();
         const grouped = groupBy(conflicts, 'path[0]');
         const ids = keys(grouped);
 
-        this.files = ids.map(id => ({
-            id: id,
-            conflicts: conflicts
-        }));
+        this.files = ids.map(id => {
+            const first = appManager.fileManager.mergeStatus.merge.first[id];
+            const second = appManager.fileManager.mergeStatus.merge.second[id];
+            return {
+                id: id,
+                first: first,
+                second: second,
+                tags: fileTags([first,second], this._conflictingTags(grouped[id]), []),
+                conflicts: grouped[id].map(g => g)
+            };
+        });
     }
 
     takeValue(file: FileConflicts, conflict: ConflictDetails, value: any) {
@@ -60,5 +86,9 @@ export default class MergeConflicts extends Vue {
     finish() {
         appManager.fileManager.resolveConflicts(this.resolved);
         this.$router.push('/home');
+    }
+
+    private _conflictingTags(conflicts: ConflictDetails[]) {
+        return uniq(conflicts.map(c => c.path[2]));
     }
 };
--- WebClient/MergeConflicts/MergeConflicts.vue
+++ WebClient/MergeConflicts/MergeConflicts.vue
@@ -3,8 +3,9 @@
         <h1>Fixing conflicts one at time!</h1>
         <div v-for="file in files" :key="file.id">
             <h2>{{file.id}}</h2>
+            <file-table :files="[file.first, file.second]" :extraTags="file.tags" :readOnly="true"/>
             <div v-for="(conflict, index) in file.conflicts" :key="index">
-                <h3>{{conflict.path.join('.')}}</h3>
+                <h3>{{conflictName(conflict)}}</h3>
                 <ul>
                     <li><md-button @click="takeValue(file, conflict, conflict.conflict[first])">{{conflict.conflict[first]}}</md-button></li>
                     <li><md-button @click="takeValue(file, conflict, conflict.conflict[second])">{{conflict.conflict[second]}}</md-button></li>
