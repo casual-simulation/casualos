c94a7d22f268d81ee68662e4f7c78c9ee83c3b1d
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-02-01 14:46:34 -0500

    Disable auto-capitalization and autocorrect on the input fields

--- WebClient/FileValue/FileValue.vue
+++ WebClient/FileValue/FileValue.vue
@@ -6,7 +6,9 @@
         :value="value" 
         class="value-input" 
         :disabled="readOnly"
-        :class="[{ focused: isFocused, formula: isFormula }, tag]">
+        :class="[{ focused: isFocused, formula: isFormula }, tag]"
+        autocapitalize="none"
+        autocorrect="off">
 </template>
 <script src="./FileValue.ts"></script>
 <style src="./FileValue.css"></style>
