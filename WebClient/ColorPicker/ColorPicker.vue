bef0d40fd7cf7f67b1dcbefe56bbc36937688398
Ryan Cook <ryan.cook@yeticgi.com>
2019-02-07 00:56:23 -0500

    Disabled alpha slider on color picker.

--- WebClient/ColorPicker/ColorPicker.vue
+++ WebClient/ColorPicker/ColorPicker.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="color-picker" :style="colorPickerStyle">
-        <chrome-color-picker v-show="colorPickerVisible" v-model="colors" @input="onColorPickerInput"/>
+        <chrome-color-picker v-show="colorPickerVisible" v-model="colors" @input="onColorPickerInput" :disableAlpha="true"/>
     </div>
 </template>
 <script src="./ColorPicker.ts"></script>
