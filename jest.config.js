94fa8d233f537a54c3ff9ef6d8173d0ac63bac8e
Ryan Cook <ryan.cook@yeticgi.com>
2019-02-25 17:08:17 -0500

    Doing a clean before running tests.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    'formula\\-lib\\.ts$': '<rootDir>/__mocks__/formulaLibMock.js'
-  },
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
-    'formula\\-lib$': '<rootDir>/src/aux-common/Formulas/formula-lib.ts',
     '^aux-common/(.*)$': '<rootDir>/src/aux-common/$1',
     '^three\\-examples$': '<rootDir>/node_modules/three/examples/js',
   }
