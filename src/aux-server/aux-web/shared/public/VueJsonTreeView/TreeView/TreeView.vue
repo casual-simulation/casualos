2184bb290815f541038f9e22a211623989fa095b
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-03-13 00:26:14 -0400

    Add an interactive tree for the file state
    - This makes it easier to look at

---
+++ src/aux-server/aux-web/shared/public/VueJsonTreeView/TreeView/TreeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tree-view-wrapper">
+    <tree-view-item
+      class="tree-view-item-root"
+      :data="parsedData"
+      :max-depth="allOptions.maxDepth"
+      :current-depth="0"
+      :modifiable="allOptions.modifiable"
+      :link="allOptions.link"
+      :limit-render-depth="allOptions.limitRenderDepth"
+      @change-data="onChangeData"
+    ></tree-view-item>
+  </div>
+</template>
+
+<script src="./TreeView.ts"></script>
+
+<style scoped>
+
+/* Find the first nested node and override the indentation */
+.tree-view-item-root > .tree-view-item-leaf > .tree-view-item {
+  margin-left: 0 !important;
+}
+
+/* Root node should not be indented */
+.tree-view-item-root {
+  margin-left: 0 !important;
+}
+</style>
---
+++ src/aux-server/aux-web/shared/public/VueJsonTreeView/index.ts
@@ -0,0 +1,9 @@
+import TreeViewItemValue from './TreeViewItemValue/TreeViewItemValue';
+import TreeViewItem from './TreeViewItem/TreeViewItem';
+import TreeView from './TreeView/TreeView';
+
+export {
+    TreeView,
+    TreeViewItem,
+    TreeViewItemValue
+};
