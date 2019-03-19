91211d9e31bf00dbbb3d7fd8a7677888102f766c
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-03-18 21:51:48 -0400

    Add the ability to proxy files so that they're easier to work with

---
+++ src/aux-common/Files/FileProxy.spec.ts
@@ -0,0 +1,156 @@
+import { createFile, createCalculationContext } from "./FileCalculations";
+import { createFileProxy, isProxy, proxyObject } from './FileProxy';
+
+describe('FileProxy', () => {
+    describe('createFileProxy()', () => {
+        it('should return a proxy object', () => {
+            const file = createFile('testId');
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy[isProxy]).toBe(true);
+            expect(proxy).not.toBe(file);
+        });
+
+        it('should be able to return the original file', () => {
+            const file = createFile('testId');
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            
+            expect(proxy[proxyObject]).toBe(file);
+        });
+
+        it('should support Symbol.toPrimitive', () => {
+            const file = createFile('testId');
+            file.tags.num = 100;
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            const pVal = proxy.num;
+            expect(pVal[isProxy]).toBe(true);
+            expect(pVal[Symbol.toPrimitive]('default')).toBe(100);
+            
+            const val = pVal[proxyObject];
+            expect(val instanceof Number).toBe(true);
+            expect(val).not.toBe(100);
+            expect(val == 100).toBe(true);
+        });
+
+        it('should support valueOf() on primitives', () => {
+            const file = createFile('testId');
+            file.tags.str = 'hello, world';
+            file.tags.num = 100;
+            file.tags.bool = true;
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.str.valueOf()).toBe('hello, world');
+            expect(proxy.num.valueOf()).toBe(100);
+            expect(proxy.bool.valueOf()).toBe(true);
+        });
+
+        it('should support toString() on primitives', () => {
+            const file = createFile('testId');
+            file.tags.str = 'hello, world';
+            file.tags.num = 100;
+            file.tags.bool = true;
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.str.toString()).toBe('hello, world');
+            expect(proxy.num.toString()).toBe('100');
+            expect(proxy.bool.toString()).toBe('true');
+        });
+
+        it('should be able to add numbers', () => {
+            const file = createFile('testId');
+            file.tags.num = 100;
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.num + 5).toBe(105);
+        });
+
+        it('should be able to concatenate strings', () => {
+            const file = createFile('testId');
+            file.tags.str = 'hello';
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.str + ', world').toBe('hello, world');
+        });
+
+        it('should be able to get simple tag values', () => {
+            const file = createFile('testId');
+            file.tags.str = 'test';
+            file.tags.num = 100;
+            file.tags.bool = true;
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.str == 'test').toBe(true);
+            expect(proxy.num == 100).toBe(true);
+            expect(proxy.bool == true).toBe(true);
+        });
+
+        it('should be able to get nested tag values', () => {
+            const file = createFile('testId');
+            file.tags['aux.builder.context'] = 'test';
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.aux.builder.context == 'test').toBe(true);
+        });
+
+        it('should be able to handle nested objects', () => {
+            const file = createFile('testId');
+            file.tags['my.obj'] = {
+                test: 'hahaha'
+            };
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.my.obj.test == 'hahaha').toBe(true);
+        });
+
+        it('should be able to handle multiple layers of nested objects', () => {
+            const file = createFile('testId');
+            file.tags['my.obj'] = {
+                'test.deep': {
+                    final: 'cool'
+                }
+            };
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.my.obj.test.deep.final == 'cool').toBe(true);
+        });
+
+        it('should be able to return a falsy value', () => {
+            const file = createFile('testId');
+
+            const context = createCalculationContext([file]);
+            const proxy = createFileProxy(context, file);
+
+            expect(proxy.some.crazy.tag == false).toBe(true);
+            
+            // Type is a 
+            expect(!proxy.some.crazy.tag).toBe(false);
+            
+            const val = proxy.some.crazy.tag;
+            expect(typeof val === 'object').toBe(true);
+            expect(val instanceof String).toBe(true);
+            expect(val.valueOf()).toBe('');
+        });
+    });
+});
---
+++ src/aux-common/Files/FileProxy.ts
@@ -0,0 +1,92 @@
+import { File, FileTags } from './File';
+import { FileCalculationContext } from './FileCalculations';
+
+/**
+ * The symbol that can be used to tell if an object represents a proxy.
+ */
+export const isProxy = Symbol('isProxy');
+
+/**
+ * The symbol that can be used to get the object that a proxy represents.
+ */
+export const proxyObject = Symbol('proxyObject');
+
+/**
+ * Defines an interface for a file that is being proxied so that
+ * formulas are transparently calculated and deep values can be handled transparently.
+ */
+export interface FileProxy extends FileTags {
+    [isProxy]: boolean;
+    [proxyObject]: File;
+    id: File['id'];
+}
+
+/**
+ * Creates a new file proxy from the given file and calculation context.
+ * @param calc The calculation context to use.
+ * @param file The file.
+ */
+export function createFileProxy(calc: FileCalculationContext, file: File): FileProxy {
+    return <FileProxy>new Proxy(file, _createProxyHandler(calc, file.tags));
+}
+
+
+function _createProxyHandler(calc: FileCalculationContext, tags: any, props?: string): ProxyHandler<any> {
+    return {
+        get: function (target, property) {
+            let nextTags = tags;
+            if (typeof property === 'symbol') {
+                if (property === isProxy) {
+                    return true;
+                } else if (property === proxyObject) {
+                    return target;
+                } else if (property === Symbol.toPrimitive) {
+                    return function (hint: string) {
+                        if (target instanceof Number) {
+                            return target.valueOf();
+                        } else if (target instanceof Boolean) {
+                            return target.valueOf();
+                        } else if (target instanceof String) {
+                            return target.valueOf();
+                        }
+                    };
+                }
+                
+                return target[property];
+            }
+
+            let fullProp: string = null;
+            let val = target[property];
+            if (typeof val === 'undefined') {
+                fullProp = props ? `${props}.${property}` : property.toString();
+                val = target[fullProp];
+                if (typeof val === 'undefined') {
+                    val = tags[fullProp];
+                }
+            }
+
+            if (val) {
+                fullProp = null;
+                nextTags = val;
+            }
+
+            if (typeof val === 'boolean') {
+                return new Proxy(new Boolean(val), _createProxyHandler(calc, nextTags, fullProp));
+            } else if (typeof val === 'number') {
+                return new Proxy(new Number(val), _createProxyHandler(calc, nextTags, fullProp));
+            } else if (typeof val === 'string') {
+                return new Proxy(new String(val), _createProxyHandler(calc, nextTags, fullProp));
+            }
+
+            return new Proxy(val || new String(''), _createProxyHandler(calc, nextTags, fullProp));
+        },
+
+        apply: function(target: Function, thisArg, args) {
+            if (thisArg[isProxy]) {
+                thisArg = thisArg[proxyObject];
+            }
+
+            return target.apply(thisArg, args);
+        }
+    };
+}
