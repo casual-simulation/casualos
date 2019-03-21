4e8c02d80c35ac515d732f0f21ebe21502f74ef0
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-03-20 22:13:33 -0400

    Make AUX Projector load on all the local IP Addresses in development

--- src/aux-server/server/projector.config.ts
+++ src/aux-server/server/projector.config.ts
@@ -1,17 +1,28 @@
 import * as path from 'path';
+import * as os from 'os';
 import { ClientConfig } from "./config";
+import { getLocalIpAddresses } from './utils';
+
+let domains = [
+    'filesimulator.com',
+    'projector.filesimulator.com',
+    'projector.*.filesimulator.com',
+    'projector.localhost',
+    'auxbuilder.com',
+    'localhost'
+];
+
+const env = process.env.NODE_ENV;
+if (env === 'production') {
+} else {
+    const ipAddresses = getLocalIpAddresses();
+    domains.push(...ipAddresses);
+}
 
 const config: ClientConfig = {
     dist: path.resolve(__dirname, '..', '..', 'aux-web', 'dist'),
     index: 'projector-index.html',
-    domains: [
-        'filesimulator.com',
-        'projector.filesimulator.com',
-        'projector.*.filesimulator.com',
-        'projector.localhost',
-        'auxbuilder.com',
-        'localhost'
-    ]
+    domains: domains
 };
 
 export default config;
--- src/aux-server/server/utils.ts
+++ src/aux-server/server/utils.ts
@@ -1,5 +1,7 @@
 import {Request, Response, Handler} from 'express';
 import { AxiosError } from 'axios';
+import * as os from 'os';
+import { flatMap } from 'lodash';
 
 export const asyncMiddleware: (fn: Handler) => Handler = (fn: Handler) => {
     return (req, res, next) => {
@@ -13,4 +15,17 @@ export const asyncMiddleware: (fn: Handler) => Handler = (fn: Handler) => {
                 next(er);
             });
     };
+}
+
+/**
+ * Gets the list of IP Addresses that are assigned to this machine.
+ * Excludes internal IP Addresses. (e.g. 127.0.0.1)
+ */
+export function getLocalIpAddresses() {
+    const ifaces = os.networkInterfaces();
+    return flatMap(Object.keys(ifaces), ifname => {
+        return ifaces[ifname]
+            .filter(iface => !iface.internal)
+            .map(iface => iface.address);
+    });
 }
