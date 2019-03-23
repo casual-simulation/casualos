e56a354caef5e7ba136a0e88325f84a5b451d520
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-03-23 12:53:39 -0400

    feat: Add a progress spinner to the Welcome pages
    - Also make the welcome pages use scoped CSS

--- src/aux-server/aux-web/aux-projector/Welcome/Welcome.vue
+++ src/aux-server/aux-web/aux-projector/Welcome/Welcome.vue
@@ -8,20 +8,26 @@
         </md-card-header>
 
         <md-card-content>
-          <div>
-            <md-field>
-              <label for="name">Name</label>
-              <md-input name="name" id="name" autocomplete="name" v-model="email"/>
-            </md-field>
-          </div>
-          <div class="continue-as-guest-section">
-            <span>Don't want an account?</span>
-            <a class="md-primary guest-button" @click="continueAsGuest">Continue as a Guest</a>
-          </div>
+            <div v-if="!showProgress">
+                <div>
+                    <md-field>
+                        <label for="name">Name</label>
+                        <md-input name="name" id="name" autocomplete="name" v-model="email"/>
+                    </md-field>
+                </div>
+                <div class="continue-as-guest-section">
+                    <span>Don't want an account?</span>
+                    <a class="md-primary guest-button" @click="continueAsGuest">Continue as a Guest</a>
+                </div>
+            </div>
+            <div v-else class="progress-section">
+                <p>Logging in...</p>
+                <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
+            </div>
         </md-card-content>
 
         <md-card-actions>
-          <md-button type="submit" class="md-primary md-raised">Next</md-button>
+          <md-button v-if="!showProgress" type="submit" class="md-primary md-raised">Next</md-button>
         </md-card-actions>
       </md-card>
     </form>
@@ -54,4 +60,4 @@
   </div>
 </template>
 <script src="./Welcome.ts"></script>
-<style src="./Welcome.css"></style>
+<style src="./Welcome.css" scoped></style>
