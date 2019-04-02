985b1d61978036ce8822e430716b6564d21a9bc3
Ryan Cook <ryan.cook@yeticgi.com>
2019-04-02 16:34:42 -0400

    improvement: Inventory bar adjusts to mobile screen resolutions.

--- src/aux-server/aux-web/aux-player/GameView/GameView.vue
+++ src/aux-server/aux-web/aux-player/GameView/GameView.vue
@@ -5,18 +5,17 @@
     <slot></slot>
     <div class="ui-container">
         <div class="toolbar">
-          <span v-show="vrDisplay" id="vr-button-container" class="vr-button-container"></span>
-
-          <div ref="inventory">
-            <div v-if="!vrDisplay && inventoryContext" class="toolbar-layout">
-              <inventory-file v-for="(file, index) in inventoryContext.slots" :key="index" 
-                :file="file"
-                :slotIndex="index"
-                :context="inventoryContext.context"
-                :selected="file && inventoryContext.selectedFile === file"
-                @click="inventoryContext.selectFile(file)"></inventory-file>
+            <div ref="inventory">
+                <div v-if="!vrDisplay && inventoryContext" class="inventory-layout">
+                <inventory-file v-for="(file, index) in inventoryContext.slots" :key="index" 
+                    :file="file"
+                    :slotIndex="index"
+                    :context="inventoryContext.context"
+                    :selected="file && inventoryContext.selectedFile === file"
+                    @click="inventoryContext.selectFile(file)"></inventory-file>
+                </div>
             </div>
-          </div>
+            <span v-show="vrDisplay" id="vr-button-container" class="vr-button-container"></span>
         </div>
     </div>
   </div>
