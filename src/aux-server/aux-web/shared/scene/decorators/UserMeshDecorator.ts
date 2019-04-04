69d8d058b49322d85489a130236d374c611c3bd7
Ryan Cook <ryan.cook@yeticgi.com>
2019-04-04 12:57:33 -0400

    improvement: user mesh is now a semi-transparent square cone mesh.

--- src/aux-server/aux-web/shared/scene/decorators/UserMeshDecorator.ts
+++ src/aux-server/aux-web/shared/scene/decorators/UserMeshDecorator.ts
@@ -1,13 +1,12 @@
 import {
-    Object3D,
     Vector3,
-    Camera,
-    PerspectiveCamera,
-    CameraHelper,
+    Group,
+    Mesh,
+    Math as ThreeMath
 } from "three";
 import { Text3D } from "../Text3D";
 import { FileCalculationContext, AuxObject } from '@yeti-cgi/aux-common'
-import { setLayer, disposeMaterial } from "../SceneUtils";
+import { setLayer, disposeMesh, createUserCone } from "../SceneUtils";
 import { LayersHelper } from "../LayersHelper";
 import { AuxFile3DDecorator } from "../AuxFile3DDecorator";
 import { AuxFile3D } from "../AuxFile3D";
@@ -30,22 +29,17 @@ export class UserMeshDecorator extends AuxFile3DDecorator {
     file3D: AuxFile3D;
 
     /**
-     * The object that acts as the visual representation of the file.
+     * The mesh that acts as the visual representation of the user.
      */
-    cameraHelper: CameraHelper;
-
-    /**
-     * The camera that this mesh is displaying.
-     */
-    camera: Camera;
+    userMesh: Mesh;
 
     /**
      * The container for the meshes.
      */
-    container: Object3D;
+    container: Group;
 
     /**
-     * The label for the file.
+     * The label for the user.
      */
     label: Text3D;
 
@@ -53,30 +47,29 @@ export class UserMeshDecorator extends AuxFile3DDecorator {
     constructor(file3D: AuxFile3D) {
         super(file3D);
 
-        this.container = new Object3D();
-        this.camera = new PerspectiveCamera(60, 1, 0.1, 0.5);
-        this.cameraHelper = new CameraHelper(this.camera);
-        this.cameraHelper.updateMatrixWorld(true);
+        // Container
+        this.container = new Group();
+        this.file3D.display.add(this.container);
 
-        // Setup label
+        // Label
         this.label = new Text3D();
         this.label.setText(this.file3D.file.tags._user);
         setLayer(this.label, LayersHelper.Layer_UIWorld);
         this.label.setScale(Text3D.defaultScale * 2);
         this.label.setWorldPosition(new Vector3(0,0,0));
         this.label.setRotation(0, 180, 0);
-        this.label.position.add(new Vector3(1.55, 0.25, 0)); // This is hardcoded. To lazy to figure out that math.
-        
-        this.cameraHelper.add(this.label);
-        
-        this.container.add(this.cameraHelper);
-        this.file3D.display.add(this.camera);
-        this.file3D.add(this.container);
+        this.container.add(this.label);
+        this.label.position.add(new Vector3(1.55, 0.7, 0)); // This is hardcoded. To lazy to figure out that math.
+
+        // User Mesh
+        this.userMesh = createUserCone();
+        this.container.add(this.userMesh);
+        this.userMesh.rotation.x = ThreeMath.degToRad(90.0);
+        this.userMesh.rotation.y = ThreeMath.degToRad(45.0);
     }
 
     fileUpdated(calc: FileCalculationContext): void {
-        this.camera.updateMatrixWorld(true);
-        this.cameraHelper.update();
+        this.file3D.display.updateMatrixWorld(false);
     }
 
     frameUpdate(calc: FileCalculationContext) {
@@ -87,14 +80,12 @@ export class UserMeshDecorator extends AuxFile3DDecorator {
     }
 
     dispose() {
-        this.file3D.remove(this.container);
-        this.file3D.display.remove(this.camera);
+        this.file3D.display.remove(this.container);
 
-        this.cameraHelper.geometry.dispose();
-        disposeMaterial(this.cameraHelper.material);
+        this.userMesh.geometry.dispose();
+        disposeMesh(this.userMesh);
 
-        this.camera = null;
-        this.cameraHelper = null;
+        this.userMesh = null;
         this.container = null;
     }
 
