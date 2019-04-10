d688686aa384ce3cbe9ff6de0684dc6e9855f402
Ryan Cook <ryan.cook@yeticgi.com>
2019-04-10 11:37:31 -0400

    chore: added typings for THREE.MeshToonMaterial

--- src/aux-server/typings/three/index.d.ts
+++ src/aux-server/typings/three/index.d.ts
@@ -1,7 +1,15 @@
-import { Object3D, Box3, Color } from "three";
+import { Object3D, Box3, Color, MeshPhongMaterial, MeshPhongMaterialParameters, Texture } from "three";
 
 declare module 'three' {
     class Box3Helper extends Object3D {
         constructor(box: Box3, color: Color);
+    }
+
+    class MeshToonMaterial extends MeshPhongMaterial {
+        defines: any;
+        gradientMap: Texture;
+        isMeshToonMaterial: boolean;
+
+        constructor(parameters: MeshPhongMaterialParameters);
     }
   }
