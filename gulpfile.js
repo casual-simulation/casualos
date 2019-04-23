46e784893af46dcea57b93e6d93c4da452713999
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-04-22 23:26:15 -0400

    refactor: Start making the libraries use the new typescript project references
    - Faster builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp');
 const del = require('del');
+const childProcess = require('child_process');
+const path = require('path');
 
 let folders = [
     `${__dirname}/src/aux-common`,
-    `${__dirname}/src/causal-trees`,
-    `${__dirname}/src/causal-tree-server-socketio`,
-    `${__dirname}/src/crypto`,
-    `${__dirname}/src/crypto-node`,
-    `${__dirname}/src/crypto-browser`,
+    `${__dirname}/src/causal-trees/lib`,
+    `${__dirname}/src/causal-tree-server-socketio/lib`,
+    `${__dirname}/src/crypto/lib`,
+    `${__dirname}/src/crypto-node/lib`,
+    `${__dirname}/src/crypto-browser/lib`,
 ];
 
 let patterns = [`/**/*.js`, `/**/*.js.map`, `/**/*.d.ts`];
@@ -28,3 +30,15 @@ folders.forEach(f => {
 gulp.task('clean', function() {
     return del(globs);
 });
+
+gulp.task('build:libs', function() {
+    return childProcess.exec('npx tsc --build');
+});
+
+gulp.task('build:server', function() {
+    return childProcess.exec('npm run build', {
+        cwd: path.resolve(__dirname, 'src', 'aux-server'),
+    });
+});
+
+gulp.task('build', ['clean', 'build:libs', 'build:server']);
