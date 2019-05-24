8623a4c5f6ee9d2825fd3972d5b592fe4075deaa
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-05-24 13:54:16 -0400

    feat: Add a play button to the search bar that executes the script

--- src/aux-server/aux-web/aux-projector/FileSearch/FileSearch.vue
+++ src/aux-server/aux-web/aux-projector/FileSearch/FileSearch.vue
@@ -5,6 +5,10 @@
             <md-input v-model="search"></md-input>
             <!-- <span class="md-suffix num-files" @click="toggleOpen()">1</span> -->
         </md-field>
+        <md-button v-show="search" class="md-icon-button" @click="executeSearch()">
+            <md-icon>play_arrow</md-icon>
+            <md-tooltip md-direction="bottom">Run Script</md-tooltip>
+        </md-button>
         <md-button class="md-icon-button num-files" @click="toggleOpen()">
             {{ files.length || 0 }}
         </md-button>
