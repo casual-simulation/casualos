d3294ed3332383520444dd3712de98dd16a8596b
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-06-20 14:48:15 -0400

    fix: Make source maps work for the web worker in development
    - Uses the publicPath property to tell webpack where to get the source map from.
    - For production we'll need a different path. Maybe in the future we'll have a public repo that people can get them from.

--- src/aux-server/aux-web/webpack.common.js
+++ src/aux-server/aux-web/webpack.common.js
@@ -112,10 +112,6 @@ module.exports = {
                 include: [/aux-common/, /aux-vm/],
                 enforce: 'pre',
             },
-            // {
-            //     test: /\.worker\.ts$/,
-            //     use: ['worker-loader'],
-            // },
         ],
     },
     resolve: {
