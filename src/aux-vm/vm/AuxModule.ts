d16d1b775f8228a1ead162f195ea30cd3d933130
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-07-18 23:47:03 -0400

    feat: Add an interface for modules that can be added to AuxChannels

---
+++ src/aux-vm/vm/AuxModule.ts
@@ -0,0 +1,14 @@
+import { AuxChannel } from './AuxChannel';
+import { Subscription } from 'rxjs';
+
+/**
+ * Defines an interface for objects which are able to extend a AuxChannel with custom logic.
+ */
+export interface AuxModule {
+    /**
+     * Sets up the services/dependencies that the module needs
+     * to perform its duties. Returns a subscription that, when unsubscribed, will dispose of extra resources.
+     * @param channel The channel that the module should be setup on.
+     */
+    setup(channel: AuxChannel): Subscription;
+}
