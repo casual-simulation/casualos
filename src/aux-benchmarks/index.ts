e6bb35f162c3d45ca1e4eb25040d68c54015f1a0
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-08-30 18:02:28 -0400

    feat: Add the ability to view a flamegraph of the benchmarks

--- src/aux-benchmarks/index.ts
+++ src/aux-benchmarks/index.ts
@@ -2,21 +2,10 @@ import 'platform';
 import Benchmark from 'benchmark';
 import AuxCommonBenchmarks from '@casual-simulation/aux-common/benchmarks';
 import CausalTreeBenchmarks from '@casual-simulation/causal-trees/benchmarks';
-import fs from 'fs';
-import path from 'path';
-const profiler = require('v8-profiler');
 
 run([...CausalTreeBenchmarks, ...AuxCommonBenchmarks]);
 
 async function run(benchmarks: Benchmark.Suite[]) {
-    const shouldProfile = process.argv.some(a => a === '--profile');
-    const id = 'bench-profile.json';
-
-    if (shouldProfile) {
-        console.log('Profiling...');
-        profiler.startProfiling(id);
-    }
-
     for (let bench of benchmarks) {
         bench
             .on('complete', () => {
@@ -26,13 +15,6 @@ async function run(benchmarks: Benchmark.Suite[]) {
                 }
             })
             .run({ async: true });
-    }
-
-    if (shouldProfile) {
-        const profile = profiler.stopProfiling(id);
-        fs.writeFile(path.join(__dirname, id), profile, () => {
-            process.exit();
-        });
     }
 }
 
