61d18e906438f1f6b339afb8e9bd6aa0e9c0ae27
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-08-30 16:27:15 -0400

    feat: Add correct ordering and improve discoverability of atoms in the weave

--- src/causal-trees/core2/Weave2.spec.ts
+++ src/causal-trees/core2/Weave2.spec.ts
@@ -1,11 +1,21 @@
-import { Weave } from './Weave2';
+import {
+    Weave,
+    lastInCausalGroup,
+    iterateCausalGroup,
+    AtomConflictResult,
+    iterateFrom,
+} from './Weave2';
 import { atom, atomId } from './Atom2';
 
 describe('Weave2', () => {
     describe('insert()', () => {
-        it('should add the first atom as the root', () => {
-            let weave = new Weave();
+        let weave: Weave<any>;
 
+        beforeEach(() => {
+            weave = new Weave();
+        });
+
+        it('should add the first atom as the root', () => {
             const root = atom(atomId('a', 0), null, {});
             const result = weave.insert(root);
 
@@ -17,8 +27,6 @@ describe('Weave2', () => {
         });
 
         it('should allow adding a second root', () => {
-            let weave = new Weave();
-
             const root = atom(atomId('a', 0), null, {});
             const root2 = atom(atomId('b', 0), null, {});
             weave.insert(root);
@@ -32,8 +40,6 @@ describe('Weave2', () => {
         });
 
         it('should reject the root atom if it doesnt match its own hash', () => {
-            let weave = new Weave();
-
             const root = atom(atomId('a', 0), null, {});
             root.hash = 'bad';
             const result = weave.insert(root);
@@ -46,8 +52,6 @@ describe('Weave2', () => {
         });
 
         it('should reject the atom if the cause was not found', () => {
-            let weave = new Weave();
-
             const cause = atom(atomId('a', 0), null, {});
             const atom1 = atom(atomId('a', 1), cause, {});
             const result = weave.insert(atom1);
@@ -60,8 +64,6 @@ describe('Weave2', () => {
         });
 
         it('should reject the atom if it doesnt match its own hash', () => {
-            let weave = new Weave();
-
             const cause = atom(atomId('a', 0), null, {});
             const atom1 = atom(atomId('a', 1), cause, {});
             atom1.hash = 'bad';
@@ -76,8 +78,6 @@ describe('Weave2', () => {
         });
 
         it('should reject the atom if the timestamp is before the cause timestamp', () => {
-            let weave = new Weave();
-
             const cause = atom(atomId('a', 5), null, {});
             const atom1 = atom(atomId('a', 1), cause, {});
             weave.insert(cause);
@@ -91,8 +91,6 @@ describe('Weave2', () => {
         });
 
         it('should treat a duplicate atom as if it was added', () => {
-            let weave = new Weave();
-
             const cause = atom(atomId('a', 5), null, {});
             const atom1 = atom(atomId('a', 6), cause, {});
             weave.insert(cause);
@@ -107,8 +105,6 @@ describe('Weave2', () => {
         });
 
         it('should treat atoms with different priorities as different atoms', () => {
-            let weave = new Weave();
-
             const cause = atom(atomId('a', 5), null, {});
             const atom1 = atom(atomId('a', 6), cause, {});
             const atom2 = atom(atomId('a', 6, 5), cause, {});
@@ -123,55 +119,150 @@ describe('Weave2', () => {
             expect(weave.getAtoms()).toEqual([cause, atom2, atom1]);
         });
 
-        it('should treat atoms with the same ID but different causes as a conflict', () => {
-            let weave = new Weave();
+        describe('conflicts', () => {
+            it('should treat atoms with the same ID but different causes as a conflict', () => {
+                const cause1 = atom(atomId('1', 0), null, {});
+                const cause2 = atom(atomId('2', 0), null, {});
+                const atom1 = atom(atomId('a', 6), cause1, {});
+                const atom2 = atom(atomId('a', 6), cause2, {});
+                weave.insert(cause1);
+                weave.insert(cause2);
+                weave.insert(atom1);
+                const result = weave.insert(atom2);
 
-            const cause1 = atom(atomId('1', 0), null, {});
-            const cause2 = atom(atomId('2', 0), null, {});
-            const atom1 = atom(atomId('a', 6), cause1, {});
-            const atom2 = atom(atomId('a', 6), cause2, {});
-            weave.insert(cause1);
-            weave.insert(cause2);
-            weave.insert(atom1);
-            const result = weave.insert(atom2);
-
-            // atom1 should have the lowest hash
-            expect(result).toEqual({
-                type: 'conflict',
-                winner: atom1,
-                loser: atom2,
+                // atom1 should have the lowest hash
+                expect(result).toEqual({
+                    type: 'conflict',
+                    winner: atom1,
+                    loser: atom2,
+                });
+                expect(weave.getAtoms()).toEqual([cause1, atom1, cause2]);
             });
-            expect(weave.getAtoms()).toEqual([cause1, atom1, cause2]);
-        });
 
-        it('should choose the atom with the lowest hash as the winner of the conflict', () => {
-            let weave = new Weave();
+            it('should choose the atom with the lowest hash as the winner of the conflict', () => {
+                const cause1 = atom(atomId('1', 0), null, {});
+                const cause2 = atom(atomId('2', 0), null, {});
+                const atom1 = atom(atomId('a', 6), cause1, {
+                    data: 'def',
+                });
+                const atom2 = atom(atomId('a', 6), cause2, {
+                    data: 'ghi',
+                });
+                weave.insert(cause1);
+                weave.insert(cause2);
+                weave.insert(atom1);
+                const result = weave.insert(atom2);
 
-            const cause1 = atom(atomId('1', 0), null, {});
-            const cause2 = atom(atomId('2', 0), null, {});
-            const atom1 = atom(atomId('a', 6), cause1, {
-                data: 'def',
+                // atom2 should have the lowest hash
+                expect(result).toEqual({
+                    type: 'conflict',
+                    winner: atom2,
+                    loser: atom1,
+                });
+                expect(weave.getAtoms()).toEqual([cause1, cause2, atom2]);
             });
-            const atom2 = atom(atomId('a', 6), cause2, {
-                data: 'ghi',
-            });
-            weave.insert(cause1);
-            weave.insert(cause2);
-            weave.insert(atom1);
-            const result = weave.insert(atom2);
 
-            // atom2 should have the lowest hash
-            expect(result).toEqual({
-                type: 'conflict',
-                winner: atom2,
-                loser: atom1,
+            it('should insert the new atom in the correct spot in the weave', () => {
+                const cause1 = atom(atomId('1', 0), null, {});
+                const cause2 = atom(atomId('2', 0), null, {});
+                const atom1 = atom(atomId('a', 1), cause2, {});
+                const atom2 = atom(atomId('a', 6), cause1, {
+                    data: 'def',
+                });
+                const atom3 = atom(atomId('a', 6), cause2, {
+                    data: 'ghi',
+                });
+                const atom4 = atom(atomId('a', 7), cause2, {});
+                weave.insert(cause1);
+                weave.insert(cause2);
+                weave.insert(atom1);
+                weave.insert(atom4);
+                weave.insert(atom2);
+                const result = weave.insert(atom3);
+
+                // atom2 should have the lowest hash
+                expect(result).toEqual({
+                    type: 'conflict',
+                    winner: atom3,
+                    loser: atom2,
+                });
+                expect(weave.getAtoms()).toEqual([
+                    cause1,
+                    cause2,
+                    atom4,
+                    atom3,
+                    atom1,
+                ]);
             });
-            expect(weave.getAtoms()).toEqual([cause1, cause2, atom2]);
+
+            it('should remove the children of the loser', () => {
+                const cause1 = atom(atomId('1', 0), null, {});
+                const cause2 = atom(atomId('2', 0), null, {});
+                const atom2 = atom(atomId('a', 6), cause1, {
+                    data: 'def',
+                });
+
+                const atom2a = atom(atomId('a', 7), atom2, {});
+                const atom2b = atom(atomId('a', 8), atom2, {});
+
+                const atom3 = atom(atomId('a', 6), cause2, {
+                    data: 'ghi',
+                });
+                weave.insert(cause1);
+                weave.insert(cause2);
+                weave.insert(atom2);
+                weave.insert(atom2a);
+                weave.insert(atom2b);
+                const result = weave.insert(atom3);
+
+                // atom2 should have the lowest hash
+                expect(result).toEqual({
+                    type: 'conflict',
+                    winner: atom3,
+                    loser: atom2,
+                });
+                expect(weave.getAtoms()).toEqual([cause1, cause2, atom3]);
+            });
+
+            describe('getConflictInfo()', () => {
+                it('should return a ref to the loser nodes if they were removed from the weave', () => {
+                    const root = atom(atomId('1', 0), null, {});
+                    const cause1 = atom(atomId('1', 1), root, {});
+                    const cause2 = atom(atomId('2', 1), root, {});
+                    const atom2 = atom(atomId('a', 6), cause1, {
+                        data: 'def',
+                    });
+
+                    const atom2a = atom(atomId('a', 7), atom2, {});
+                    const atom2b = atom(atomId('a', 8), atom2, {});
+
+                    const atom3 = atom(atomId('a', 6), cause2, {
+                        data: 'ghi',
+                    });
+                    weave.insert(root);
+                    weave.insert(cause1);
+                    weave.insert(cause2);
+                    weave.insert(atom2);
+                    weave.insert(atom2a);
+                    weave.insert(atom2b);
+                    const result = weave.insert(atom3) as AtomConflictResult;
+
+                    expect(result.type).toBe('conflict');
+
+                    const info = weave.getConflictInfo(result);
+                    const nodes = [...iterateFrom(info.loserRef)];
+
+                    expect(info.loserRef.prev).toBe(null);
+                    expect(nodes.map(n => n.atom)).toEqual([
+                        atom2,
+                        atom2b,
+                        atom2a,
+                    ]);
+                });
+            });
         });
 
         it('should add the atom after its cause', () => {
-            let weave = new Weave();
-
             const cause = atom(atomId('a', 5), null, {});
             const atom1 = atom(atomId('a', 6), cause, {});
             weave.insert(cause);
@@ -182,6 +273,238 @@ describe('Weave2', () => {
                 atom: atom1,
             });
             expect(weave.getAtoms()).toEqual([cause, atom1]);
+        });
+
+        it('should order atoms by their timestamp', () => {
+            const cause = atom(atomId('a', 0), null, {});
+            const atom1 = atom(atomId('a', 1), cause, {});
+            const atom2 = atom(atomId('a', 2), cause, {});
+            const atom3 = atom(atomId('a', 3), cause, {});
+            weave.insert(cause);
+            weave.insert(atom1);
+            weave.insert(atom3);
+            weave.insert(atom2);
+
+            expect(weave.getAtoms()).toEqual([cause, atom3, atom2, atom1]);
+        });
+
+        it('should order atoms by site if timestamp is the same', () => {
+            const cause = atom(atomId('a', 0), null, {});
+            const atom1 = atom(atomId('a', 1), cause, {});
+            const atom2 = atom(atomId('b', 1), cause, {});
+            const atom3 = atom(atomId('c', 1), cause, {});
+            weave.insert(cause);
+            weave.insert(atom1);
+            weave.insert(atom3);
+            weave.insert(atom2);
+
+            expect(weave.getAtoms()).toEqual([cause, atom1, atom2, atom3]);
+        });
+
+        it('should order atoms by priority first', () => {
+            const cause = atom(atomId('a', 0), null, {});
+            const atom1 = atom(atomId('a', 1, 3), cause, {});
+            const atom2 = atom(atomId('b', 2, 2), cause, {});
+            const atom3 = atom(atomId('c', 3, 1), cause, {});
+            weave.insert(cause);
+            weave.insert(atom1);
+            weave.insert(atom3);
+            weave.insert(atom2);
+
+            expect(weave.getAtoms()).toEqual([cause, atom1, atom2, atom3]);
+        });
+
+        it('should be able to insert atoms after the last atom', () => {
+            const cause = atom(atomId('a', 0), null, {});
+            const atom1 = atom(atomId('a', 1), cause, {});
+            const atom2 = atom(atomId('a', 2), cause, {});
+            const atom3 = atom(atomId('a', 3), cause, {});
+            weave.insert(cause);
+            weave.insert(atom2);
+            weave.insert(atom3);
+            weave.insert(atom1);
+
+            expect(weave.getAtoms()).toEqual([cause, atom3, atom2, atom1]);
+        });
+
+        it('should be able to insert atoms before the next causal group', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const cause = atom(atomId('a', 1), root, {});
+            const cause2 = atom(atomId('a', 2), root, {});
+            const atom1 = atom(atomId('a', 3), cause2, {});
+            weave.insert(root);
+            weave.insert(cause);
+            weave.insert(cause2);
+            weave.insert(atom1);
+
+            expect(weave.getAtoms()).toEqual([root, cause2, atom1, cause]);
+        });
+
+        it('should be able to insert atoms after a causal group', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const cause = atom(atomId('a', 1), root, {});
+            const cause3 = atom(atomId('a', 4), root, {});
+            const cause2 = atom(atomId('a', 5), root, {});
+            const atom1 = atom(atomId('a', 6), cause2, {});
+            const atom2 = atom(atomId('a', 7), cause2, {});
+
+            weave.insert(root);
+            weave.insert(cause);
+            weave.insert(cause2);
+            weave.insert(atom1);
+            weave.insert(atom2);
+            weave.insert(cause3);
+
+            expect(weave.getAtoms()).toEqual([
+                root,
+                cause2,
+                atom2,
+                atom1,
+                cause3,
+                cause,
+            ]);
+        });
+
+        it('should be able to insert atoms before the next root', () => {
+            const root1 = atom(atomId('a', 0), null, {});
+            const root2 = atom(atomId('b', 0), null, {});
+
+            const atom1 = atom(atomId('a', 1), root1, {});
+
+            weave.insert(root1);
+            weave.insert(root2);
+            weave.insert(atom1);
+
+            expect(weave.getAtoms()).toEqual([root1, atom1, root2]);
+        });
+    });
+
+    describe('lastInCausalGroup()', () => {
+        let weave: Weave<any>;
+        beforeEach(() => {
+            weave = new Weave();
+        });
+
+        it('should return the last child of the given node', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const a1 = atom(atomId('a', 1), root, {});
+            const a2 = atom(atomId('a', 2), root, {});
+
+            weave.insert(root);
+            weave.insert(a1);
+            weave.insert(a2);
+
+            const last = lastInCausalGroup(weave.roots[0]);
+
+            expect(last.atom).toBe(a1);
+            expect(weave.getAtoms()).toEqual([root, a2, a1]);
+        });
+
+        it('should return the last deeply nested child of the given node', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const a1 = atom(atomId('a', 1), root, {});
+            const a2 = atom(atomId('a', 2), a1, {});
+            const a3 = atom(atomId('a', 3), a2, {});
+            const a4 = atom(atomId('a', 4), a1, {});
+            const a5 = atom(atomId('a', 5), root, {});
+
+            weave.insert(root);
+            weave.insert(a1);
+            weave.insert(a2);
+            weave.insert(a3);
+            weave.insert(a4);
+            weave.insert(a5);
+
+            const last = lastInCausalGroup(weave.roots[0]);
+
+            expect(last.atom).toBe(a3);
+            expect(weave.getAtoms()).toEqual([root, a5, a1, a4, a2, a3]);
+        });
+
+        it('should return the given node if it has no children', () => {
+            const root = atom(atomId('a', 0), null, {});
+
+            weave.insert(root);
+
+            const last = lastInCausalGroup(weave.roots[0]);
+
+            expect(last.atom).toBe(root);
+            expect(weave.getAtoms()).toEqual([root]);
+        });
+    });
+
+    describe('iterateCausalGroup()', () => {
+        let weave: Weave<any>;
+        beforeEach(() => {
+            weave = new Weave();
+        });
+
+        it('should return the children of the given node', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const a1 = atom(atomId('a', 1), root, {});
+            const a2 = atom(atomId('a', 2), root, {});
+
+            weave.insert(root);
+            weave.insert(a1);
+            weave.insert(a2);
+
+            const nodes = [...iterateCausalGroup(weave.roots[0])];
+
+            expect(nodes.map(n => n.atom)).toEqual([a2, a1]);
+        });
+
+        it('should return the nested children of the given node', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const a1 = atom(atomId('a', 1), root, {});
+            const a2 = atom(atomId('a', 2), root, {});
+            const a3 = atom(atomId('a', 3), a1, {});
+            const a4 = atom(atomId('a', 4), a2, {});
+            const a5 = atom(atomId('a', 5), root, {});
+
+            weave.insert(root);
+            weave.insert(a1);
+            weave.insert(a2);
+            weave.insert(a3);
+            weave.insert(a4);
+            weave.insert(a5);
+
+            const nodes = [...iterateCausalGroup(weave.roots[0])];
+
+            expect(nodes.map(n => n.atom)).toEqual([a5, a2, a4, a1, a3]);
+        });
+
+        it('should exclude non-children of the given node', () => {
+            const root = atom(atomId('a', 0), null, {});
+            const a1 = atom(atomId('a', 1), root, {});
+            const a2 = atom(atomId('a', 2), root, {});
+            const a3 = atom(atomId('a', 3), a1, {});
+            const a4 = atom(atomId('a', 4), a2, {});
+            const a5 = atom(atomId('a', 5), root, {});
+
+            weave.insert(root);
+            weave.insert(a1);
+            weave.insert(a2);
+            weave.insert(a3);
+            weave.insert(a4);
+            weave.insert(a5);
+
+            const nodes = [...iterateCausalGroup(weave.getNode(a2.id))];
+
+            expect(nodes.map(n => n.atom)).toEqual([a4]);
+        });
+
+        it('should exclude atoms with the same timestamp', () => {
+            const root = atom(atomId('1', 0), null, {});
+            const cause1 = atom(atomId('1', 1), root, {});
+            const cause2 = atom(atomId('2', 1), root, {});
+
+            weave.insert(root);
+            weave.insert(cause1);
+            weave.insert(cause2);
+
+            const nodes = [...iterateCausalGroup(weave.getNode(cause1.id))];
+
+            expect(nodes.map(n => n.atom)).toEqual([]);
         });
     });
 });
--- src/causal-trees/core2/Weave2.ts
+++ src/causal-trees/core2/Weave2.ts
@@ -87,26 +87,55 @@ export interface WeaveNode<T> {
 }
 
 /**
+ * Info about the conflict.
+ */
+export interface ConflictInfo<T> {
+    /**
+     * The conflict.
+     */
+    conflict: AtomConflictResult;
+
+    /**
+     * A reference to the weave nodes that were removed from the weave.
+     */
+    loserRef: WeaveNode<T>;
+}
+
+/**
  * Defines a weave.
  * That is, the depth-first preorder traversal of a causal tree.
  */
 export class Weave<T> {
     private _roots: WeaveNode<T>[];
     private _idMap: Map<string, WeaveNode<T>>;
+    private _lastConflict: ConflictInfo<T>;
 
-    iterateFrom = function*(start: WeaveNode<T>) {
-        let current = start;
-        while (current) {
-            yield current.atom;
-            current = current.next;
+    /**
+     * Gets the root nodes used by this weave.
+     */
+    get roots() {
+        return this._roots;
+    }
+
+    /**
+     * Gets the info for the given conflict.
+     * If null, then the info has been disposed.
+     * @param conflict The conflict to lookup.
+     */
+    getConflictInfo(conflict: AtomConflictResult): ConflictInfo<T> {
+        if (this._lastConflict && this._lastConflict.conflict === conflict) {
+            return this._lastConflict;
         }
-    };
+        return null;
+    }
 
-    iterate() {
+    iterateAtoms() {
         const _this = this;
         function* iterator() {
             for (let root of _this._roots) {
-                yield* _this.iterateFrom(root);
+                for (let node of iterateFrom(root)) {
+                    yield node.atom;
+                }
             }
         }
 
@@ -117,7 +146,7 @@ export class Weave<T> {
      * Gets the full list of atoms from the weave.
      */
     getAtoms(): Atom<T>[] {
-        return [...this.iterate()];
+        return [...this.iterateAtoms()];
     }
 
     /**
@@ -186,21 +215,34 @@ export class Weave<T> {
             if (existing.atom.hash !== atom.hash) {
                 if (existing.atom.hash < atom.hash) {
                     // No changes needed
-                    return {
+                    const conflict: AtomConflictResult = {
                         type: 'conflict',
                         winner: existing.atom,
                         loser: atom,
                     };
+
+                    this._lastConflict = {
+                        conflict: conflict,
+                        loserRef: null,
+                    };
+
+                    return conflict;
                 } else {
                     // Replace the existing atom with the new one.
                     this._remove(existing);
-                    this._insertAfter(causeNode, atom);
+                    this._insertUnder(causeNode, atom);
 
-                    return {
+                    const conflict: AtomConflictResult = {
                         type: 'conflict',
                         winner: atom,
                         loser: existing.atom,
                     };
+                    this._lastConflict = {
+                        conflict: conflict,
+                        loserRef: existing,
+                    };
+
+                    return conflict;
                 }
             }
             return {
@@ -209,7 +251,7 @@ export class Weave<T> {
             };
         }
 
-        this._insertAfter(causeNode, atom);
+        this._insertUnder(causeNode, atom);
 
         return {
             type: 'atom_added',
@@ -220,6 +262,44 @@ export class Weave<T> {
     getNode(atomId: AtomId): WeaveNode<T> {
         const id = atomIdToString(atomId);
         const node = this._idMap.get(id);
+        return node;
+    }
+
+    /**
+     * Inserts the given atom under the given cause atom.
+     * @param cause The cause.
+     * @param atom The atom.
+     */
+    private _insertUnder(cause: WeaveNode<T>, atom: Atom<T>) {
+        let last: WeaveNode<T>;
+        for (let node of iterateCausalGroup(cause)) {
+            if (idEquals(node.atom.cause, cause.atom.id)) {
+                if (_compareAtomIds(atom.id, node.atom.id) < 0) {
+                    return this._insertBefore(node, atom);
+                }
+            }
+            last = node;
+        }
+
+        if (last) {
+            return this._insertAfter(last, atom);
+        } else {
+            return this._insertAfter(cause, atom);
+        }
+    }
+
+    private _insertBefore(pos: WeaveNode<T>, atom: Atom<T>) {
+        const prev = pos.prev;
+        const node: WeaveNode<T> = {
+            atom: atom,
+            next: pos,
+            prev: prev,
+        };
+        if (prev) {
+            prev.next = node;
+        }
+        pos.prev = node;
+        this._addNodeToIdMap(node);
         return node;
     }
 
@@ -239,7 +319,8 @@ export class Weave<T> {
     }
 
     private _remove(node: WeaveNode<T>) {
-        const next = node.next;
+        const last = lastInCausalGroup(node);
+        const next = last.next;
         const prev = node.prev;
         if (next) {
             next.prev = prev;
@@ -247,14 +328,102 @@ export class Weave<T> {
         if (prev) {
             prev.next = next;
         }
-        this._removeNodeIdFromMap(node);
+        node.prev = null;
+        last.next = null;
+        this._removeNodeFromMap(node);
     }
 
     private _addNodeToIdMap(node: WeaveNode<T>) {
         this._idMap.set(atomIdToString(node.atom.id), node);
     }
 
+    private _removeNodeFromMap(node: WeaveNode<T>) {
+        this._removeNodeIdFromMap(node);
+        for (let n of iterateCausalGroup(node)) {
+            this._removeNodeIdFromMap(n);
+        }
+    }
+
     private _removeNodeIdFromMap(node: WeaveNode<T>) {
         this._idMap.delete(atomIdToString(node.atom.id));
     }
+}
+
+/**
+ * Gets the last node in the given node's causal group.
+ * @param start The node to start from.
+ */
+export function lastInCausalGroup<T>(start: WeaveNode<T>) {
+    let last: WeaveNode<T>;
+    for (let node of iterateCausalGroup(start)) {
+        last = node;
+    }
+    return last || start;
+}
+
+/**
+ * Iterates all of the nodes in the given node's causal group.
+ * @param start The node to start from.
+ */
+export function* iterateCausalGroup<T>(start: WeaveNode<T>) {
+    for (let node of iterateFrom(start.next)) {
+        if (node.atom.id.timestamp <= start.atom.id.timestamp) {
+            break;
+        }
+        yield node;
+    }
+}
+
+/**
+ * Iterates all of the nodes to the end of the linked list.
+ * @param start The node to start from.
+ */
+export function* iterateFrom<T>(start: WeaveNode<T>) {
+    let current = start;
+    while (current) {
+        yield current;
+        current = current.next;
+    }
+}
+
+/**
+ * Determines if the first atom ID should sort before, at, or after the second atom ID.
+ * Returns -1 if the first should be before the second.
+ * Returns 0 if the IDs are equal.
+ * Returns 1 if the first should be after the second.
+ * @param first The first atom ID.
+ * @param second The second atom ID.
+ */
+function _compareAtomIds(first: AtomId, second: AtomId) {
+    if (!first && second) {
+        return -1;
+    } else if (!second && first) {
+        return 1;
+    } else if (first === second) {
+        return 0;
+    }
+    const firstPriority = _getPriority(first);
+    const secondPriority = _getPriority(second);
+    if (firstPriority > secondPriority) {
+        return -1;
+    } else if (firstPriority < secondPriority) {
+        return 1;
+    } else if (firstPriority === secondPriority) {
+        if (first.timestamp > second.timestamp) {
+            return -1;
+        } else if (first.timestamp < second.timestamp) {
+            return 1;
+        } else if (first.timestamp === second.timestamp) {
+            if (first.site < second.site) {
+                return -1;
+            } else if (first.site > second.site) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+function _getPriority(id: AtomId) {
+    return id.priority || 0;
 }
