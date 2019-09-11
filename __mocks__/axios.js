3d19522733853296f925a1a34e4204097ca7fe86
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-09-11 13:47:43 -0400

    feat: Add better error handling to the Directory service
    - Also improve the webhook system to know whether the webhook succeeded or failed and if it should resend.

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -2,12 +2,19 @@
 
 const axios = jest.genMockFromModule('axios');
 
+let shouldFail = false;
 let lastPost;
 let lastPut;
 axios.post = (url, data) => {
+    if (shouldFail) {
+        throw new Error('Post failed.');
+    }
     lastPost = [url, data];
 };
 axios.put = (url, data) => {
+    if (shouldFail) {
+        throw new Error('Put failed.');
+    }
     lastPut = [url, data];
 };
 
@@ -16,6 +23,10 @@ axios.__getLastPut = () => lastPut;
 axios.__reset = () => {
     lastPost = undefined;
     lastPut = undefined;
+    shouldFail = false;
+};
+axios.__setFail = fail => {
+    shouldFail = fail;
 };
 
 module.exports = axios;
