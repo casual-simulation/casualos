40c274f1865a70ee6fbec28eb09e4f0257b6e98a
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-09-11 13:16:47 -0400

    fix: Use the correct network interfaces for getting private IP Addresses
    - Also switch the key to be based on a generated value instead of a MAC address. (MAC Addresses are too unpredictable)

--- src/aux-server/server/directory/DirectoryClient.spec.ts
+++ src/aux-server/server/directory/DirectoryClient.spec.ts
@@ -3,6 +3,8 @@ import { DirectoryStore } from './DirectoryStore';
 import { MemoryDirectoryStore } from './MemoryDirectoryStore';
 import { DEFAULT_PING_INTERVAL } from './DirectoryClientSettings';
 
+console.error = jest.fn();
+
 jest.mock('axios');
 jest.mock('os');
 jest.useFakeTimers();
@@ -13,6 +15,7 @@ describe('DirectoryClient', () => {
 
     beforeEach(async () => {
         require('os').__setHostname('testHostname');
+        require('os').__setInterfaces({});
 
         store = new MemoryDirectoryStore();
 
@@ -33,6 +36,7 @@ describe('DirectoryClient', () => {
                 pingInterval: DEFAULT_PING_INTERVAL,
                 token: null,
                 password: expect.any(String),
+                key: expect.any(String),
             });
         });
 
@@ -41,6 +45,7 @@ describe('DirectoryClient', () => {
                 pingInterval: 100,
                 token: null,
                 password: 'def',
+                key: 'test',
             });
 
             await client.init();
@@ -51,6 +56,7 @@ describe('DirectoryClient', () => {
                 pingInterval: 100,
                 token: null,
                 password: 'def',
+                key: 'test',
             });
         });
     });
@@ -58,10 +64,37 @@ describe('DirectoryClient', () => {
     describe('upstream', () => {
         beforeEach(async () => {
             require('axios').__reset();
+            require('os').__setInterfaces({
+                abc: [
+                    {
+                        address: '172.16.99.1',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+                eth0: [
+                    {
+                        address: '192.168.1.65',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'ethernet:address',
+                    },
+                ],
+                wlan0: [
+                    {
+                        address: '192.168.1.128',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'wlan:address',
+                    },
+                ],
+            });
             await store.saveClientSettings({
                 pingInterval: 100,
                 token: null,
                 password: 'def',
+                key: 'test',
             });
             await client.init();
         });
@@ -72,7 +105,7 @@ describe('DirectoryClient', () => {
                 Array [
                   "https://example.com/api/directory",
                   Object {
-                    "key": "8c1a3f6e96e480fd4a265e3aeeab162771ae584cfdc3c03449a37403a1352ac1",
+                    "key": "test",
                     "password": "def",
                     "privateIpAddress": "192.168.1.65",
                     "publicName": "testHostname",
@@ -88,7 +121,7 @@ describe('DirectoryClient', () => {
                 Array [
                   "https://example.com/api/directory",
                   Object {
-                    "key": "8c1a3f6e96e480fd4a265e3aeeab162771ae584cfdc3c03449a37403a1352ac1",
+                    "key": "test",
                     "password": "def",
                     "privateIpAddress": "192.168.1.65",
                     "publicName": "testHostname",
@@ -106,13 +139,167 @@ describe('DirectoryClient', () => {
                 Array [
                   "https://example.com/api/directory",
                   Object {
-                    "key": "8c1a3f6e96e480fd4a265e3aeeab162771ae584cfdc3c03449a37403a1352ac1",
+                    "key": "test",
                     "password": "def",
                     "privateIpAddress": "192.168.1.65",
                     "publicName": "testHostname",
                   },
                 ]
             `);
+        });
+    });
+
+    describe('network interfaces', () => {
+        beforeEach(async () => {
+            require('axios').__reset();
+
+            await store.saveClientSettings({
+                pingInterval: 100,
+                token: null,
+                password: 'def',
+                key: 'test',
+            });
+        });
+
+        it('should choose eth interfaces first', async () => {
+            require('os').__setInterfaces({
+                abc: [
+                    {
+                        address: '172.16.99.1',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+                eth0: [
+                    {
+                        address: '192.168.1.65',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'ethernet:address',
+                    },
+                ],
+                wlan0: [
+                    {
+                        address: '192.168.1.128',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'wlan:address',
+                    },
+                ],
+            });
+            await client.init();
+
+            let [url, request] = require('axios').__getLastPut();
+            expect(request.privateIpAddress).toEqual('192.168.1.65');
+        });
+
+        it('should choose en interfaces second', async () => {
+            require('os').__setInterfaces({
+                abc: [
+                    {
+                        address: '172.16.99.1',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+                en1: [
+                    {
+                        address: '192.168.1.65',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'ethernet:address',
+                    },
+                ],
+                wlan0: [
+                    {
+                        address: '192.168.1.128',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'wlan:address',
+                    },
+                ],
+            });
+            await client.init();
+
+            let [url, request] = require('axios').__getLastPut();
+            expect(request.privateIpAddress).toEqual('192.168.1.65');
+        });
+
+        it('should choose wlan interfaces third', async () => {
+            require('os').__setInterfaces({
+                abc: [
+                    {
+                        address: '172.16.99.1',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+                wlan0: [
+                    {
+                        address: '192.168.1.128',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'wlan:address',
+                    },
+                ],
+            });
+            await client.init();
+
+            let [url, request] = require('axios').__getLastPut();
+            expect(request.privateIpAddress).toEqual('192.168.1.128');
+        });
+
+        it('should choose wl interfaces fourth', async () => {
+            require('os').__setInterfaces({
+                abc: [
+                    {
+                        address: '172.16.99.1',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+                wlan0: [
+                    {
+                        address: '192.168.1.128',
+                        family: 'IPv4',
+                        internal: false,
+                        max: 'wlan:address',
+                    },
+                ],
+            });
+            await client.init();
+
+            let [url, request] = require('axios').__getLastPut();
+            expect(request.privateIpAddress).toEqual('192.168.1.128');
+        });
+
+        it('should choose interfaces alphabetically last', async () => {
+            require('os').__setInterfaces({
+                zyd: [
+                    {
+                        address: '10.10.10.10',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+                abc: [
+                    {
+                        address: '172.16.99.1',
+                        family: 'IPv4',
+                        internal: false,
+                        mac: 'wrong:address',
+                    },
+                ],
+            });
+            await client.init();
+
+            let [url, request] = require('axios').__getLastPut();
+            expect(request.privateIpAddress).toEqual('172.16.99.1');
         });
     });
 });
--- src/aux-server/server/directory/DirectoryClient.ts
+++ src/aux-server/server/directory/DirectoryClient.ts
@@ -8,6 +8,7 @@ import { randomBytes } from 'crypto';
 import { hostname, networkInterfaces } from 'os';
 import { sha256 } from 'sha.js';
 import axios from 'axios';
+import { sortBy } from 'lodash';
 
 /**
  * Defines a client for the directory.
@@ -30,6 +31,7 @@ export class DirectoryClient {
             this._settings = {
                 pingInterval: DEFAULT_PING_INTERVAL,
                 password: generatePassword(),
+                key: getKey(),
                 token: null,
             };
             await this._store.saveClientSettings(this._settings);
@@ -49,27 +51,30 @@ export class DirectoryClient {
     }
 
     private async _ping() {
-        const iface = getNetworkInterface(false);
-        const url = new URL('/api/directory', this._config.upstream);
-        await axios.put(url.href, {
-            key: getKey(),
-            password: this._settings.password,
-            publicName: hostname(),
-            privateIpAddress: iface.address,
-        });
+        try {
+            const iface = getNetworkInterface(false);
+            if (!iface) {
+                throw new Error(
+                    'Cannot find a valid non-local network interface.'
+                );
+            }
+            const url = new URL('/api/directory', this._config.upstream);
+            await axios.put(url.href, {
+                key: this._settings.key,
+                password: this._settings.password,
+                publicName: hostname(),
+                privateIpAddress: iface.address,
+            });
+        } catch (ex) {
+            console.error('Unable to ping upstream directory.', ex);
+        }
     }
 }
 
 function getKey() {
-    const iface = getNetworkInterface(true);
-    if (!iface) {
-        throw new Error(
-            'Cannot calculate key because no valid network interfaces are available.'
-        );
-    }
-    const str = `${hostname()}.${iface.mac}`;
+    const bytes = randomBytes(32);
     const hash = new sha256();
-    hash.update(str);
+    hash.update(bytes);
     return hash.digest().toString('hex');
 }
 
@@ -80,25 +85,57 @@ function generatePassword() {
 }
 
 /**
- * Gets the first IPv4 Network interface.
+ * Gets the first IPv4 Network interface. Prefers ethernet and wlan interfaces before others.
  * @param local Whether to include the loopback interface.
  */
 function getNetworkInterface(local: boolean = false) {
     const net = networkInterfaces();
-    const keys = Object.keys(net).sort();
+    const keys = sortBy(
+        Object.keys(net),
+        ifname => {
+            if (ifname.startsWith('lo')) {
+                return 0;
+            } else if (ifname.startsWith('eth') || ifname.startsWith('en')) {
+                return 1;
+            } else if (ifname.startsWith('wlan') || ifname.startsWith('wl')) {
+                return 2;
+            } else {
+                return 3;
+            }
+        },
+        ifname => ifname
+    );
+
+    if (local) {
+        for (let key of keys) {
+            const ifname = net[key];
+            if (!ifname) {
+                continue;
+            }
+
+            for (let i = 0; i < ifname.length; i++) {
+                const iface = ifname[i];
+                if (iface.internal && iface.family === 'IPv4') {
+                    return iface;
+                }
+            }
+        }
+    }
+
     for (let key of keys) {
         const ifname = net[key];
-
         if (!ifname) {
             continue;
         }
 
         for (let i = 0; i < ifname.length; i++) {
             const iface = ifname[i];
-            if ((iface.internal && !local) || iface.family !== 'IPv4') {
+            if (iface.internal || iface.family !== 'IPv4') {
                 continue;
             }
             return iface;
         }
     }
+
+    return null;
 }
