1a1cda970655ef815c2bbf23566c1de6979a23b8
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-09-20 12:23:13 -0400

    feat: Add the ability to select an account from the list to the login popup

--- src/aux-server/aux-web/shared/vue-components/LoginPopup/LoginPopup.ts
+++ src/aux-server/aux-web/shared/vue-components/LoginPopup/LoginPopup.ts
@@ -1,6 +1,10 @@
 import Component from 'vue-class-component';
 import Vue from 'vue';
-import { Prop } from 'vue-property-decorator';
+import { Prop, Watch } from 'vue-property-decorator';
+import { appManager } from '../../AppManager';
+import { BrowserSimulation } from '@casual-simulation/aux-vm-browser';
+import uuid from 'uuid/v4';
+import { AuxUser } from '@casual-simulation/aux-vm';
 
 @Component({
     components: {},
@@ -8,7 +12,45 @@ import { Prop } from 'vue-property-decorator';
 export default class LoginPopup extends Vue {
     @Prop({ required: true }) show: boolean;
 
+    showProgress: boolean = false;
+    showList: boolean = true;
+    users: AuxUser[];
+
+    private _sim: BrowserSimulation;
+
     close() {
         this.$emit('close');
+    }
+
+    @Watch('show')
+    visibleChanged() {
+        this._sim = appManager.simulationManager.primary;
+        this.showProgress = false;
+    }
+
+    async created() {
+        this.users = (await appManager.getUsers()).filter(u => !u.isGuest);
+    }
+
+    continueAsGuest() {
+        this._login(`guest_${uuid()}`);
+    }
+
+    signIn(user: AuxUser) {
+        this._login(user.username);
+    }
+
+    private async _login(username: string, grant?: string) {
+        this.showProgress = true;
+
+        const user = await appManager.getUser(username);
+        await appManager.setCurrentUser(user);
+
+        if (this._sim) {
+            await this._sim.login.setUser(user);
+        } else {
+            console.error('[LoginPopup] Dont have a simulation!');
+        }
+        this.close();
     }
 }
--- src/aux-server/aux-web/shared/vue-components/LoginPopup/LoginPopup.vue
+++ src/aux-server/aux-web/shared/vue-components/LoginPopup/LoginPopup.vue
@@ -1,9 +1,33 @@
 <template>
     <div>
-        <md-dialog :md-active.sync="show" @md-closed="close()">
-            <p>
-                Hi!
-            </p>
+        <md-dialog :md-active="show" @md-closed="close()">
+            <div class="login-wrapper">
+                <div v-if="!showProgress">
+                    <p>Hi!</p>
+                    <div v-if="showList">
+                        <md-list>
+                            <md-list-item
+                                v-for="user in users"
+                                :key="user.username"
+                                @click="signIn(user)"
+                            >
+                                <span class="md-list-item-text">{{ user.username }}</span>
+                            </md-list-item>
+                        </md-list>
+                    </div>
+                    <a class="md-primary guest-button" @click="continueAsGuest"
+                        >Continue as a Guest</a
+                    >
+                </div>
+                <div v-else class="progress-section">
+                    <p>Logging in...</p>
+                    <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
+                </div>
+            </div>
+
+            <md-dialog-actions>
+                <md-button @click="close()">Close</md-button>
+            </md-dialog-actions>
         </md-dialog>
     </div>
 </template>
