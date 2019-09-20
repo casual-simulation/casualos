6b922b6f7849eef15212a44e3a1e632e9dd2ac28
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-09-20 15:23:34 -0400

    feat: Enable the authorization popup in AUXPlayer

--- src/aux-server/aux-web/aux-player/PlayerApp/PlayerApp.vue
+++ src/aux-server/aux-web/aux-player/PlayerApp/PlayerApp.vue
@@ -248,6 +248,7 @@
             </md-dialog>
 
             <login :show="showLogin" @close="showLogin = false"></login>
+            <authorize :show="showAuthorize" @close="showAuthorize = false"></authorize>
 
             <md-snackbar
                 md-position="center"
