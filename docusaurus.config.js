d7043b55111796e279bf66c566d7fc13fa21f747
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-10-23 22:57:55 -0400

    chore: Remove the blog link from the header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,6 @@ module.exports = {
       },
       links: [
         {to: 'docs/tags', label: 'Docs', position: 'left'},
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/casual-simulation/aux',
           label: 'GitHub',
