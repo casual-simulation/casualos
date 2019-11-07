8552099bacc52bb00b672d56b937b17574f2eb8c
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-11-07 17:21:12 -0500

    WIP: Work in progress on implementing the new Causal Tree Server

--- src/causal-tree-server-socketio/CausalTreeServer2SocketIO.ts
+++ src/causal-tree-server-socketio/CausalTreeServer2SocketIO.ts
@@ -19,6 +19,11 @@ import {
     tap,
     switchMap,
     takeWhile,
+    scan,
+    share,
+    endWith,
+    skipWhile,
+    take,
 } from 'rxjs/operators';
 import { empty } from '@hapi/joi';
 
@@ -55,45 +60,134 @@ export class CausalTreeServer2SocketIO {
     }
 
     private _setupServer() {
-        const observable = connections(this._socketServer).pipe(
-            flatMap(socket =>
-                // For each socket connection,
-                // listen for join_branch requests
-                joinBranchRequests(socket).pipe(
-                    flatMap(branch =>
-                        // For each branch request, load the branch
-                        // from the store.
-                        loadBranchFromStore(
-                            this._branches,
-                            this._store,
-                            branch
-                        ).pipe(
-                            switchMap(repo =>
-                                // When we get a repo,
-                                // listen for incoming atoms.
-                                merge(
-                                    incomingAtoms(socket, branch),
-                                    incomingEvents(socket, branch),
-                                    leaveBranch(socket, branch)
-                                )
-                            ),
+        const conn = connections(this._socketServer);
+        const newSocketActors = conn.pipe(map(socketActor));
+        const removedSocketActors = newSocketActors.pipe(
+            flatMap(a => disconnect(a.socket).pipe(map(_ => a)))
+        );
 
-                            // When we get a leave_branch request,
-                            // cancel the subscription.
-                            takeWhile(e => e.type !== 'leave_branch'),
+        const newActors = newSocketActors;
+        const removedActors = removedSocketActors;
 
-                            tap(e => {
-                                if (e.type === 'incoming_atom') {
-                                    // Add atom
-                                } else if (e.type === 'incoming_event') {
-                                    // issue remote event
-                                }
-                            })
-                        )
-                    )
+        const actorEvents = merge(
+            newActors.pipe(map(add)),
+            removedActors.pipe(map(remove))
+        );
+
+        const allActors = actorEvents.pipe(
+            scan(intoMap(a => a.id), new Map<string, SocketActor>()),
+            share()
+        );
+
+        const allJoins = newActors.pipe(
+            flatMap(a => a.join, (actor, branch) => [actor, branch] as const)
+        );
+
+        const allLeaves = allJoins.pipe(
+            flatMap(([actor, branch]) =>
+                actor.messages(branch).pipe(
+                    skipWhile(m => m.type !== 'leave'),
+                    map(m => [actor, branch] as const),
+                    take(1)
                 )
             )
         );
+
+        const branchEvents = merge(
+            allJoins.pipe(map(add)),
+            allLeaves.pipe(map(remove))
+        );
+
+        const allBranches = branchEvents.pipe(
+            scan(
+                intoMap(([a, b]) => b),
+                new Map<string, [SocketActor, string]>()
+            )
+        );
+
+        const allMessages = allJoins.pipe(
+            flatMap(([actor, branch]) =>
+                actor.messages(branch).pipe(takeWhile(m => m.type !== 'leave'))
+            )
+        );
+
+        // const allBranches = newActors.pipe(
+        //     flatMap(a => a.join.pipe(
+        //         map(add),
+        //         endWith(remove(a)),
+        //     )),
+        // )
+
+        const o = newActors.pipe(
+            // For each new actor,
+            // get the branches that it is joining
+            flatMap(a =>
+                a.join.pipe(
+                    // For each branch to join,
+                    // get the messages from that branch
+                    flatMap(branch =>
+                        a.messages(branch).pipe(
+                            // Take messages until a leave event happens
+                            takeWhile(m => m.type !== 'leave')
+                        )
+                    ),
+
+                    // Take branches until the actor disconnects
+                    takeUntil(a.disconnect)
+                )
+            )
+        );
+
+        // const o = allActors.pipe(
+        //     flatMap(a => a.join.pipe(
+        //         flatMap(branch =>
+        //             a.messages(branch)
+        //         ),
+
+        //         tap(m => {
+
+        //         }),
+
+        //         takeWhile(m => m.type !== 'leave'),
+        //     )
+        // ));
+        // const observable = connections(this._socketServer).pipe(
+        //     flatMap(socket =>
+        //         // For each socket connection,
+        //         // listen for join_branch requests
+        //         .pipe(
+        //             flatMap(branch =>
+        //                 // For each branch request, load the branch
+        //                 // from the store.
+        //                 loadBranchFromStore(
+        //                     this._branches,
+        //                     this._store,
+        //                     branch
+        //                 ).pipe(
+        //                     switchMap(repo =>
+        //                         // When we get a repo,
+        //                         // listen for incoming atoms.
+        //                         merge(
+
+        //                         )
+        //                     ),
+
+        //                     // When we get a leave_branch request,
+        //                     // cancel the subscription.
+        //                     takeWhile(e => e.type !== 'leave_branch'),
+
+        //                     tap(e => {
+        //                         if (e.type === 'incoming_atom') {
+        //                             // Add atom
+        //                         } else if (e.type === 'incoming_event') {
+        //                             // issue remote event
+        //                         }
+        //                     })
+        //                 )
+        //             )
+        //         )
+        //     )
+        // );
     }
 }
 
@@ -102,6 +196,46 @@ type ServerConnection = DeviceConnection<DeviceExtras>;
 interface DeviceExtras {
     socket: Socket;
 }
+
+type Messages = IncomingAtom | IncomingEvent | LeaveEvent;
+
+interface IncomingAtom {
+    type: 'atom';
+    atom: Atom<any>;
+}
+
+interface IncomingEvent {
+    type: 'event';
+    event: RemoteAction;
+}
+
+interface LeaveEvent {
+    type: 'leave';
+}
+
+interface Actor {
+    id: string;
+    join: Observable<string>;
+    disconnect: Observable<any>;
+    messages: (branch: string) => Observable<Messages>;
+}
+
+interface SocketActor extends Actor {
+    socket: Socket;
+}
+
+const socketActor: (socket: Socket) => SocketActor = (socket: Socket) => ({
+    id: socket.id,
+    join: joinBranchRequests(socket),
+    disconnect: disconnect(socket),
+    messages: branch =>
+        merge(
+            incomingAtoms(socket, branch),
+            incomingEvents(socket, branch),
+            leaveBranch(socket, branch)
+        ),
+    socket,
+});
 
 // connections
 // join_branch
@@ -114,13 +248,20 @@ const incomingEvents = (socket: Socket, branch: string) =>
 const leaveBranch = (socket: Socket, branch: string) =>
     leaveBranchRequests(socket, branch).pipe(
         map(_ => ({
-            type: 'leave_branch' as const,
+            type: 'leave' as const,
             branch: branch,
         }))
     );
 
 const connections = (server: Server) =>
     fromEventPattern<Socket>(h => server.on('connection', h));
+
+const disconnect = (socket: Socket) =>
+    fromEventPattern(
+        h => socket.on('disconnect', h),
+        h => socket.off('disconnect', h)
+    );
+
 const joinBranchRequests = (socket: Socket) =>
     socketEvent<string>(socket, 'join_branch');
 const leaveBranchRequests = (socket: Socket, branch: string) =>
@@ -135,7 +276,7 @@ const localAtoms = () => empty();
 const localEvents = () => empty();
 
 const incomingAtom = (atom: Atom<any>) => ({
-    type: 'incoming_atom' as const,
+    type: 'atom' as const,
     atom: atom,
 });
 
@@ -145,7 +286,7 @@ const outgoingAtom = (atom: Atom<any>) => ({
 });
 
 const incomingEvent = (event: RemoteAction) => ({
-    type: 'incoming_event' as const,
+    type: 'event' as const,
     event: event,
 });
 
@@ -191,12 +332,65 @@ function loadBranch(store: CausalRepoStore, branch: string) {
     });
 }
 
-interface IncomingAtom {
-    type: 'incoming_atom';
-    atom: Atom<any>;
+function add<T>(item: T): AddToList<T> {
+    return {
+        type: 'add',
+        item,
+    };
 }
 
-interface OutgoingAtom {
-    type: 'outgoing_atom';
-    atom: Atom<any>;
+function remove<T>(item: T): RemoveFromList<T> {
+    return {
+        type: 'remove',
+        item,
+    };
+}
+
+type ListMessage<T> = AddToList<T> | RemoveFromList<T>;
+
+interface AddToList<T> {
+    type: 'add';
+    item: T;
+}
+
+interface RemoveFromList<T> {
+    type: 'remove';
+    item: T;
+}
+
+function intoList<R, T extends ListMessage<R>>(
+    list: R[],
+    message: T,
+    index: number
+): R[] {
+    if (message.type === 'add') {
+        return [...list, message.item];
+    } else {
+        const index = list.indexOf(message.item);
+        if (index >= 0) {
+            const newList = list.slice();
+            return newList.splice(index, 1);
+        }
+        return list;
+    }
+}
+
+function intoMap<RKey, R, T extends ListMessage<R>>(keyFunc: (val: R) => RKey) {
+    return (map: Map<RKey, R>, message: T, index: number) =>
+        intoMapFull(keyFunc, map, message, index);
+}
+
+function intoMapFull<RKey, R, T extends ListMessage<R>>(
+    keyFunc: (val: R) => RKey,
+    map: Map<RKey, R>,
+    message: T,
+    index: number
+): Map<RKey, R> {
+    const key = keyFunc(message.item);
+    if (message.type === 'add') {
+        map.set(key, message.item);
+    } else {
+        map.delete(key);
+    }
+    return map;
 }
