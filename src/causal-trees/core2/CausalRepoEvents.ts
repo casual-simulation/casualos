f137b05c0329512beb1cd80b71a76780b503ddd2
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-11-18 16:29:57 -0500

    refactor: Namespace all the causal repo events with "repo"

--- src/causal-trees/core2/CausalRepoEvents.ts
+++ src/causal-trees/core2/CausalRepoEvents.ts
@@ -3,59 +3,59 @@ import { Atom } from './Atom2';
 /**
  * The name of the event which starts watching for when branches are loaded/unloaded.
  */
-export const WATCH_BRANCHES = 'watch_branches';
+export const WATCH_BRANCHES = 'repo/watch_branches';
 
 /**
  * The name of the event which stops watching for when branches are loaded/unloaded.
  */
-export const UNWATCH_BRANCHES = 'unwatch_branches';
+export const UNWATCH_BRANCHES = 'repo/unwatch_branches';
 
 /**
  * The name of the event which starts watching changes on a branch.
  * In particular, watches for new atoms.
  */
-export const WATCH_BRANCH = 'watch_branch';
+export const WATCH_BRANCH = 'repo/watch_branch';
 
 /**
  * The name of the event which stops watching changes on a branch.
  */
-export const UNWATCH_BRANCH = 'unwatch_branch';
+export const UNWATCH_BRANCH = 'repo/unwatch_branch';
 
 /**
  * The name of the event which notifies that some atoms were added to a branch.
  */
-export const ADD_ATOMS = 'add_atoms';
+export const ADD_ATOMS = 'repo/add_atoms';
 
 /**
  * The name of the event which notifies that a branch was loaded into server memory.
  */
-export const LOAD_BRANCH = 'load_branch';
+export const LOAD_BRANCH = 'repo/load_branch';
 
 /**
  * The name of the event which notifies that a branch was unloaded from server memory.
  */
-export const UNLOAD_BRANCH = 'unload_branch';
+export const UNLOAD_BRANCH = 'repo/unload_branch';
 
 /**
  * The name of the event which starts watching for connection/disconnection events to the server.
  */
-export const WATCH_DEVICES = 'watch_devices';
+export const WATCH_DEVICES = 'repo/watch_devices';
 
 /**
  * The name of the event which stops watching for connection/disconnection events to the server.
  */
-export const UNWATCH_DEVICES = 'unwatch_devices';
+export const UNWATCH_DEVICES = 'repo/unwatch_devices';
 
 /**
  * The name of the event which notifies that a device became connected to a branch.
  */
-export const DEVICE_CONNECTED_TO_BRANCH = 'device_connected_to_branch';
+export const DEVICE_CONNECTED_TO_BRANCH = 'repo/device_connected_to_branch';
 
 /**
  * The name of the event which notifies that a device become disconnected from a branch.
  */
 export const DEVICE_DISCONNECTED_FROM_BRANCH =
-    'device_disconnected_from_branch';
+    'repo/device_disconnected_from_branch';
 
 /**
  * Defines an event which indicates that atoms should be added for the given branch.
