5d2af2c146769d280184c731e0b349865a766059
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-11-18 12:26:16 -0500

    feat: Add a connection server that merges connections from different connection servers

---
+++ src/causal-tree-server/MultiConnectionServer.spec.ts
@@ -0,0 +1,35 @@
+import {
+    MemoryConnectionServer,
+    MemroyConnection,
+} from './MemoryConnectionServer';
+import { MultiConnectionServer } from './MultiConnectionServer';
+import { Connection } from './ConnectionServer';
+import { waitAsync } from './test/TestHelpers';
+
+describe('MultiConnectionServer', () => {
+    let server1: MemoryConnectionServer;
+    let server2: MemoryConnectionServer;
+    let subject: MultiConnectionServer;
+    let connections: Connection[];
+
+    beforeEach(() => {
+        connections = [];
+        server1 = new MemoryConnectionServer();
+        server2 = new MemoryConnectionServer();
+        subject = new MultiConnectionServer([server1, server2]);
+
+        subject.connection.subscribe(c => connections.push(c));
+    });
+
+    it('should merge connections from different servers', async () => {
+        const conn1 = new MemroyConnection('abc');
+        const conn2 = new MemroyConnection('def');
+
+        server1.connection.next(conn1);
+        server1.connection.next(conn2);
+
+        await waitAsync();
+
+        expect(connections).toEqual([conn1, conn2]);
+    });
+});
