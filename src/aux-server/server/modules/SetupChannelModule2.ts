3d2e092c1b3adad95ffced8f1255c0c0f8972c65
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-11-26 00:40:56 -0500

    feat: Add a new implementation of SetupChannelModule which implements the new Aux Module API

---
+++ src/aux-server/server/modules/SetupChannelModule2.ts
@@ -0,0 +1,133 @@
+import {
+    AuxModule2,
+    AuxChannel,
+    getTreeName,
+    AuxUser,
+    CausalRepoClientPartitionConfig,
+    Simulation,
+} from '@casual-simulation/aux-vm';
+import {
+    USERNAME_CLAIM,
+    RealtimeChannelInfo,
+    DeviceInfo,
+    remote,
+    SESSION_ID_CLAIM,
+    GUEST_ROLE,
+} from '@casual-simulation/causal-trees';
+import { Subscription } from 'rxjs';
+import { flatMap, map } from 'rxjs/operators';
+import {
+    calculateBotValue,
+    getBotRoles,
+    getUserAccountBot,
+    getTokensForUserAccount,
+    findMatchingToken,
+    AuxBot,
+    ShellAction,
+    getChannelBotById,
+    LocalActions,
+    EchoAction,
+    action,
+    SendWebhookAction,
+    BotAction,
+    SetupChannelAction,
+    isBot,
+    createBot,
+    CREATE_ACTION_NAME,
+} from '@casual-simulation/aux-common';
+import {
+    NodeAuxChannel,
+    AuxChannelManager,
+    nodeSimulationForBranch,
+} from '@casual-simulation/aux-vm-node';
+import { sendWebhook } from '../../shared/WebhookUtils';
+import { CausalRepoClient } from '@casual-simulation/causal-trees/core2';
+
+/**
+ * Defines an AuxModule2 that adds setup channel functionality.
+ */
+export class SetupChannelModule2 implements AuxModule2 {
+    private _client: CausalRepoClient;
+    private _user: AuxUser;
+
+    constructor(user: AuxUser, client: CausalRepoClient) {
+        this._user = user;
+        this._client = client;
+    }
+
+    async setup(simulation: Simulation): Promise<Subscription> {
+        let sub = new Subscription();
+
+        sub.add(
+            simulation.localEvents
+                .pipe(
+                    flatMap(async event => {
+                        if (event.type === 'setup_channel') {
+                            await this._setupChannel(event);
+                        }
+                    })
+                )
+                .subscribe()
+        );
+
+        return sub;
+    }
+
+    async deviceConnected(
+        simulation: Simulation,
+        device: DeviceInfo
+    ): Promise<void> {}
+
+    async deviceDisconnected(
+        simulation: Simulation,
+        device: DeviceInfo
+    ): Promise<void> {}
+
+    private async _setupChannel(event: SetupChannelAction) {
+        try {
+            const hasChannel = await this._client
+                .branchInfo(event.channel)
+                .pipe(map(e => e.exists))
+                .toPromise();
+            if (!hasChannel) {
+                console.log(
+                    `[SetupChannelModule2] Setting up new channel ${
+                        event.channel
+                    }`
+                );
+                const simulation = nodeSimulationForBranch(
+                    this._user,
+                    this._client,
+                    event.channel
+                );
+                try {
+                    await simulation.init();
+
+                    if (event.botOrMod) {
+                        console.log(`[SetupChannelModule2] Creating new bot`);
+                        const botId = await simulation.helper.createBot(
+                            undefined,
+                            isBot(event.botOrMod)
+                                ? event.botOrMod.tags
+                                : event.botOrMod
+                        );
+                        console.log(
+                            `[SetupChannelModule2] Created bot ${botId}`
+                        );
+                        await simulation.helper.transaction(
+                            action(
+                                CREATE_ACTION_NAME,
+                                [botId],
+                                simulation.helper.userId
+                            )
+                        );
+                    }
+                } finally {
+                    simulation.unsubscribe();
+                }
+            }
+        } catch (err) {
+            console.error('[SetupChannelModule2]', err);
+        }
+    }
+}
