6f07270f3db5309a5050429509c47b7e84a1c99b
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-12-09 16:34:22 -0500

    chore: Add script to automatically deploy a patch-incremented version

---
+++ release-patch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Step 1: Checkout develop
+
+# Step 2: Replace "TBD" with the current date
+CURRENT_DATE="$(date +%-m\/%-d\/%Y)"
+sed -i '' -e "s!### Date: TBD!### Date: ${CURRENT_DATE}!g" ./CHANGELOG.md
+
+# Step 3: Commit Change
+git add CHANGELOG.md
+git commit -m "chore: Update CHANGELOG Date"
+
+# 4. Merge develop into master
+git checkout master
+git fetch origin
+git reset origin/master --hard
+git merge develop --no-ff
+
+# 5. Run `lerna version` specify patch
+lerna version patch --yes
