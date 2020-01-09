5dda6f1d4cc14aa2951aae8a598ec59136dda990
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-01-09 13:42:53 -0500

    fix: Check for valid URLs with a real URL parser instead of a RegEx

--- src/aux-server/aux-web/shared/SharedUtils.ts
+++ src/aux-server/aux-web/shared/SharedUtils.ts
@@ -69,17 +69,13 @@ export async function copyBotsFromSimulation(
 }
 
 export function isValidURL(str: string) {
-    // Reference: https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-    var pattern = new RegExp(
-        '^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-            '(\\#[-a-z\\d_]*)?$',
-        'i'
-    ); // fragment locator
-    return !!pattern.test(str);
+    try {
+        // Test the URL by making one.
+        const test = new URL(str);
+        return true;
+    } catch {
+        return false;
+    }
 }
 
 /**
