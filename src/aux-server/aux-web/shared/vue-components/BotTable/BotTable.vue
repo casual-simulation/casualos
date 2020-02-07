10b5c8bdcb405a989e9227bd667346d132fe09c6
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-02-07 13:40:30 -0500

    feat: Move the exit sheet button to the top of the bot table

--- src/aux-server/aux-web/shared/vue-components/BotTable/BotTable.vue
+++ src/aux-server/aux-web/shared/vue-components/BotTable/BotTable.vue
@@ -38,6 +38,10 @@
                                 <cube-icon></cube-icon>
                                 <md-tooltip>Create Empty Bot</md-tooltip>
                             </md-button>
+                            <md-button class="md-icon-button exit-sheet" @click="exitSheet()">
+                                <md-icon>exit_to_app</md-icon>
+                                <md-tooltip>{{ exitButtonText }}</md-tooltip>
+                            </md-button>
                         </div>
                     </div>
 
@@ -165,12 +169,6 @@
                 </div>
                 <div class="bot-section-holder-outer" v-if="getTagWhitelist().length > 0">
                     <div class="bot-section-holder-inner">
-                        <div class="bot-section" v-if="dimension">
-                            <md-button class="bot-section active" @click="exitSheet()">
-                                <md-icon>exit_to_app</md-icon>
-                                <md-tooltip>{{ exitButtonText }}</md-tooltip>
-                            </md-button>
-                        </div>
                         <div
                             v-for="(tagWhitelist, index) in getTagWhitelist()"
                             :key="index"
