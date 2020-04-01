451041fb327446fef10026ddc64e90f3d7113bb0
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-04-01 13:55:15 -0400

    feat: Add the math functions to the new library

--- src/aux-common/runtime/AuxLibrary.ts
+++ src/aux-common/runtime/AuxLibrary.ts
@@ -276,6 +276,16 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
                 perform,
                 reject,
             },
+
+            math: {
+                sum,
+                avg,
+                sqrt,
+                abs,
+                stdDev,
+                randomInt,
+                random,
+            },
         },
     };
 
@@ -1364,6 +1374,110 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
     function reject(action: any) {
         const event = calcReject(getOriginalObject(action));
         return addAction(event);
+    }
+
+    /**
+     * Sums the given array of numbers and returns the result.
+     * If any value in the list is not a number, it will be converted to one.
+     * If the given value is not an array, then it will be converted to a number and returned.
+     *
+     * @param list The value that should be summed. If it is a list, then the result will be the sum of the items in the list.
+     *             If it is not a list, then the result will be the value converted to a number.
+     */
+    function sum(list: any): number {
+        if (!Array.isArray(list)) {
+            return parseFloat(list);
+        }
+
+        let carry = 0;
+        for (let i = 0; i < list.length; i++) {
+            const l = list[i];
+            if (!Array.isArray(l)) {
+                carry += parseFloat(l);
+            } else {
+                carry += sum(l);
+            }
+        }
+        return carry;
+    }
+
+    /**
+     * Calculates the average of the numbers in the given list and returns the result.
+     * @param list The value that should be averaged.
+     *             If it is a list, then the result will be sum(list)/list.length.
+     *             If it is not a list, then the result will be the value converted to a number.
+     */
+    function avg(list: any) {
+        if (!Array.isArray(list)) {
+            return parseFloat(list);
+        }
+
+        let total = sum(list);
+        let count = list.length;
+        return total / count;
+    }
+
+    /**
+     * Calculates the square root of the given number.
+     * @param value The number.
+     */
+    function sqrt(value: any) {
+        return Math.sqrt(parseFloat(value));
+    }
+
+    /**
+     * Calculates the absolute value of a number.
+     * @param number The number to get the absolute value of.
+     */
+    function abs(number: any) {
+        return Math.abs(parseFloat(number));
+    }
+
+    /**
+     * Calculates the standard deviation of the numbers in the given list and returns the result.
+     *
+     * @param list The value that the standard deviation should be calculated for.
+     */
+    function stdDev(list: any) {
+        if (!Array.isArray(list)) {
+            list = [parseFloat(list)];
+        }
+
+        let mean = avg(list);
+        let numbersMinusMean = list.map((l: number) => (l - mean) * (l - mean));
+
+        let standardMean = avg(numbersMinusMean);
+        return sqrt(standardMean);
+    }
+
+    /**
+     * Generates a random integer number between min and max.
+     * @param min The smallest allowed value.
+     * @param max The largest allowed value.
+     */
+    function randomInt(min: number = 0, max?: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        const rand = Math.random();
+        if (max) {
+            return Math.floor(rand * (max - min)) + min;
+        } else {
+            return Math.floor(rand) + min;
+        }
+    }
+
+    /**
+     * Generates a random number between min and max.
+     * @param min The smallest allowed value.
+     * @param max The largest allowed value.
+     */
+    function random(min: number = 0, max?: number): number {
+        const rand = Math.random();
+        if (max) {
+            return rand * (max - min) + min;
+        } else {
+            return rand + min;
+        }
     }
 
     // Helpers
