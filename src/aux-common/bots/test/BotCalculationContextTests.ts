21969eaec6fc661d51be2616183a492dbfc36a88
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-05-05 20:41:35 -0400

    refactor: Merge the BotActionsTests and BotCalculationContextTests files

--- src/aux-common/bots/test/BotCalculationContextTests.ts
+++ src/aux-common/bots/test/BotCalculationContextTests.ts
@@ -61,6 +61,7 @@ import {
     isBotFocusable,
     getBotLabelAlignment,
     getBotScaleMode,
+    getActiveObjects,
 } from '../BotCalculations';
 import {
     Bot,
@@ -69,6 +70,7 @@ import {
     GLOBALS_BOT_ID,
     AuxDomain,
     DEFAULT_WORKSPACE_SCALE,
+    BotsState,
 } from '../Bot';
 import { buildLookupTable } from '../BotLookupTable';
 import { BotLookupTableHelper } from '../BotLookupTableHelper';
@@ -79,6 +81,12 @@ import {
     numericalTagValueTests,
     possibleTagNameCases,
 } from './BotTestHelpers';
+import { reject, botRemoved, action, toast } from '../BotEvents';
+import {
+    calculateDestroyBotEvents,
+    resolveRejectedActions,
+} from '../BotActions';
+import { getBotsForAction } from '../BotsChannel';
 
 export function botCalculationContextTests(
     uuidMock: jest.Mock,
@@ -4656,6 +4664,196 @@ export function botCalculationContextTests(
                 expect(getUserBotColor(calc, bot, globals, domain)).toBe('red');
             }
         );
+    });
+
+    describe('calculateDestroyBotEvents()', () => {
+        it('should return a list of events needed to destroy the given bot', () => {
+            const bot1 = createBot('bot1');
+            const bot2 = createBot('bot2', {
+                auxCreator: 'bot1',
+            });
+            const bot3 = createBot('bot3', {
+                auxCreator: 'bot2',
+            });
+            const bot4 = createBot('bot4', {
+                auxCreator: 'bot1',
+            });
+            const bot5 = createBot('bot5');
+
+            const calc = createCalculationContext(
+                [bot1, bot2, bot3, bot4, bot5],
+                undefined
+            );
+            const events = calculateDestroyBotEvents(calc, bot1);
+
+            expect(events).toEqual([
+                botRemoved('bot1'),
+                botRemoved('bot2'),
+                botRemoved('bot3'),
+                botRemoved('bot4'),
+            ]);
+        });
+
+        it('should not return a destroy event for bots that are not destroyable', () => {
+            const bot1 = createBot('bot1');
+            const bot2 = createBot('bot2', {
+                auxCreator: 'bot1',
+                auxDestroyable: false,
+            });
+            const bot3 = createBot('bot3', {
+                auxCreator: 'bot2',
+            });
+            const bot4 = createBot('bot4', {
+                auxCreator: 'bot1',
+            });
+            const bot5 = createBot('bot5');
+
+            const calc = createCalculationContext(
+                [bot1, bot2, bot3, bot4, bot5],
+                undefined
+            );
+            const events = calculateDestroyBotEvents(calc, bot1);
+
+            expect(events).toEqual([
+                botRemoved('bot1'),
+                // bot2 and bot3 are not destroyed because they are not destroyable
+                botRemoved('bot4'),
+            ]);
+        });
+    });
+
+    describe('getBotsForAction()', () => {
+        it('should return the list of bots sorted by ID', () => {
+            const state: BotsState = {
+                thisBot: {
+                    id: 'thisBot',
+                    tags: {},
+                },
+                thatBot: {
+                    id: 'thatBot',
+                    tags: {},
+                },
+            };
+
+            const botAction = action('test', ['thisBot', 'thatBot']);
+            const calc = createCalculationContext(
+                getActiveObjects(state),
+                null
+            );
+            const { bots } = getBotsForAction(botAction, calc);
+
+            expect(bots).toEqual([state['thatBot'], state['thisBot']]);
+        });
+
+        it('should not sort IDs if the action specifies not to', () => {
+            const state: BotsState = {
+                thisBot: {
+                    id: 'thisBot',
+                    tags: {},
+                },
+                thatBot: {
+                    id: 'thatBot',
+                    tags: {},
+                },
+            };
+
+            const botAction = action(
+                'test',
+                ['thisBot', 'thatBot'],
+                undefined,
+                undefined,
+                false
+            );
+            const calc = createCalculationContext(
+                getActiveObjects(state),
+                null
+            );
+            const { bots } = getBotsForAction(botAction, calc);
+
+            expect(bots).toEqual([state['thisBot'], state['thatBot']]);
+        });
+
+        it('should filter out bots which are not in the state', () => {
+            const state: BotsState = {};
+
+            const botAction = action('test', ['badBot']);
+            const calc = createCalculationContext(
+                getActiveObjects(state),
+                null
+            );
+            const { bots } = getBotsForAction(botAction, calc);
+
+            expect(bots).toEqual([]);
+        });
+    });
+
+    describe('resolveRejectedActions()', () => {
+        it('should remove an action if it has been rejected after it was issued', () => {
+            let toastAction = toast('abc');
+            let actions = [toastAction, reject(toastAction)];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([]);
+        });
+
+        it('should keep an action if it has been rejected before it was issued', () => {
+            let toastAction = toast('abc');
+            let actions = [reject(toastAction), toastAction];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([toast('abc')]);
+        });
+
+        it('should be able to remove a rejection', () => {
+            let toastAction = toast('abc');
+            let rejection = reject(toastAction);
+            let actions = [toastAction, rejection, reject(rejection)];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([toast('abc')]);
+        });
+
+        it('should preserve the order of the original actions', () => {
+            let actions = [toast('abc'), toast('def')];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([toast('abc'), toast('def')]);
+        });
+
+        it('should handle rejecting an action twice', () => {
+            let toastAction = toast('abc');
+            let actions = [
+                toastAction,
+                reject(toastAction),
+                reject(toastAction),
+            ];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([]);
+        });
+
+        it('should remove duplicate actions', () => {
+            let toastAction = toast('abc');
+            let actions = [toastAction, toastAction];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([toastAction]);
+        });
+
+        it('should allow an action if it is re-added after it is rejected', () => {
+            let toastAction = toast('abc');
+            let actions = [toastAction, reject(toastAction), toastAction];
+
+            const final = resolveRejectedActions(actions);
+
+            expect(final).toEqual([toastAction]);
+        });
     });
 
     describe('BotLookupTable', () => {
