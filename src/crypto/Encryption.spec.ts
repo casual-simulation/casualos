401e56e067c9d1e2c56f32486ef6f08f3528294d
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-07-17 17:33:51 -0400

    feat: Add implementations of version 1 for encryption and decryption

--- src/crypto/Encryption.spec.ts
+++ src/crypto/Encryption.spec.ts
@@ -1,3 +1,93 @@
+import { encryptV1, decryptV1 } from './Encryption';
+
 describe('Encryption', () => {
-    it('should return');
+    const passwordCases = [[null], [undefined], ['']];
+
+    describe('encryptV1()', () => {
+        it('should encrypt the data with the given password', async () => {
+            const data = 'hello, world!';
+            const encoder = new TextEncoder();
+            const dataBytes = encoder.encode(data);
+            const cyphertext = await encryptV1('testPassword', dataBytes);
+            expect(cyphertext).not.toContain(data);
+        });
+
+        const differentCases = [
+            [
+                'should encrypt the same data with the same passwords differently',
+                'hello, world!',
+                'testPassword',
+                'testPassword',
+            ],
+            [
+                'should encrypt the same data with different passwords differently',
+                'hello, world!',
+                'testPassword',
+                'testPassword1',
+            ],
+            [
+                'should encrypt an empty string with the same password differently',
+                '',
+                'testPassword',
+                'testPassword',
+            ],
+            [
+                'should encrypt an empty string with different passwords differently',
+                '',
+                'testPassword',
+                'testPassword1',
+            ],
+        ];
+
+        it.each(differentCases)(
+            '%s',
+            async (desc, data, password1, password2) => {
+                const encoder = new TextEncoder();
+                const dataBytes = encoder.encode(data);
+                const cyphertext1 = await encryptV1(password1, dataBytes);
+                const cyphertext2 = await encryptV1(password2, dataBytes);
+                const [version1, salt1, nonce1, data1] = cyphertext1.split('.');
+                const [version2, salt2, nonce2, data2] = cyphertext2.split('.');
+
+                expect(data1).not.toEqual(data2);
+            }
+        );
+
+        it.each(passwordCases)('should reject %s passwords', async password => {
+            const data = 'test';
+            const encoder = new TextEncoder();
+            const dataBytes = encoder.encode(data);
+            await expect(encryptV1(password, dataBytes)).rejects.toEqual(
+                expect.any(Error)
+            );
+        });
+    });
+
+    describe('decryptV1()', () => {
+        it('should be able to decrypt the data returned from encryptV1()', async () => {
+            const data = 'hello, world!';
+            const encoder = new TextEncoder();
+            const dataBytes = encoder.encode(data);
+            const cyphertext = await encryptV1('testPassword', dataBytes);
+            const plaintext = await decryptV1('testPassword', cyphertext);
+
+            const decoder = new TextDecoder();
+            const final = decoder.decode(plaintext);
+            expect(final).toEqual(data);
+        });
+
+        it('should return null when trying to decrypt data that was corrupted', async () => {
+            const data = 'hello, world!';
+            const encoder = new TextEncoder();
+            const dataBytes = encoder.encode(data);
+            let cyphertext = await encryptV1('testPassword', dataBytes);
+
+            // Replace the last character of the data with a 0 (or a 1 if it was already a 0)
+            let corrupted = cyphertext.slice(0, cyphertext.length - 1);
+            corrupted += cyphertext[cyphertext.length - 1] === '0' ? '1' : '0';
+
+            const decrypted = await decryptV1('testPassword', corrupted);
+            expect(decrypted).toBe(null);
+        });
+    });
 });
--- src/crypto/Encryption.ts
+++ src/crypto/Encryption.ts
@@ -1,14 +1,16 @@
 import { randomBytes, secretbox } from 'tweetnacl';
-import scrypt, { ScryptOptions } from 'scrypt-async';
+import scrypt, { Options } from 'scrypt-async';
 import { fromByteArray, toByteArray } from 'base64-js';
 
 function scryptAsync(
     password: string | Uint8Array,
     salt: string | Uint8Array,
-    options: ScryptOptions
+    options: Options
 ): Promise<string | Uint8Array | Array<number>> {
     return new Promise((resolve, reject) => {
-        scrypt(password, salt, options, result => resolve(result));
+        scrypt(<any>password, <any>salt, options, (result: any) =>
+            resolve(result)
+        );
     });
 }
 
@@ -20,7 +22,7 @@ interface DerivedKey {
 const ITERATIONS = 16384;
 const BLOCK_SIZE = 8;
 const PARALLELISM = 1;
-const KEY_LENGTH = 16;
+const KEY_LENGTH = secretbox.keyLength;
 
 async function deriveKey(
     password: string,
@@ -41,31 +43,114 @@ async function deriveKey(
 }
 
 /**
- * Encrypts the given data with the given password and returns the resulting cyphertext as a base64 string.
+ * Encrypts the given data with the given password and returns the resulting cyphertext.
  *
- * Based on https://github.com/dchest/tweetnacl-js
- * Uses XSalsa20 and Poly1305 for authenticated encryption.
+ * The returned cyphertext contains a version number at the beginning which determines the format of the following data.
  *
- * Always uses a new initialization vector.
+ * v1 encryptions use XSalsa20 as the cypher and Poly1305 for authentication in addition to scrypt for password-based key derivation.
+ * The output string is formatted as following with periods between the components:
+ * 1. The version number (v1)
+ * 2. The base64 of the salt used to derive the key from the password. (pseudorandom)
+ * 3. The base64 of the nonce used by the cypher. (pseudorandom)
+ * 4. The base64 of the encrypted data.
+ *
  * @param password The password to use to encrypt.
  * @param data The data to encrypt.
  */
-export async function encrypt(password: string, data: string): Promise<string> {
-    const nonce = randomBytes(secretbox.nonceLength);
-
-    // Use the nonce as the salt of the derived key.
-    // This should be fine since both salts and nonces are
-    // supposed to be random unguessable values.
-    const key = await deriveKey(password, nonce);
-
-    const decoder = new TextEncoder();
-    const dataBytes = decoder.encode(data);
-    const cypherBytes = secretbox(dataBytes, nonce, key.hash);
-
-    // const cypher
+export function encrypt(password: string, data: Uint8Array): Promise<string> {
+    return encryptV1(password, data);
 }
 
-export async function decrypt(
+// export async function decrypt(
+//     password: string,
+//     cyphertext: string
+// ): Promise<string> {
+
+// }
+
+/**
+ * Encrypts the given data with the given password using version 1 of the encryption mechanisms in this file and returns the resulting
+ * cyphertext.
+ *
+ * version 1 encryptions use XSalsa20 as the cypher and Poly1305 for authentication in addition to scrypt for password-based key derivation.
+ * The output string is formatted as following with periods between the components:
+ * 1. The version number (v1)
+ * 2. The base64 of the salt used to derive the key from the password. (pseudorandom)
+ * 3. The base64 of the nonce used by the cypher. (pseudorandom)
+ * 4. The base64 of the encrypted data.
+ *
+ * @param password The password to use to encrypt the data.
+ * @param data The data to encrypt.
+ */
+export async function encryptV1(
+    password: string,
+    data: Uint8Array
+): Promise<string> {
+    if (!password) {
+        throw new Error('Invalid password. Must not be null or undefined.');
+    }
+    if (!data) {
+        throw new Error('Invalid data. Must not be null or undefined.');
+    }
+    const nonce = randomBytes(secretbox.nonceLength);
+    const salt = randomBytes(secretbox.nonceLength);
+
+    const key = await deriveKey(password, salt);
+    const cypherBytes = secretbox(data, nonce, key.hash);
+    const cyphertext = `v1.${fromByteArray(salt)}.${fromByteArray(
+        nonce
+    )}.${fromByteArray(cypherBytes)}`;
+
+    return cyphertext;
+}
+
+/**
+ * Decrypts the given data with the given password using version 1 of the encryption mechanisms in this file and returns the resulting
+ * plaintext.
+ *
+ * version 1 encryptions use XSalsa20 as the cypher and Poly1305 for authentication in addition to scrypt for password-based key derivation.
+ * The output string is formatted as following with periods between the components:
+ * 1. The version number (v1)
+ * 2. The base64 of the salt used to derive the key from the password. (pseudorandom)
+ * 3. The base64 of the nonce used by the cypher. (pseudorandom)
+ * 4. The base64 of the encrypted data.
+ *
+ * @param password The password to use to decrypt the data.
+ * @param cyphertext The cyphertext produced from encryptV1().
+ */
+export async function decryptV1(
     password: string,
     cyphertext: string
-): Promise<string> {}
+): Promise<Uint8Array> {
+    if (!password) {
+        throw new Error('Invalid password. Must not be null or undefined.');
+    }
+    if (!cyphertext) {
+        throw new Error('Invalid cyphertext. Must not be null or undefined.');
+    }
+    if (!cyphertext.startsWith('v1.')) {
+        throw new Error('Invalid cyphertext. Must start with "v1."');
+    }
+
+    const withoutVersion = cyphertext.slice('v1.'.length);
+    let nextPeriod = withoutVersion.indexOf('.');
+    if (nextPeriod < 0) {
+        throw new Error('Invalid cyphertext. Must contain a salt.');
+    }
+    const saltBase64 = withoutVersion.slice(0, nextPeriod);
+    const withoutSalt = withoutVersion.slice(nextPeriod + 1);
+    nextPeriod = withoutSalt.indexOf('.');
+    if (nextPeriod < 0) {
+        throw new Error('Invalid cyphertext. Must contain a nonce.');
+    }
+    const nonceBase64 = withoutSalt.slice(0, nextPeriod);
+    const dataBase64 = withoutSalt.slice(nextPeriod + 1);
+
+    const salt = toByteArray(saltBase64);
+    const nonce = toByteArray(nonceBase64);
+    const data = toByteArray(dataBase64);
+
+    const key = await deriveKey(password, salt);
+
+    return secretbox.open(data, nonce, key.hash);
+}
