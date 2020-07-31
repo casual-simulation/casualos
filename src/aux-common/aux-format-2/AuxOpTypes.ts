da6a623db9e56415394dc7394677164730ab497e
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-07-31 16:52:34 -0400

    feat: Add op types for certificates, signatures, and revocations

--- src/aux-common/aux-format-2/AuxOpTypes.ts
+++ src/aux-common/aux-format-2/AuxOpTypes.ts
@@ -1,4 +1,5 @@
 import assign from 'lodash/assign';
+import { AtomId } from '@casual-simulation/causal-trees';
 
 /**
  * The list of operation types.
@@ -10,12 +11,23 @@ export enum AuxOpType {
     value = 3,
     delete = 4,
     insert = 5,
+    certificate = 6,
+    revocation = 7,
+    signature = 8,
 }
 
 /**
  * Defines a union of all the possible op types.
  */
-export type AuxOp = BotOp | TagOp | ValueOp | InsertOp | DeleteOp;
+export type AuxOp =
+    | BotOp
+    | TagOp
+    | ValueOp
+    | InsertOp
+    | DeleteOp
+    | CertificateOp
+    | SignatureOp
+    | RevocationOp;
 
 /**
  * Defines an interface for all the AUX atom values.
@@ -107,6 +119,73 @@ export interface DeleteOp extends AuxOpBase {
 }
 
 /**
+ * Defines an atom value that instructs the system to create a certificate. (See https://en.wikipedia.org/wiki/Public_key_certificate)
+ * Certificates create a chain of trust (https://en.wikipedia.org/wiki/Chain_of_trust) that can be used to validate that specific tag values have been created by a particular certificate.
+ */
+export interface CertificateOp extends AuxOpBase {
+    type: AuxOpType.certificate;
+
+    /**
+     * The keypair that is stored in the certificate.
+     * Stores both the public and private keys in the format specified in
+     * the crypto package.
+     * The public key is unencrypted and available for anyone to use
+     * while the private key is encrypted with a password.
+     */
+    keypair: string;
+
+    /**
+     * The signature that is applied to this certificate.
+     * Must be from the atom's cause.
+     */
+    signature: string;
+}
+
+/**
+ * Defines an atom value that represents a signature applied to a value.
+ */
+export interface SignatureOp extends AuxOpBase {
+    type: AuxOpType.signature;
+
+    /**
+     * The ID of the atom that created this signature.
+     */
+    certId: AtomId;
+
+    /**
+     * The hash of the atom that created this signature.
+     */
+    certHash: string;
+
+    /**
+     * The signature data that was created by the certificate.
+     */
+    signature: string;
+}
+
+/**
+ * Defines an atom value that represents the revocation of a certificate.
+ */
+export interface RevocationOp extends AuxOpBase {
+    type: AuxOpType.revocation;
+
+    /**
+     * The ID of the certificate that created this revocation.
+     */
+    certId: AtomId;
+
+    /**
+     * The hash of the certificate that created this revocation.
+     */
+    certHash: string;
+
+    /**
+     * The signature data that was created by the certificate.
+     */
+    signature: string;
+}
+
+/**
  * Creates a bot atom op.
  */
 export function bot(id: string): BotOp {
@@ -154,6 +233,54 @@ export function del(start?: number, end?: number): DeleteOp {
     return op<DeleteOp>(AuxOpType.delete, {
         start,
         end,
+    });
+}
+
+/**
+ * Creates a certificate op.
+ * @param keypair The keypair for the certificate.
+ * @param signature The signature for the certificate.
+ */
+export function cert(keypair: string, signature?: string): CertificateOp {
+    return op<CertificateOp>(AuxOpType.certificate, {
+        keypair,
+        signature: signature || null,
+    });
+}
+
+/**
+ * Creates a signature op.
+ * @param certId The ID of the cert that created the signature.
+ * @param certHash The hash of the certificate.
+ * @param signature The signature.
+ */
+export function sig(
+    certId: AtomId,
+    certHash: string,
+    signature: string
+): SignatureOp {
+    return op<SignatureOp>(AuxOpType.signature, {
+        certId,
+        certHash,
+        signature,
+    });
+}
+
+/**
+ * Creates a revocation op.
+ * @param certId The ID of the cert that created the signature.
+ * @param certHash The hash of the certificate.
+ * @param signature The signature.
+ */
+export function revocation(
+    certId: AtomId,
+    certHash: string,
+    signature: string
+): RevocationOp {
+    return op<RevocationOp>(AuxOpType.revocation, {
+        certId,
+        certHash,
+        signature,
     });
 }
 
