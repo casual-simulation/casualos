4fd0f6c56858da4523865e280bf59fee82dc2161
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-11-04 22:43:15 -0500

    fix: Make the next_version.sh and release-patch.sh scripts work no matter where they are executed from

--- script/next_version.sh
+++ script/next_version.sh
@@ -1,8 +1,10 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 CURRENT_VERSION_MARKUP=`awk '
     BEGIN {f=0}
     /^\#\# V/{f+=1}
     f == 1 { print };
-' CHANGELOG.md`
+' "$SCRIPTPATH/../CHANGELOG.md"`
 
 CURRENT_VERSION=$(echo "${CURRENT_VERSION_MARKUP}" | awk '/\#\# V/ {print $2}' | cut -c 2-10000)
 BUILD_NUMBER="$1"
--- script/release-patch.sh
+++ script/release-patch.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 TARGET_BRANCH="$1"
 BUILD_NUMBER="$2"
 
@@ -11,10 +12,10 @@ git reset origin/develop --hard
 
 # Step 2: Replace "TBD" with the current date
 CURRENT_DATE="$(date +%-m\/%-d\/%Y)"
-sed -i '' -e "s!#### Date: TBD!#### Date: ${CURRENT_DATE}!g" ../CHANGELOG.md
+sed -i '' -e "s!#### Date: TBD!#### Date: ${CURRENT_DATE}!g" "${SCRIPTPATH}/../CHANGELOG.md"
 
 # Step 3: Commit Change
-git add CHANGELOG.md
+git add "${SCRIPTPATH}/../CHANGELOG.md"
 git commit -m "chore: Update CHANGELOG Date"
 
 # 4. Merge develop into the target branch
