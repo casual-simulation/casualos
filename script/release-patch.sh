6e55688465796e3bbc37d8faa0589ffe8e1e574d
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-11-04 22:21:56 -0500

    chore: Add a couple scripts to handle creating a release for master and a release for staging

--- script/release-patch.sh
+++ script/release-patch.sh
@@ -2,26 +2,28 @@
 
 set -e
 
+TARGET_BRANCH="$1"
+
 # Step 1: Checkout develop
 git checkout develop
 git reset origin/develop --hard
 
 # Step 2: Replace "TBD" with the current date
 CURRENT_DATE="$(date +%-m\/%-d\/%Y)"
-sed -i '' -e "s!#### Date: TBD!#### Date: ${CURRENT_DATE}!g" ./CHANGELOG.md
+sed -i '' -e "s!#### Date: TBD!#### Date: ${CURRENT_DATE}!g" ../CHANGELOG.md
 
 # Step 3: Commit Change
 git add CHANGELOG.md
 git commit -m "chore: Update CHANGELOG Date"
 
-# 4. Merge develop into master
-git checkout master
+# 4. Merge develop into the target branch
+git checkout "$TARGET_BRANCH"
 git fetch origin
-git reset origin/master --hard
+git reset "origin/$TARGET_BRANCH" --hard
 git merge develop --no-ff
 
 # 5. Get version
-VERSION=$(./script/next_version.sh)
+VERSION=$(./next_version.sh)
 
 # 6/ Run `lerna version` specify patch
 lerna version "$VERSION" --yes --no-push
