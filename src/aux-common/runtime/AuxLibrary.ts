19a820d83effc7b482b6fc7ac4ba384a7fbeedb9
Wesley <wesley@yeticgi.com>
2020-11-12 10:33:02 -0500

    feat: added serialFlush and serialDrain

--- src/aux-common/runtime/AuxLibrary.ts
+++ src/aux-common/runtime/AuxLibrary.ts
@@ -137,6 +137,8 @@ import {
     serialWritePin,
     serialReadPin,
     serialClosePin,
+    serialFlushPin,
+    serialDrainPin,
     serialPausePin,
     serialResumePin,
     Easing,
@@ -343,11 +345,7 @@ export interface TweenOptions {
  * @param context The global context that should be used.
  */
 export function createDefaultLibrary(context: AuxGlobalContext) {
-    webhook.post = function (
-        url: string,
-        data?: any,
-        options?: WebhookOptions
-    ) {
+    webhook.post = function(url: string, data?: any, options?: WebhookOptions) {
         return webhook({
             ...options,
             method: 'POST',
@@ -513,6 +511,8 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
                 serialWrite,
                 serialRead,
                 serialClose,
+                serialFlush,
+                serialDrain,
                 serialPause,
                 serialResume,
                 shell,
@@ -591,13 +591,11 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
      */
     function getBots(...args: any[]): RuntimeBot[] {
         if (args.length > 0 && typeof args[0] === 'function') {
-            const filtered = context.bots.filter((b) =>
-                args.every((f) => f(b))
-            );
+            const filtered = context.bots.filter(b => args.every(f => f(b)));
 
             const sortFuncs = args
-                .filter((f) => typeof f.sort === 'function')
-                .map((f) => f.sort);
+                .filter(f => typeof f.sort === 'function')
+                .map(f => f.sort);
             const sorted =
                 sortFuncs.length > 0
                     ? sortBy(filtered, ...sortFuncs)
@@ -617,12 +615,12 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
 
         if (hasValue(filter)) {
             if (typeof filter === 'function') {
-                return context.bots.filter((b) => filter(b.tags[tag]));
+                return context.bots.filter(b => filter(b.tags[tag]));
             } else {
-                return context.bots.filter((b) => b.tags[tag] === filter);
+                return context.bots.filter(b => b.tags[tag] === filter);
             }
         } else {
-            return context.bots.filter((b) => hasValue(b.tags[tag]));
+            return context.bots.filter(b => hasValue(b.tags[tag]));
         }
     }
 
@@ -645,13 +643,13 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
      */
     function getBotTagValues(tag: string, filter?: TagFilter): any[] {
         const values = context.bots
-            .map((b) => getTag(b, tag))
-            .filter((t) => hasValue(t));
+            .map(b => getTag(b, tag))
+            .filter(t => hasValue(t));
         if (hasValue(filter)) {
             if (typeof filter === 'function') {
-                return values.filter((val) => filter(val));
+                return values.filter(val => filter(val));
             } else {
-                return values.filter((val) => val === filter);
+                return values.filter(val => val === filter);
             }
         } else {
             return values;
@@ -721,22 +719,22 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
     function byTag(tag: string, filter?: TagFilter): BotFilterFunction {
         tag = trimTag(tag);
         if (filter && typeof filter === 'function') {
-            return (bot) => {
+            return bot => {
                 let val = bot.tags[tag];
                 return hasValue(val) && filter(val);
             };
         } else if (hasValue(filter)) {
-            return (bot) => {
+            return bot => {
                 let val = bot.tags[tag];
                 return hasValue(val) && filter === val;
             };
         } else if (filter === null) {
-            return (bot) => {
+            return bot => {
                 let val = bot.tags[tag];
                 return !hasValue(val);
             };
         } else {
-            return (bot) => {
+            return bot => {
                 let val = bot.tags[tag];
                 return hasValue(val);
             };
@@ -756,8 +754,8 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
      */
     function byMod(mod: Mod): BotFilterFunction {
         let tags = isBot(mod) ? mod.tags : mod;
-        let filters = Object.keys(tags).map((k) => byTag(k, tags[k]));
-        return (bot) => filters.every((f) => f(bot));
+        let filters = Object.keys(tags).map(k => byTag(k, tags[k]));
+        return bot => filters.every(f => f(bot));
     }
 
     /**
@@ -792,8 +790,8 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
         const inCtx = inDimension(dimension);
         const atX = byTag(`${dimension}X`, x);
         const atY = byTag(`${dimension}Y`, y);
-        const filter: BotFilterFunction = (b) => inCtx(b) && atX(b) && atY(b);
-        filter.sort = (b) => getTag(b, `${dimension}SortOrder`) || 0;
+        const filter: BotFilterFunction = b => inCtx(b) && atX(b) && atY(b);
+        filter.sort = b => getTag(b, `${dimension}SortOrder`) || 0;
         return filter;
     }
 
@@ -895,7 +893,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
      * );
      */
     function either(...filters: BotFilterFunction[]): BotFilterFunction {
-        return (bot) => filters.some((f) => f(bot));
+        return bot => filters.some(f => f(bot));
     }
 
     /**
@@ -907,7 +905,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
      * let bots = getBots(not(inDimension("test")));
      */
     function not(filter: BotFilterFunction): BotFilterFunction {
-        return (bot) => !filter(bot);
+        return bot => !filter(bot);
     }
 
     /**
@@ -1392,7 +1390,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
         if (getTag(bot, dimension) === true) {
             return 0;
         } else if (
-            KNOWN_PORTALS.some((portal) => getTag(bot, portal) === dimension)
+            KNOWN_PORTALS.some(portal => getTag(bot, portal) === dimension)
         ) {
             return 1;
         }
@@ -1582,7 +1580,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
         if (!hasValue(inventoryDimension)) {
             return false;
         }
-        return every(bots, (f) => getTag(f, inventoryDimension) === true);
+        return every(bots, f => getTag(f, inventoryDimension) === true);
     }
 
     /**
@@ -2375,6 +2373,34 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
     }
 
     /**
+     * Flush discards data that has been received but not read, or written but not transmitted by the operating system.
+     */
+    function serialFlush() {
+        const task = context.createTask(true, true);
+        const event = calcRemote(
+            serialFlushPin(),
+            undefined,
+            undefined,
+            task.taskId
+        );
+        return addAsyncAction(task, event);
+    }
+
+    /**
+     * Waits until all output data is transmitted to the serial port. After any pending write has completed, it calls `tcdrain()` or `FlushFileBuffers()` to ensure it has been written to the device.
+     */
+    function serialDrain() {
+        const task = context.createTask(true, true);
+        const event = calcRemote(
+            serialDrainPin(),
+            undefined,
+            undefined,
+            task.taskId
+        );
+        return addAsyncAction(task, event);
+    }
+
+    /**
      * Causes a stream in flowing mode to stop emitting 'data' events, switching out of flowing mode. Any data that becomes available remains in the internal buffer.
      */
     function serialPause() {
@@ -2770,7 +2796,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
      * @param time The Time to sleep in ms. 1 second is 1000 ms.
      */
     function sleep(time: number) {
-        let sleepy = new Promise((resolve) => setTimeout(resolve, time));
+        let sleepy = new Promise(resolve => setTimeout(resolve, time));
         return sleepy;
     }
 
@@ -3657,7 +3683,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
             }
         }
 
-        let bots: Bot[] = variants.map((v) => {
+        let bots: Bot[] = variants.map(v => {
             let bot: Bot = {
                 id: idFactory(),
                 tags: {},
@@ -3727,7 +3753,7 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
         bot: RuntimeBot | string | Bot | (RuntimeBot | string | Bot)[]
     ) {
         if (typeof bot === 'object' && Array.isArray(bot)) {
-            bot.forEach((f) => destroyBot(f));
+            bot.forEach(f => destroyBot(f));
         } else {
             destroyBot(bot);
         }
@@ -4049,14 +4075,14 @@ export function createDefaultLibrary(context: AuxGlobalContext) {
         sendListenEvents: boolean = true
     ) {
         let ids = !!bots
-            ? bots.map((bot) => {
+            ? bots.map(bot => {
                   return !!bot
                       ? typeof bot === 'string'
                           ? bot
                           : bot.id
                       : null;
               })
-            : context.bots.map((b) => b.id);
+            : context.bots.map(b => b.id);
 
         let results = [] as any[];
         let tag = trimEvent(name);
