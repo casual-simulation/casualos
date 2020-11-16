62e792396ff68a9fa6c67e889bb673580485905b
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-11-16 16:48:57 -0500

    feat: Add a basic handler for encoding and decoding messages in a AWS-specific way
    - Required because AWS API Gateway limits all WebSocket messages to 128KB and requires that binary messages be base64 encoded.
    - Solution that we have taken is to use a binary message protocol to split the message into separate packets because text protocols do poorly at this kind of arbitrary splitting.
    - But we also have to convert data to/from base64 so that is what the handler does.
    - It also handles the math to ensure that the base64 encoded data does not go over the size limit.

---
+++ src/causal-tree-client-apiary/AwsSocketHandler.spec.ts
@@ -0,0 +1,61 @@
+import { before } from 'lodash';
+import { AwsSocketHandler } from './AwsSocketHandler';
+
+describe('AwsSocketHandler', () => {
+    describe('encode()', () => {
+        let handler: AwsSocketHandler;
+
+        beforeEach(() => {
+            handler = new AwsSocketHandler(1_000);
+        });
+
+        it('should encode the given data', () => {
+            const messages = handler.encode('abcdefghijk', 0);
+            expect(messages).toHaveLength(1);
+            expect(messages[0]).not.toEqual('');
+        });
+    });
+
+    describe('handleMessage()', () => {
+        let handler: AwsSocketHandler;
+
+        beforeEach(() => {
+            handler = new AwsSocketHandler(30);
+        });
+
+        it('should be able to decode previously encoded data', () => {
+            const messages = handler.encode('abcdef', 0);
+
+            const event = handler.handleMessage({
+                data: messages[0],
+            });
+
+            expect(event).toEqual({
+                data: 'abcdef',
+            });
+        });
+
+        it('should be able to decode data that was encoded into multiple messages', () => {
+            let str = 'abcdefghijklmnopqrstuvwxyz';
+            const data = str.repeat(10);
+            const messages = handler.encode(data, 0);
+
+            expect(messages.every((m) => m.length <= 30)).toBe(true);
+
+            for (let message of messages.slice(0, messages.length - 1)) {
+                const event = handler.handleMessage({
+                    data: message,
+                });
+                expect(event).toBe(null);
+            }
+
+            const event = handler.handleMessage({
+                data: messages[messages.length - 1],
+            });
+
+            expect(event).toEqual({
+                data: data,
+            });
+        });
+    });
+});
