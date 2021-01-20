ae3c7265416504dd0f7b5af661293c2bbdb58f55
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-01-19 21:37:00 -0500

    feat: Add the ability to do some basic bundling of entrypoints

--- src/aux-vm/vm/PortalBundler.spec.ts
+++ src/aux-vm/vm/PortalBundler.spec.ts
@@ -1,3 +1,10 @@
+import {
+    createBot,
+    createPrecalculatedBot,
+    stateUpdatedEvent,
+} from '@casual-simulation/aux-common';
+import { waitAsync } from '@casual-simulation/aux-common/test/TestHelpers';
+import { Subscription } from 'rxjs';
 import { Bundle, PortalBundler } from './PortalBundler';
 
 describe('PortalBundler', () => {
@@ -9,9 +16,121 @@ describe('PortalBundler', () => {
 
     describe('onBundleUpdated', () => {
         let bundles: Bundle[];
+        let sub: Subscription;
+        let func1: jest.Mock<any>;
+        let func2: jest.Mock<any>;
 
         beforeEach(() => {
             bundles = [];
+            (<any>globalThis).func1 = func1 = jest.fn();
+            (<any>globalThis).func2 = func2 = jest.fn();
+            sub = bundler.onBundleUpdated.subscribe((b) => {
+                bundles.push(b);
+            });
+        });
+
+        afterEach(() => {
+            sub.unsubscribe();
+            delete (<any>globalThis).func2;
+            delete (<any>globalThis).func1;
+        });
+
+        describe('stateUpdated()', () => {
+            it('should emit a bundle containing the code of the specified tags', async () => {
+                bundler.registerCustomPortal('test');
+
+                bundler.addEntryPoint('test', { tag: '📖main' });
+
+                bundler.stateUpdated(
+                    stateUpdatedEvent({
+                        bot1: createPrecalculatedBot('bot1', {
+                            main: '📖console.log("abc")',
+                        }),
+                        bot2: createPrecalculatedBot('bot2', {
+                            main: '📖console.log("def")',
+                        }),
+                    })
+                );
+
+                await waitAsync();
+
+                expect(bundles.length).toBe(1);
+                expect(bundles).toMatchInlineSnapshot(`
+                    Array [
+                      Object {
+                        "portalId": "test",
+                        "source": "(function () {
+                    	'use strict';
+
+                    	console.log(\\"abc\\");
+
+                    	console.log(\\"def\\");
+
+                    }());
+                    ",
+                      },
+                    ]
+                `);
+            });
+
+            it('should execute entry points in bot ID alphabetical order', async () => {
+                bundler.registerCustomPortal('test');
+
+                bundler.addEntryPoint('test', { tag: '📖main' });
+
+                bundler.stateUpdated(
+                    stateUpdatedEvent({
+                        def: createPrecalculatedBot('def', {
+                            main: '📖globalThis.func1("second");',
+                        }),
+                        abc: createPrecalculatedBot('abc', {
+                            main: '📖globalThis.func1("first");',
+                        }),
+                    })
+                );
+
+                await waitAsync();
+
+                expect(bundles.length).toBe(1);
+
+                eval(bundles[0].source);
+
+                expect(func1).toBeCalledTimes(2);
+                expect(func1).toHaveBeenNthCalledWith(1, 'first');
+                expect(func1).toHaveBeenNthCalledWith(2, 'second');
+            });
+
+            it('should use scripts from previous states', async () => {
+                bundler.stateUpdated(
+                    stateUpdatedEvent({
+                        abc: createPrecalculatedBot('abc', {
+                            main: '📖globalThis.func1("first");',
+                        }),
+                    })
+                );
+
+                bundler.registerCustomPortal('test');
+
+                bundler.addEntryPoint('test', { tag: '📖main' });
+
+                bundler.stateUpdated(
+                    stateUpdatedEvent({
+                        def: createPrecalculatedBot('def', {
+                            main: '📖globalThis.func1("second");',
+                        }),
+                    })
+                );
+
+                await waitAsync();
+
+                expect(bundles.length).toBe(1);
+
+                eval(bundles[0].source);
+
+                expect(func1).toBeCalledTimes(2);
+                expect(func1).toHaveBeenNthCalledWith(1, 'first');
+                expect(func1).toHaveBeenNthCalledWith(2, 'second');
+            });
         });
     });
 });
--- src/aux-vm/vm/PortalBundler.ts
+++ src/aux-vm/vm/PortalBundler.ts
@@ -1,5 +1,13 @@
-import { StateUpdatedEvent } from '@casual-simulation/aux-common';
+import {
+    applyUpdates,
+    BotsState,
+    PrecalculatedBotsState,
+    StateUpdatedEvent,
+} from '@casual-simulation/aux-common';
 import { Observable, Subject } from 'rxjs';
+import { rollup } from 'rollup';
+import values from 'lodash/values';
+import { sortBy } from 'lodash';
 
 export interface PortalEntrypoint {
     botId?: string;
@@ -7,7 +15,13 @@ export interface PortalEntrypoint {
 }
 
 export interface Bundle {
+    portalId: string;
     source: string;
+}
+
+export interface Portal {
+    portalId: string;
+    entrypoints: PortalEntrypoint[];
 }
 
 /**
@@ -15,6 +29,8 @@ export interface Bundle {
  * It listens for state updates and is able to asynchrounously emit bundles that should be injected into custom portals.
  */
 export class PortalBundler {
+    private _portals: Map<string, Portal>;
+    private _state: PrecalculatedBotsState;
     private _onBundleUpdated: Subject<Bundle>;
 
     /**
@@ -25,24 +41,143 @@ export class PortalBundler {
     }
 
     constructor() {
+        this._portals = new Map();
         this._onBundleUpdated = new Subject();
     }
 
     /**
      * Processes the given state update event.
      */
-    stateUpdated(event: StateUpdatedEvent) {}
+    stateUpdated(event: StateUpdatedEvent) {
+        this._state = applyUpdates(this._state, event);
+
+        for (let portal of this._portals.values()) {
+            let hasEntrypointUpdate = false;
+            for (let id in event.state) {
+                const updatedBot = event.state[id];
+                hasEntrypointUpdate = portal.entrypoints.some(
+                    (e) => e.tag in updatedBot.values
+                );
+
+                if (hasEntrypointUpdate) {
+                    break;
+                }
+            }
+
+            if (hasEntrypointUpdate) {
+                let modules = new Map<string, string>();
+                let entryCode = '';
+                let bots = sortBy(values(this._state), (b) => b.id);
+
+                for (let entrypoint of portal.entrypoints) {
+                    let tagModules = bots
+                        .map((b) => ({
+                            name: `${b.id}-${entrypoint.tag}`,
+                            code: b.values[entrypoint.tag],
+                        }))
+                        .filter((value) => isEntrypointTag(value.code))
+                        .map((m) => ({
+                            ...m,
+                            code: trimEntrypointTag(m.code),
+                        }));
+
+                    for (let m of tagModules) {
+                        modules.set(m.name, m.code);
+                    }
+                }
+
+                for (let [name, code] of modules) {
+                    entryCode += `import ${JSON.stringify(name)};\n`;
+                }
+
+                // bundle it
+                rollup({
+                    input: 'main',
+                    plugins: [
+                        {
+                            name: 'test',
+                            resolveId(importee, importer) {
+                                if (!importer) {
+                                    return importee;
+                                }
+
+                                return importee;
+                            },
+                            load(id) {
+                                if (id === 'main') {
+                                    return entryCode;
+                                }
+
+                                const code = modules.get(id);
+
+                                if (!code) {
+                                    throw new Error(
+                                        `Missing module ${id}. Maybe it was not a script?`
+                                    );
+                                }
+
+                                return code;
+                            },
+                        },
+                    ],
+                }).then(async (bundle) => {
+                    const { output } = await bundle.generate({
+                        format: 'iife',
+                    });
+
+                    let final = '';
+                    for (let chunkOrAsset of output) {
+                        if (chunkOrAsset.type === 'chunk') {
+                            final += chunkOrAsset.code;
+                        }
+                    }
+
+                    this._onBundleUpdated.next({
+                        portalId: portal.portalId,
+                        source: final,
+                    });
+                });
+            }
+        }
+    }
 
     /**
      * Registers a custom portal with the given ID.
      * @param portalId The ID of the portal.
      */
-    registerCustomPortal(portalId: string): void {}
+    registerCustomPortal(portalId: string): void {
+        this._portals.set(portalId, {
+            portalId,
+            entrypoints: [],
+        });
+    }
 
     /**
      * Adds an entry point to the portal with the given ID.
      * @param portalId The ID of the portal.
      * @param entrypoint The entrypoint.
      */
-    addEntryPoint(portalId: string, entrypoint: PortalEntrypoint): void {}
+    addEntryPoint(portalId: string, entrypoint: PortalEntrypoint): void {
+        let portal = this._portals.get(portalId);
+        if (portal) {
+            portal.entrypoints.push({
+                tag: trimEntrypointTag(entrypoint.tag),
+                botId: entrypoint.botId,
+            });
+        }
+    }
+}
+
+/**
+ * Trims the leading # symbol off the given tag.
+ */
+export function trimEntrypointTag(tag: string): string {
+    if (tag.startsWith('📖')) {
+        return tag.substring('📖'.length);
+    }
+    return tag;
+}
+
+export function isEntrypointTag(value: unknown): value is string {
+    return typeof value === 'string' && value.startsWith('📖');
 }
