b1a391d5be89c9af14cb4f9350c3ef7c5a6664fa
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-01-25 16:03:00 -0500

    feat: Add the structure for a custom portal component

---
+++ src/aux-server/aux-web/shared/vue-components/CustomPortal/CustomPortal.ts
@@ -0,0 +1,7 @@
+import Vue, { ComponentOptions } from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    components: {},
+})
+export default class CustomPortal extends Vue {}
---
+++ src/aux-server/aux-web/shared/vue-components/CustomPortal/CustomPortal.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="custom-portal">Hello!</div>
+</template>
+<script src="./CustomPortal.ts"></script>
+<style src="./CustomPortal.css" scoped></style>
