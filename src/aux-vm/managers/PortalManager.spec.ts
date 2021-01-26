ac15a7a77371342278981c8e374f63662c2d1471
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-01-25 23:48:27 -0500

    feat: Add an initial PortalManager instance
    - Useful for keeping track of custom portals and their data values.

---
+++ src/aux-vm/managers/PortalManager.spec.ts
@@ -0,0 +1,240 @@
+import { waitAsync } from '@casual-simulation/aux-common/test/TestHelpers';
+import { Subscription } from 'rxjs';
+import { TestAuxVM } from '../vm/test/TestAuxVM';
+import { PortalData, PortalManager, PortalUpdate } from './PortalManager';
+
+describe('PortalManager', () => {
+    let manager: PortalManager;
+    let vm: TestAuxVM;
+    let sub: Subscription;
+
+    beforeEach(() => {
+        sub = new Subscription();
+        vm = new TestAuxVM();
+        manager = new PortalManager(vm);
+    });
+
+    afterEach(() => {
+        sub.unsubscribe();
+    });
+
+    describe('onPortalDiscovered', () => {
+        let portals = [] as PortalData[];
+
+        beforeEach(() => {
+            portals = [];
+            sub.add(
+                manager.portalsDiscovered.subscribe((p) => portals.push(...p))
+            );
+        });
+
+        it('should not resolve registered portals until they have some source', async () => {
+            expect(portals).toEqual([]);
+
+            vm.portalEvents.next([
+                {
+                    type: 'register_portal',
+                    portalId: 'test-portal',
+                },
+            ]);
+
+            await waitAsync();
+
+            expect(portals).toEqual([]);
+
+            vm.portalEvents.next([
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'abc',
+                },
+            ]);
+
+            await waitAsync();
+
+            expect(portals).toEqual([
+                {
+                    id: 'test-portal',
+                    source: 'abc',
+                },
+            ]);
+        });
+
+        it('should resolve multiple portals at the same time', async () => {
+            expect(portals).toEqual([]);
+
+            vm.portalEvents.next([
+                {
+                    type: 'register_portal',
+                    portalId: 'test-portal',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'abc',
+                },
+                {
+                    type: 'register_portal',
+                    portalId: 'other-portal',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'other-portal',
+                    source: 'def',
+                },
+            ]);
+
+            await waitAsync();
+
+            expect(portals).toEqual([
+                {
+                    id: 'test-portal',
+                    source: 'abc',
+                },
+                {
+                    id: 'other-portal',
+                    source: 'def',
+                },
+            ]);
+        });
+
+        it('should resolve portals that already have source', async () => {
+            vm.portalEvents.next([
+                {
+                    type: 'register_portal',
+                    portalId: 'test-portal',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'abc',
+                },
+                {
+                    type: 'register_portal',
+                    portalId: 'other-portal',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'other-portal',
+                    source: 'def',
+                },
+            ]);
+
+            await waitAsync();
+
+            let existingPortals: PortalData[][] = [];
+            manager.portalsDiscovered.subscribe((p) => existingPortals.push(p));
+
+            expect(existingPortals).toEqual([
+                [
+                    {
+                        id: 'test-portal',
+                        source: 'abc',
+                    },
+                    {
+                        id: 'other-portal',
+                        source: 'def',
+                    },
+                ],
+            ]);
+        });
+    });
+
+    describe('onPortalUpdated', () => {
+        let updates = [] as PortalUpdate[];
+
+        beforeEach(async () => {
+            updates = [];
+            sub.add(
+                manager.portalsUpdated.subscribe((p) => updates.push(...p))
+            );
+
+            vm.portalEvents.next([
+                {
+                    type: 'register_portal',
+                    portalId: 'test-portal',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'abc',
+                },
+                {
+                    type: 'register_portal',
+                    portalId: 'other-portal',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'other-portal',
+                    source: 'def',
+                },
+            ]);
+
+            await waitAsync();
+        });
+
+        it('should resolve updates to a portal', async () => {
+            expect(updates).toEqual([]);
+
+            vm.portalEvents.next([
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'different',
+                },
+            ]);
+
+            await waitAsync();
+
+            expect(updates).toEqual([
+                {
+                    oldPortal: {
+                        id: 'test-portal',
+                        source: 'abc',
+                    },
+                    portal: {
+                        id: 'test-portal',
+                        source: 'different',
+                    },
+                },
+            ]);
+        });
+
+        it('should collapse multiple source updates', async () => {
+            expect(updates).toEqual([]);
+
+            vm.portalEvents.next([
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'different1',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'different2',
+                },
+                {
+                    type: 'update_portal_source',
+                    portalId: 'test-portal',
+                    source: 'different3',
+                },
+            ]);
+
+            await waitAsync();
+
+            expect(updates).toEqual([
+                {
+                    oldPortal: {
+                        id: 'test-portal',
+                        source: 'abc',
+                    },
+                    portal: {
+                        id: 'test-portal',
+                        source: 'different3',
+                    },
+                },
+            ]);
+        });
+    });
+});
---
+++ src/aux-vm/managers/PortalManager.ts
@@ -0,0 +1,140 @@
+import { Observable, Subject, Subscription, SubscriptionLike } from 'rxjs';
+import { startWith, tap } from 'rxjs/operators';
+import { PortalEvent } from '../vm/PortalEvents';
+import { AuxVM } from '../vm/AuxVM';
+import { hasValue } from '@casual-simulation/aux-common';
+
+/**
+ * Defines a class that is able to manage portals and their interactions.
+ */
+export class PortalManager implements SubscriptionLike {
+    private _portals: Map<string, PortalData>;
+
+    private _portalsDiscovered: Subject<PortalData[]>;
+    private _portalsUpdated: Subject<PortalUpdate[]>;
+    private _sub: Subscription;
+
+    /**
+     * Gets an observable that resolves when a new portal is discovered.
+     */
+    get portalsDiscovered(): Observable<PortalData[]> {
+        return this._portalsDiscovered.pipe(
+            startWith([...this._portals.values()])
+        );
+    }
+
+    /**
+     * Gets an observable that resolves when a portal is updated.
+     */
+    get portalsUpdated(): Observable<PortalUpdate[]> {
+        return this._portalsUpdated;
+    }
+
+    constructor(vm: AuxVM) {
+        this._portals = new Map();
+        this._portalsDiscovered = new Subject();
+        this._portalsUpdated = new Subject();
+        this._sub = new Subscription();
+
+        this._sub.add(
+            vm.portalEvents
+                .pipe(tap((events) => this._onPortalEvents(events)))
+                .subscribe()
+        );
+    }
+
+    private _onPortalEvents(events: PortalEvent[]): void {
+        let newPortals: PortalData[] = [];
+        let updatedPortals: Map<string, PortalUpdate> = new Map();
+
+        for (let event of events) {
+            if (event.type === 'register_portal') {
+                if (this._portals.has(event.portalId)) {
+                    // TODO: update properties
+                } else {
+                    const newPortal: PortalData = {
+                        id: event.portalId,
+                        source: null,
+                    };
+
+                    this._portals.set(event.portalId, newPortal);
+                }
+            } else if (event.type === 'update_portal_source') {
+                const currentPortal = this._portals.get(event.portalId);
+
+                if (currentPortal) {
+                    const nextPortal: PortalData = {
+                        ...currentPortal,
+                        source: event.source,
+                    };
+                    this._portals.set(event.portalId, nextPortal);
+
+                    if (hasValue(currentPortal.source)) {
+                        // it is an update
+                        let currentUpdate = updatedPortals.get(event.portalId);
+                        if (!currentUpdate) {
+                            currentUpdate = {
+                                oldPortal: currentPortal,
+                                portal: nextPortal,
+                            };
+                            updatedPortals.set(event.portalId, currentUpdate);
+                        } else {
+                            currentUpdate.portal = nextPortal;
+                        }
+                    } else {
+                        // it is a portal that does not have source yet
+                        newPortals.push(nextPortal);
+                    }
+                }
+            }
+        }
+
+        if (newPortals.length > 0) {
+            this._portalsDiscovered.next(newPortals);
+        }
+        if (updatedPortals.size > 0) {
+            this._portalsUpdated.next([...updatedPortals.values()]);
+        }
+    }
+
+    unsubscribe(): void {
+        if (this._sub) {
+            this._sub.unsubscribe();
+            this._sub = null;
+        }
+    }
+
+    get closed(): boolean {
+        return this._sub.closed;
+    }
+}
+
+/**
+ * Defines data about a portal.
+ */
+export interface PortalData {
+    /**
+     * The ID of the portal.
+     */
+    id: string;
+
+    /**
+     * The source code that the portal should use.
+     */
+    source: string;
+}
+
+/**
+ * Contains information about a portal update.
+ */
+export interface PortalUpdate {
+    /**
+     * The updated portal.
+     */
+    portal: PortalData;
+
+    /**
+     * The old portal.
+     */
+    oldPortal: PortalData;
+}
