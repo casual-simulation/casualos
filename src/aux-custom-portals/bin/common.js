b3a947fb5b2d631c43180d2962b093041ca64369
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-02-15 17:44:11 -0500

    feat: Bundle lodash and rxjs separately so they can be included as their own libraries

--- src/aux-custom-portals/bin/common.js
+++ src/aux-custom-portals/bin/common.js
@@ -4,8 +4,8 @@ const ts = require('rollup-plugin-ts');
 module.exports = [
     // esbuild is for fast builds
     {
-        type: 'esbuild',
         id: 'casualos',
+        type: 'esbuild',
         options: {
             entryPoints: [path.resolve(__dirname, '../lib/casualos.ts')],
             outfile: path.resolve(__dirname, '../dist/esbuild/casualos.js'),
@@ -15,14 +15,32 @@ module.exports = [
                 __dirname,
                 '../dist/esbuild/casualos.meta.json'
             ),
-            external: ['lodash', 'rxjs'],
+            minifySyntax: true,
+            external: [
+                'lodash',
+                'rxjs',
+                'three',
+                'scrypt-js',
+                'base64-js',
+                'uuid/v4',
+                'uuid/v5',
+                'hash.js',
+                'tweetnacl',
+                'acorn',
+                'lru-cache',
+                'estraverse',
+                'mime',
+                'fast-json-stable-stringify',
+                'astring',
+                '@tweenjs/tween.js',
+            ],
         },
     },
 
     // rollup is for typescript declarations
     {
-        type: 'rollup',
         id: 'casualos',
+        type: 'rollup',
         options: {
             input: path.resolve(__dirname, '../lib/casualos.ts'),
             plugins: [
@@ -31,6 +49,37 @@ module.exports = [
                     tsconfig: path.resolve(__dirname, '../tsconfig.d.json'),
                 }),
             ],
+        },
+    },
+
+    {
+        id: 'lodash',
+        type: 'esbuild',
+        options: {
+            entryPoints: [path.resolve(__dirname, '../lib/lodash.ts')],
+            outfile: path.resolve(__dirname, '../dist/esbuild/lodash.js'),
+            bundle: true,
+            format: 'esm',
+            metafile: path.resolve(
+                __dirname,
+                '../dist/esbuild/lodash.meta.json'
+            ),
+            external: [],
+        },
+    },
+
+    {
+        id: 'rxjs',
+        type: 'esbuild',
+        options: {
+            entryPoints: [
+                path.resolve(__dirname, '../lib/rxjs.ts'),
+                path.resolve(__dirname, '../lib/rxjs-operators.ts'),
+            ],
+            outdir: path.resolve(__dirname, '../dist/esbuild/rxjs'),
+            bundle: true,
+            format: 'esm',
+            metafile: path.resolve(__dirname, '../dist/esbuild/rxjs.meta.json'),
         },
     },
 ];
