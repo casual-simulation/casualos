311d0866992e20ae2496dac3a5073ca70c4e884c
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-02-16 13:36:55 -0500

    chore: Only build rollup builds when they haven't been built

--- src/aux-custom-portals/bin/watch.js
+++ src/aux-custom-portals/bin/watch.js
@@ -1,15 +1,33 @@
 const esbuild = require('esbuild');
 const builds = require('./common');
+const fs = require('fs');
 
 for (let b of builds) {
     start(b);
 }
 
 async function start(build) {
-    if (build.type === 'esbuild') {
-        await esbuild.build({
-            ...build.options,
-            watch: true,
-        });
+    try {
+        if (build.type === 'esbuild') {
+            await esbuild.build({
+                ...build.options,
+                watch: true,
+            });
+        } else {
+            const dir = path.resolve(__dirname, `../dist/rollup/${build.id}`);
+            // Only do rollup builds during watch when the output directory does not exist.
+            if (!fs.existsSync(dir)) {
+                const bundle = await rollup.rollup({
+                    ...build.options,
+                });
+
+                await bundle.write({
+                    dir,
+                });
+                await bundle.close();
+            }
+        }
+    } catch (err) {
+        console.error(err);
     }
 }
