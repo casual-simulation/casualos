65738b6848ba275e8c3efe04f9ff1165fb71ca51
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-02-22 16:41:32 -0500

    feat: Improve how scrollbars are handled on the IDE portal

--- src/aux-server/aux-web/shared/vue-components/IdePortal/IdePortal.vue
+++ src/aux-server/aux-web/shared/vue-components/IdePortal/IdePortal.vue
@@ -4,21 +4,23 @@
             <md-card-content>
                 <div class="items-list">
                     <div class="items-list-header">Tags</div>
-                    <div
-                        v-for="item in items"
-                        :key="item.key"
-                        @click="selectItem(item)"
-                        class="item"
-                        :class="{ selected: selectedItem && item.key === selectedItem.key }"
-                    >
-                        <bot-tag
-                            :tag="item.name"
-                            :isScript="item.isScript"
-                            :isFormula="item.isFormula"
-                            :prefix="item.prefix"
-                            :light="true"
+                    <div class="items-list-items">
+                        <div
+                            v-for="item in items"
+                            :key="item.key"
+                            @click="selectItem(item)"
+                            class="item"
+                            :class="{ selected: selectedItem && item.key === selectedItem.key }"
                         >
-                        </bot-tag>
+                            <bot-tag
+                                :tag="item.name"
+                                :isScript="item.isScript"
+                                :isFormula="item.isFormula"
+                                :prefix="item.prefix"
+                                :light="true"
+                            >
+                            </bot-tag>
+                        </div>
                     </div>
                 </div>
                 <div class="portal-content" v-if="currentBot && currentTag">
@@ -30,11 +32,11 @@
                         :showDesktopEditor="true"
                         :showResize="false"
                     ></tag-value-editor>
-                    <md-button v-if="showButton" class="md-fab exit-portal" @click="exitPortal()">
-                        <md-icon>{{ finalButtonIcon }}</md-icon>
-                        <md-tooltip>{{ finalButtonHint }}</md-tooltip>
-                    </md-button>
                 </div>
+                <md-button v-if="showButton" class="md-fab exit-portal" @click="exitPortal()">
+                    <md-icon>{{ finalButtonIcon }}</md-icon>
+                    <md-tooltip>{{ finalButtonHint }}</md-tooltip>
+                </md-button>
             </md-card-content>
         </md-card>
     </div>
