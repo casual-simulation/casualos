ad2ebbef8fd82459447203a4626a0c57c45ee489
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-03-23 14:47:30 -0400

    fix: An issue where negative numbers could not be used for the sort order of menu bots

--- src/aux-server/aux-web/aux-player/MenuPortal.spec.ts
+++ src/aux-server/aux-web/aux-player/MenuPortal.spec.ts
@@ -1,4 +1,4 @@
-import { createBot } from '@casual-simulation/aux-common';
+import { botUpdated, createBot } from '@casual-simulation/aux-common';
 import { waitAsync } from '@casual-simulation/aux-common/test/TestHelpers';
 import { waitForSync } from '@casual-simulation/aux-vm';
 import { Simulation, SimulationManager } from '@casual-simulation/aux-vm';
@@ -76,5 +76,60 @@ describe('MenuPortal', () => {
         await waitAsync();
 
         expect(dim.items.length).toEqual(2);
+    });
+
+    it('should sort items by their sort order', async () => {
+        const dim = new MenuPortal(simulationManager, ['menuPortal']);
+
+        const sim = await simulationManager.addSimulation('test');
+        sim.helper.userId = 'user';
+
+        await waitForSync(sim);
+
+        await sim.helper.updateBot(sim.helper.botsState['user'], {
+            tags: {
+                menuPortal: 'menu',
+            },
+        });
+
+        await sim.helper.transaction(
+            botUpdated('test1', {
+                tags: {
+                    menu: true,
+                },
+            }),
+            botUpdated('test2', {
+                tags: {
+                    menu: true,
+                    menuSortOrder: 1,
+                },
+            }),
+            botUpdated('test3', {
+                tags: {
+                    menu: true,
+                    menuSortOrder: -1,
+                },
+            })
+        );
+
+        await waitAsync();
+
+        expect(dim.items).toEqual([
+            {
+                bot: sim.helper.botsState['test3'],
+                dimensions: new Set(['menu']),
+                simulationId: 'test',
+            },
+            {
+                bot: sim.helper.botsState['test1'],
+                dimensions: new Set(['menu']),
+                simulationId: 'test',
+            },
+            {
+                bot: sim.helper.botsState['test2'],
+                dimensions: new Set(['menu']),
+                simulationId: 'test',
+            },
+        ]);
     });
 });
