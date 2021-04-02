394a313beb85769ad24477be8afd81b8da8b2d9d
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-04-02 14:44:02 -0400

    fix: IMU data on iPhones
    - iPhone does not support the AbsoluteOrientationSensor API but it does support sending "deviceorientation".
    - One weird thing is that the DeviceMotionEvent.requestPermission() API needs to be called from an event that was directly triggered by a user action (like a onclick). This is different from a number of APIs and is why we have a custom confirm dialog in this case.

--- src/aux-server/aux-web/shared/vue-components/ImuPortal/ImuPortal.vue
+++ src/aux-server/aux-web/shared/vue-components/ImuPortal/ImuPortal.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="imu-portal-container"></div>
+    <div class="imu-portal-container">
+        <md-dialog-confirm
+            :md-active.sync="showRequestDeviceMotionPermission"
+            class="imu-request-dialog"
+            md-title="Allow tracking device motion?"
+            md-content="This allows CasualOS to read information about your device's spatial orientation."
+            md-confirm-text="Yes"
+            md-cancel-text="No"
+            @md-cancel="onCancelDeviceMotion"
+            @md-confirm="onConfirmDeviceMotion"
+        />
+    </div>
 </template>
 <script src="./ImuPortal.ts"></script>
-<style src="./ImuPortal.css" scoped></style>
+<style src="./ImuPortal.css"></style>
