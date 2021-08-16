ed10417ff6873d02208e62ee355dafc99b97184a
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-08-16 12:01:33 -0400

    feat: Add a basic implementation for the API

---
+++ src/aux-auth/serverless/aws/src/handlers/metadata.js
@@ -0,0 +1,94 @@
+// Create clients and set shared const values outside of the handler.
+const { Magic } = require('@magic-sdk/admin');
+
+// Get the DynamoDB table name from environment variables
+const USERS_TABLE = process.env.USERS_TABLE;
+const MAGIC_SECRET_KEY = process.env.MAGIC_SECRET_KEY;
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient({
+    endpoint: ENDPOINT,
+});
+const magic = new Magic(MAGIC_SECRET_KEY);
+
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+exports.getIssuerMetadata = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(
+            `getIssuerMetadata only accept GET method, you tried: ${event.httpMethod}`
+        );
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    const issuer = event.pathParameters.issuer;
+
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    const data = await docClient
+        .get({
+            TableName: USERS_TABLE,
+            Key: { id: issuer },
+        })
+        .promise();
+    const item = data.Item;
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            name: item.name,
+            avatarUrl: item.avatarUrl,
+            avatarPortraitUrl: item.avatarPortraitUrl,
+        }),
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(
+        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+    );
+    return response;
+};
+
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+exports.putIssuerMetadata = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(
+            `getIssuerMetadata only accept GET method, you tried: ${event.httpMethod}`
+        );
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    const token = event.pathParameters.token;
+    const issuer = magic.token.getIssuer(token);
+    const data = event.body;
+
+    await docClient
+        .put({
+            TableName: USERS_TABLE,
+            Item: {
+                id: issuer,
+                name: data.name,
+                avatarUrl: data.avatarUrl,
+                avatarPortraitUrl: data.avatarPortraitUrl,
+            },
+        })
+        .promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({}),
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(
+        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+    );
+    return response;
+};
---
+++ src/aux-auth/serverless/aws/src/handlers/services.js
@@ -0,0 +1,101 @@
+// Create clients and set shared const values outside of the handler.
+const { Magic } = require('@magic-sdk/admin');
+
+// Get the DynamoDB table name from environment variables
+const USER_SERVICES_TABLE = 'UserServices';
+const MAGIC_SECRET_KEY = process.env.MAGIC_SECRET_KEY;
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient({
+    endpoint: ENDPOINT,
+});
+const magic = new Magic(MAGIC_SECRET_KEY);
+
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+exports.getServiceForIssuer = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(
+            `getIssuerMetadata only accept GET method, you tried: ${event.httpMethod}`
+        );
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    const issuer = event.pathParameters.issuer;
+    const service = event.pathParameters.service;
+
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    const data = await docClient
+        .get({
+            TableName: USER_SERVICES_TABLE,
+            Key: { userId: issuer, service: service },
+        })
+        .promise();
+    const item = data.Item;
+
+    let response;
+    if (!item) {
+        response = {
+            statusCode: 404,
+        };
+    } else {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                userId: item.userId,
+                service: item.service,
+            }),
+        };
+    }
+
+    // All log statements are written to CloudWatch
+    console.info(
+        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+    );
+    return response;
+};
+
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+exports.putService = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(
+            `getIssuerMetadata only accept GET method, you tried: ${event.httpMethod}`
+        );
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    const token = event.pathParameters.token;
+    const issuer = magic.token.getIssuer(token);
+    const { service, token: serviceToken } = event.body;
+
+    magic.token.validate(serviceToken, service);
+
+    await docClient
+        .put({
+            TableName: USER_SERVICES_TABLE,
+            Item: {
+                userId: issuer,
+                service: service,
+            },
+        })
+        .promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({}),
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(
+        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+    );
+    return response;
+};
