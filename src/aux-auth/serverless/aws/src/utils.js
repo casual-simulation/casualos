70dcd17171deb130cd4e267d387173e6199f7929
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-08-18 17:00:54 -0400

    fix: Properly support case-insensitive header names

--- src/aux-auth/serverless/aws/src/utils.js
+++ src/aux-auth/serverless/aws/src/utils.js
@@ -3,9 +3,19 @@ const allowedOrigins = new Set([
     'https://casualos.me',
 ]);
 
+function findHeader(request, header) {
+    let headerKey = Object.keys(request.headers).find(
+        (key) => key.toLowerCase() === header.toLowerCase()
+    );
+    if (headerKey) {
+        return request.headers[headerKey];
+    }
+    return undefined;
+}
+
 module.exports = {
     formatResponse: (request, response) => {
-        const origin = request.headers.Origin ?? request.headers.origin;
+        const origin = findHeader(request, 'origin');
         console.log('test', JSON.stringify(request.headers));
         console.log('hit');
         let headers = {};
