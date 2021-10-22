b3a587342aa10778293e1fc708078cd3c233487f
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-10-22 13:59:20 -0400

    feat: Set the dist directory for Vite builds

--- src/aux-server/aux-web/aux-player/vite.config.ts
+++ src/aux-server/aux-web/aux-player/vite.config.ts
@@ -5,6 +5,7 @@ import { createVuePlugin } from 'vite-plugin-vue2';
 // @ts-ignore
 import { GIT_HASH, GIT_TAG } from '../../../../script/git-stats';
 
+const distDir = path.resolve(__dirname, '..', 'dist');
 const casualOsPackages = fs
     .readdirSync(
         // src folder
@@ -14,12 +15,17 @@ const casualOsPackages = fs
 
 export default defineConfig({
     plugins: [createVuePlugin()],
+    build: {
+        outDir: distDir,
+        emptyOutDir: false,
+    },
     assetsInclude: ['**/*.gltf', '**/*.glb'],
     define: {
         GIT_HASH: JSON.stringify(GIT_HASH),
         GIT_TAG: JSON.stringify(GIT_TAG),
         PRODUCTION: JSON.stringify(false),
     },
+    publicDir,
     resolve: {
         extensions: ['.vue', '.ts', '.mjs', '.js', '.tsx', '.jsx', '.json'],
         alias: {
