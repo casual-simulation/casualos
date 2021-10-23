3f60d3530399eb19d13a930aa4ad9bdcf7fa613e
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-10-23 00:57:41 -0400

    feat: Add runtime caching for some assets

--- src/aux-server/aux-web/aux-player/sw.ts
+++ src/aux-server/aux-web/aux-player/sw.ts
@@ -1,5 +1,12 @@
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 import { clientsClaim } from 'workbox-core';
+import { registerRoute, Route } from 'workbox-routing';
+import {
+    StaleWhileRevalidate,
+    CacheFirst,
+    NetworkFirst,
+} from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
 import '../shared/service-worker';
 
 declare let self: ServiceWorkerGlobalScope;
@@ -14,3 +21,43 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     clientsClaim();
 });
+
+registerRoute(
+    ({ url }) => /esbuild(\.\w+)?\.wasm$/.test(url.pathname),
+    new CacheFirst({
+        cacheName: 'esbuild',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 1,
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ url }) => /draco_decoder(\.\w+)?\.wasm$/.test(url.pathname),
+    new StaleWhileRevalidate({
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 1,
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ url }) => /\.wasm$/.test(url.pathname),
+    new CacheFirst({
+        cacheName: 'wasm',
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 604800, // 7 days in seconds
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({ url }) => /\/api\/config$/.test(url.pathname),
+    new NetworkFirst()
+);
