84cfdc97b24848a3176d846a6adad62f74529b94
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-12-28 18:38:25 -0500

    feat: Add the ability to use various matchers for in-assertion expectations

--- jest/jest-setup.ts
+++ jest/jest-setup.ts
@@ -1,1 +1,33 @@
 import '@testing-library/jest-dom';
+
+/**
+ * Defines a matcher that makes it possible to use any Jest expect method inside a assertion that supports
+ * matchers.
+ * See https://stackoverflow.com/a/47160877/1832856
+ */
+class SatisfiesMatcher {
+    matcher: keyof jest.MatcherMethods;
+    matcherArgs: any[];
+
+    constructor(matcher: keyof jest.MatcherMethods, ...matcherArgs: any[]) {
+        this.matcher = matcher;
+        this.matcherArgs = matcherArgs;
+    }
+
+    asymmetricMatch(other: any) {
+        let result = expect(other)[this.matcher];
+        if (typeof result === 'function') {
+            (result as (...args: any[]) => void)(...this.matcherArgs);
+        } else {
+            throw new Error(
+                'Unable to perform match on ' +
+                    this.matcher.toString() +
+                    '. It is not a function.'
+            );
+        }
+        return true;
+    }
+}
+
+(expect as any).expect = (matcher: keyof jest.MatcherMethods, ...args: any) =>
+    new SatisfiesMatcher(matcher, ...args);
