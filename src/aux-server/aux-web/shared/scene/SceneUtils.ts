e2d4828d67c0853319b5525675f082ec5f098411
Kallyn Gowdy <kallyngowdy@gmail.com>
2022-02-04 13:36:05 -0500

    feat: Add the ability to use the camera feed as the gridPortal background

--- src/aux-server/aux-web/shared/scene/SceneUtils.ts
+++ src/aux-server/aux-web/shared/scene/SceneUtils.ts
@@ -43,6 +43,7 @@ import {
     BotLabelAnchor,
     getBotScale,
     getBotTransformer,
+    CameraType,
 } from '@casual-simulation/aux-common';
 import { getOptionalValue } from '../SharedUtils';
 import { Simulation } from '@casual-simulation/aux-vm';
@@ -796,4 +797,74 @@ export function convertRotationToAuxCoordinates(rotation: Matrix4) {
     return rotation
         .premultiply(CHANGE_ROTATION_Y_TO_Z_1)
         .multiply(CHANGE_ROTATION_Y_TO_Z_2);
+}
+
+/**
+ * Parses special CasualOS URLs into an object that indicates what it should be used for.
+ * Currently only supports "casualos://camera-feed/{rear|front}".
+ * Returns null if the URL has no special CasualOS meaning.
+ * @param url The URL that should be parsed.
+ * @returns
+ */
+export function parseCasualOSUrl(
+    url: string | Partial<URL>
+): ParsedCasualOSUrl {
+    try {
+        const uri = typeof url === 'object' ? url : new URL(url);
+        if (uri.protocol !== 'casualos:') {
+            return null;
+        }
+
+        if (uri.hostname === 'camera-feed') {
+            let camera: CameraType;
+            if (uri.pathname === '/front') {
+                camera = 'front';
+            } else if (uri.pathname === '/rear') {
+                camera = 'rear';
+            }
+
+            let result: ParsedCasualOSUrl = {
+                type: 'camera-feed',
+            };
+
+            if (camera) {
+                result.camera = camera;
+            }
+
+            return result;
+        } else if (uri.hostname === '') {
+            // Chrome/Firefox
+            // See https://bugs.chromium.org/p/chromium/issues/detail?id=869291 and https://bugzilla.mozilla.org/show_bug.cgi?id=1374505
+            if (uri.pathname.startsWith('//camera-feed')) {
+                let path = uri.pathname.slice('//camera-feed'.length);
+                let camera: CameraType;
+                if (path === '/front') {
+                    camera = 'front';
+                } else if (path === '/rear') {
+                    camera = 'rear';
+                }
+
+                let result: ParsedCasualOSUrl = {
+                    type: 'camera-feed',
+                };
+
+                if (camera) {
+                    result.camera = camera;
+                }
+
+                return result;
+            }
+        }
+
+        return null;
+    } catch {
+        return null;
+    }
+}
+
+export type ParsedCasualOSUrl = CasualOSCameraFeedUrl;
+
+export interface CasualOSCameraFeedUrl {
+    type: 'camera-feed';
+    camera?: CameraType;
 }
