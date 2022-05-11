618e557936395a01b36c924c38409efb8da12099
KallynGowdy <kallyngowdy@gmail.com>
2022-05-11 17:36:23 -0400

    feat: Improve how API docs are rendered

--- docs/util/doc.jsx
+++ docs/util/doc.jsx
@@ -1,12 +1,123 @@
 import React from 'react';
-import {usePluginData} from '@docusaurus/useGlobalData';
+import Heading from '@theme/Heading';
+import { sortBy } from 'lodash';
+
+function memberLink(reflection, member) {
+    return `${reflection.name}-${member.name}`;
+}
+
+function sortMembers(children){
+    return sortBy(children, c => c.kindString === 'Property' ? 0 : c.kindString === 'Constructor' ? 1 : 2);
+}
+
+function groupMembers(children) {
+    let properties = [];
+    let constructors = [];
+    let methods = [];
+
+    for(let c of children) {
+        if(c.kindString === 'Property') {
+            properties.push(c);
+        } else if(c.kindString === 'Constructor') {
+            constructors.push(c);
+        } else if(c.kindString === 'Method') {
+            methods.push(c);
+        }
+    }
+
+    return {
+        properties,
+        constructors,
+        methods
+    };
+}
+
+function memberTableOfContents(reflection, member) {
+    const id = memberLink(reflection, member);
+
+    let name;
+    if (member.kindString === 'Constructor') {
+        name = functionDefinition(member.signatures[0]);
+    } else if(member.kindString === 'Method') {
+        console.log(member.kindString);
+        name = functionDefinition(member.signatures[0]);
+    } else {
+        name = propertyDefinition(member);
+    }
+
+    name = `<code>${name}</code>`;
+
+    return {
+        value: name,
+        id: id,
+        level: 3
+    };
+}
+
+export function classTableOfContents(reflection) {
+    let toc = [];
+
+    let { properties, constructors, methods } = groupMembers(reflection.children);
+
+    if (properties.length > 0) {
+        // toc.push({
+        //     value: 'Properties',
+        //     id: `${reflection.name}-properties`,
+        //     level: 3
+        // });
+
+        toc.push(...properties.map(m => memberTableOfContents(reflection, m)));
+    }
+
+    if (constructors.length > 0) {
+        // toc.push({
+        //     value: 'Constructors',
+        //     id: `${reflection.name}-constructors`,
+        //     level: 3
+        // });
+
+        toc.push(...constructors.map(m => memberTableOfContents(reflection, m)));
+    }
+
+    if (methods.length > 0) {
+        // toc.push({
+        //     value: 'Methods',
+        //     id: `${reflection.name}-methods`,
+        //     level: 3
+        // });
+
+        toc.push(...methods.map(m => memberTableOfContents(reflection, m)));
+    }
+
+    // console.log(toc);
+
+    // for (let member of sortMembers(reflection.children)) {
+    //     const id = memberLink(reflection, member);
+
+    //     let name;
+    //     if (member.kindString === 'Constructor') {
+    //         name = functionDefinition(member.signatures[0]);
+    //     } else if(member.kindString === 'Method') {
+    //         console.log(member.kindString);
+    //         name = functionDefinition(member.signatures[0]);
+    //     } else {
+    //         name = propertyDefinition(member);
+    //     }
+
+    //     name = `<code>${name}</code>`;
+
+    //     toc.push({
+    //         value: name,
+    //         id: id,
+    //         level: 3
+    //     });
+    // }
+
+    return toc;
+}
 
 export function ClassDescription(props) {
-    const { project } = usePluginData('docusaurus-plugin-typedoc');
-    if (!project) {
-        throw new Error('Unable to get TypeDoc project!');
-    }
-    const reflection = project.children.find(c => c.name === props.name);
+    const reflection = props.reflection;
     if (!reflection) {
         throw new Error('Unable to find ' + props.name + '!');
     }
@@ -19,18 +130,16 @@ export function ClassDescription(props) {
 }
 
 export function ClassMembers(props) {
-    const { project } = usePluginData('docusaurus-plugin-typedoc');
-    if (!project) {
-        throw new Error('Unable to get TypeDoc project!');
-    }
-    const reflection = project.children.find(c => c.name === props.name);
+    const reflection = props.reflection;
     if (!reflection) {
         throw new Error('Unable to find ' + props.name + '!');
     }
 
+    const children = sortMembers(reflection.children);
+
     return (
-        <div>
-            {reflection.children.map(c => <ClassMember key={c.name} member={c}/>)}
+        <div className="api">
+            {children.map(c => <ClassMember key={c.name} member={c} link={memberLink(reflection, c)}/>)}
         </div>
     );
 }
@@ -49,19 +158,25 @@ export function ClassMember(props) {
     
     return (
         <div>
-            <ClassMemberHeader {...props}/>
             {detail}
         </div>
     )
 }
 
 export function ClassMemberHeader(props) {
-    return (<h3 id={`${props.anchorRoot}-${props.member.name}`}><code>{props.member.name}</code></h3>);
+    return (
+        <Heading as='h3' id={props.link}>
+            <code>{props.member.name}</code>
+        </Heading>
+    )
 }
 
 export function ClassPropertyMember(props) {
     return (
         <div>
+            <Heading as='h3' id={props.link}>
+                <code>{props.member.name}: <TypeLink type={props.member.type}/></code>
+            </Heading>
             <p>{props.member.comment?.shortText}</p>
             {/* <pre><code>{JSON.stringify(props.member)}</code></pre> */}
         </div>
@@ -71,8 +186,7 @@ export function ClassPropertyMember(props) {
 export function ClassPropertyConstructor(props) {
     return (
         <div>
-            {props.member.signatures.map(s => <FunctionSignature func={s} />)}
-            {/* <pre><code>{JSON.stringify(props.member)}</code></pre> */}
+            <FunctionSignature func={props.member.signatures[0]} link={props.link}/>
         </div>
     )
 }
@@ -80,28 +194,56 @@ export function ClassPropertyConstructor(props) {
 export function ClassPropertyMethod(props) {
     return (
         <div>
-            {props.member.signatures.map(s => <FunctionSignature func={s} />)}
-            <pre><code>{JSON.stringify(props.member)}</code></pre>
+            <FunctionSignature func={props.member.signatures[0]} link={props.link}/>
+            {/* <pre><code>{JSON.stringify(props.member)}</code></pre> */}
         </div>
     )
 }
 
-export function FunctionSignature({func}) {
+export function FunctionSignature({func, link}) {
     const params = (func.parameters || []);
     return (
-        <>
-            <h4><code>{func.name}({params.map((p, i) => <span>{i > 0 ? ', ' : ''}{p.name}</span>)})</code></h4>
-            <span>{func.comment?.shortText}</span>
-            <p>
-                <strong>Parameters</strong>
-                <ul>
-                    {params.map(p => (
-                        <li><strong>{p.name}</strong>: <MemberType member={p}/> {p.comment?.shortText}</li>
-                    ))}
-                </ul>
-            </p>
-        </>
+        <div>
+            <Heading as='h3' id={link}>
+                <FunctionDefinition func={func}/>
+            </Heading>
+            <p>{func.comment?.shortText}</p>
+            {params.length > 0 ? (
+                <div>
+                    <h4>Parameters</h4>
+                    <ul>
+                        {params.map(p => (
+                            <li key={p.name}><strong>{p.name}</strong>: <TypeLink type={p.type}/> {p.comment?.shortText}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) : ''}
+        </div>
     );
+}
+
+export function FunctionDefinition({ func }) {
+    const params = func.parameters || [];
+    return (
+        <code>{func.name}({params.map((p, i) => <span key={p.name}>{i > 0 ? ', ' : ''}{p.name}: <TypeLink type={p.type}/></span>)}): <TypeLink type={func.type}/></code>
+    );
+}
+
+export function functionDefinition(func) {
+    const params = func.parameters || [];
+    return `${func.name}(${params.map((p, i) => p.name).join(', ')}): ${func.type.name}`;
+}
+
+export function propertyDefinition(prop) {
+    return `${prop.name}: ${prop.type.name}`;
+}
+
+function TypeLink({ type }) {
+    if (type.type === 'intrinsic') {
+        return <span>{type.name}</span>
+    } else {
+        return <a href={`#${type.name.toLowerCase()}`}>{type.name}</a>
+    }
 }
 
 export function MemberType(props) {
