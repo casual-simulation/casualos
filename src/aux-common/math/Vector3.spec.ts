bd616663414d7e6b7cfd6a7cca769514f03d1263
KallynGowdy <kallyngowdy@gmail.com>
2022-05-21 03:36:08 -0400

    test: Add additional tests for Vector3.cross()

--- src/aux-common/math/Vector3.spec.ts
+++ src/aux-common/math/Vector3.spec.ts
@@ -137,7 +137,7 @@ describe('Vector3', () => {
     });
 
     describe('cross()', () => {
-        it('should calculate the cross product', () => {
+        it('should calculate the XY cross product', () => {
             const v1 = new Vector3(1, 0, 0);
             const v2 = new Vector3(0, 1, 0);
 
@@ -148,6 +148,36 @@ describe('Vector3', () => {
 
             expect(v3).toEqual(new Vector3(0, 0, 1));
             expect(v4).toEqual(new Vector3(0, 0, -1));
+            expect(v5).toEqual(new Vector3(0, 0, 0));
+            expect(v6).toEqual(new Vector3(0, 0, 0));
+        });
+
+        it('should calculate the XZ cross product', () => {
+            const v1 = new Vector3(1, 0, 0);
+            const v2 = new Vector3(0, 0, 1);
+
+            const v3 = v1.cross(v2);
+            const v4 = v2.cross(v1);
+            const v5 = v1.cross(v1);
+            const v6 = v1.cross(new Vector3(0, 0, 0));
+
+            expect(v3).toEqual(new Vector3(0, -1, 0));
+            expect(v4).toEqual(new Vector3(0, 1, 0));
+            expect(v5).toEqual(new Vector3(0, 0, 0));
+            expect(v6).toEqual(new Vector3(0, 0, 0));
+        });
+
+        it('should calculate the YZ cross product', () => {
+            const v1 = new Vector3(0, 1, 0);
+            const v2 = new Vector3(0, 0, 1);
+
+            const v3 = v1.cross(v2);
+            const v4 = v2.cross(v1);
+            const v5 = v1.cross(v1);
+            const v6 = v1.cross(new Vector3(0, 0, 0));
+
+            expect(v3).toEqual(new Vector3(1, 0, 0));
+            expect(v4).toEqual(new Vector3(-1, 0, 0));
             expect(v5).toEqual(new Vector3(0, 0, 0));
             expect(v6).toEqual(new Vector3(0, 0, 0));
         });
