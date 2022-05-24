43577bbd5f100fdbce579c04ccbd0951571f77a6
KallynGowdy <kallyngowdy@gmail.com>
2022-05-24 12:38:20 -0400

    fix: Wrist portal offsets in VR
    - Generic hand controllers have fairly significantly different rotations now.
    - Controllers and hololens simply needed to swap Y and Z and negate the new Y value.

--- src/aux-server/aux-web/aux-player/scene/PlayerPageSimulation3D.ts
+++ src/aux-server/aux-web/aux-player/scene/PlayerPageSimulation3D.ts
@@ -32,51 +32,51 @@ import Bowser from 'bowser';
 
 const wristOffsets = {
     generic_hand_right: {
-        positionOffset: new Vector3(0.1, 0.05, -0.1),
+        positionOffset: new Vector3(0.1, 0.05, -0.05),
         rotationOffset: new Euler(
-            10 * ThreeMath.DEG2RAD,
-            270 * ThreeMath.DEG2RAD,
-            15 * ThreeMath.DEG2RAD
+            -90 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD
         ),
     },
     generic_hand_left: {
-        positionOffset: new Vector3(-0.1, 0.05, -0.1),
+        positionOffset: new Vector3(-0.1, 0.05, -0.05),
         rotationOffset: new Euler(
-            -10 * ThreeMath.DEG2RAD,
-            -270 * ThreeMath.DEG2RAD,
-            15 * ThreeMath.DEG2RAD
+            -90 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD
         ),
     },
     generic_controller_right: {
         positionOffset: new Vector3(0.05, 0.1, 0.1),
         rotationOffset: new Euler(
             -120 * ThreeMath.DEG2RAD,
-            0 * ThreeMath.DEG2RAD,
-            -90 * ThreeMath.DEG2RAD
+            90 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD
         ),
     },
     generic_controller_left: {
         positionOffset: new Vector3(-0.05, 0.1, 0.1),
         rotationOffset: new Euler(
             -120 * ThreeMath.DEG2RAD,
-            0 * ThreeMath.DEG2RAD,
-            90 * ThreeMath.DEG2RAD
+            -90 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD
         ),
     },
     hololens_hand_right: {
         positionOffset: new Vector3(0.05, 0.1, 0.1),
         rotationOffset: new Euler(
             -120 * ThreeMath.DEG2RAD,
-            0 * ThreeMath.DEG2RAD,
-            -90 * ThreeMath.DEG2RAD
+            90 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD
         ),
     },
     hololens_hand_left: {
         positionOffset: new Vector3(-0.05, 0.1, 0.1),
         rotationOffset: new Euler(
             -120 * ThreeMath.DEG2RAD,
-            0 * ThreeMath.DEG2RAD,
-            90 * ThreeMath.DEG2RAD
+            -90 * ThreeMath.DEG2RAD,
+            0 * ThreeMath.DEG2RAD
         ),
     },
     none: {
