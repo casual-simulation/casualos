138878105b2987bc447192fcdfd347c9a1ec0a8f
KallynGowdy <kallyngowdy@gmail.com>
2022-06-27 17:01:23 -0400

    feat: Add an AuthMessenger that simply logs codes to the console

---
+++ src/aux-records/ConsoleAuthMessenger.ts
@@ -0,0 +1,22 @@
+import { AddressType } from 'AuthStore';
+import { AuthMessenger, SendCodeResult } from './AuthMessenger';
+
+/**
+ * Defines a class that defines a auth messenger which sends login request codes via console.log().
+ */
+export class ConsoleAuthMessenger implements AuthMessenger {
+    async supportsAddressType(addressType: AddressType): Promise<boolean> {
+        return true;
+    }
+
+    async sendCode(
+        address: string,
+        addressType: AddressType,
+        code: string
+    ): Promise<SendCodeResult> {
+        console.log(`[ConsoleAuthMessenger] Code is (${addressType}): ${code}`);
+        return {
+            success: true,
+        };
+    }
+}
