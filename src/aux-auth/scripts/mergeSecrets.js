e39634b063ea73ca3e74839b106ebadc0387d1df
KallynGowdy <kallyngowdy@gmail.com>
2022-07-12 18:07:57 -0400

    feat: Support specifying a TextIt API Token for serverless deployments

--- src/aux-auth/scripts/mergeSecrets.js
+++ src/aux-auth/scripts/mergeSecrets.js
@@ -28,6 +28,22 @@ async function start() {
 
     let needsUpdate = !secretsExists;
 
+    if (!result?.handleRecordsV2?.TEXT_IT_API_KEY) {
+        const response = await prompts({
+            type: 'text',
+            name: 'textItApiKey',
+            message: 'Please enter the API Key for TextIt',
+        });
+
+        result = _.merge({}, result, {
+            handleRecordsV2: {
+                TEXT_IT_API_KEY: response.textItApiKey,
+            },
+        });
+
+        needsUpdate = true;
+    }
+
     if (needsUpdate) {
         writeFileSync(secretsFile, JSON.stringify(result, null, 4));
     }
