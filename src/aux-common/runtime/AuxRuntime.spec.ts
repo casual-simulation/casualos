casual-simulation/casualos

commit 1e7b5fd9cba410d90a9c06dc29e6bfc39ef81aa3

fix: An issue where creating a bot with a DateTime, Vector2, Vector3, or Rotation tag would break the system

src/aux-common/runtime/AuxGlobalContext.ts
@@ -33,6 +33,7 @@ import { tap } from 'rxjs/operators';
 import TWEEN from '@tweenjs/tween.js';
 import { v4 as uuidv4 } from 'uuid';
 import stableStringify from '@casual-simulation/fast-json-stable-stringify';
+import { ensureBotIsSerializable } from './Utils';
 
 /**
  * The interval between animation frames in miliseconds when using setInterval().
@@ -970,7 +971,7 @@ export class MemoryGlobalContext implements AuxGlobalContext {
                 }
             }
         }
-        this.enqueueAction(botAdded(bot));
+        this.enqueueAction(botAdded(ensureBotIsSerializable(bot)));
         return script;
     }
 

src/aux-common/runtime/AuxGlobalContext.spec.ts
@@ -15,6 +15,10 @@ import { waitAsync } from '../test/TestHelpers';
 import { RanOutOfEnergyError } from './AuxResults';
 import { v4 as uuid } from 'uuid';
 import { types } from 'util';
+import { DateTime } from 'luxon';
+import { Vector2 } from '../math/Vector2';
+import { Vector3 } from '../math/Vector3';
+import { Rotation } from '../math/Rotation';
 
 const uuidMock: jest.Mock = <any>uuid;
 jest.mock('uuid');
@@ -37,7 +41,7 @@ describe('AuxGlobalContext', () => {
                 minor: 2,
                 patch: 3,
                 alpha: true,
-                playerMode: 'builder'
+                playerMode: 'builder',
             },
             {
                 supportsAR: false,
@@ -304,6 +308,38 @@ describe('AuxGlobalContext', () => {
             expect(context.getBotIdsWithListener('func1')).toEqual(['test1']);
             expect(context.getBotIdsWithListener('func2')).toEqual(['test1']);
         });
+
+        const dataTypeCases = [
+            [
+                'DateTime',
+                DateTime.utc(1999, 11, 19, 5, 42, 8),
+                '📅1999-11-19T05:42:08Z',
+            ] as const,
+            ['Vector2', new Vector2(1, 2), '➡️1,2'] as const,
+            ['Vector3', new Vector3(1, 2, 3), '➡️1,2,3'] as const,
+            ['Rotation', new Rotation(), '🔁0,0,0,1'] as const,
+        ];
+
+        it.each(dataTypeCases)(
+            'should support creating a bot with a %s tag',
+            (desc, given, expected) => {
+                let result = context.createBot(
+                    createBot('test1', {
+                        value: given,
+                    })
+                );
+                expect(result.tags.value).toEqual(given);
+                expect(result.raw.value).toEqual(given);
+                const actions = context.dequeueActions();
+                expect(actions).toEqual([
+                    botAdded(
+                        createBot('test1', {
+                            value: expected,
+                        })
+                    ),
+                ]);
+            }
+        );
     });
 
     describe('destroyBot()', () => {

src/aux-common/runtime/AuxLibrary.spec.ts
@@ -12439,6 +12439,37 @@ describe('AuxLibrary', () => {
                 );
             });
         });
+
+        const dataTypeCases = [
+            [
+                'DateTime',
+                DateTime.utc(1999, 11, 19, 5, 42, 8),
+                '📅1999-11-19T05:42:08Z',
+            ] as const,
+            ['Vector2', new Vector2(1, 2), '➡️1,2'] as const,
+            ['Vector3', new Vector3(1, 2, 3), '➡️1,2,3'] as const,
+            ['Rotation', new Rotation(), '🔁0,0,0,1'] as const,
+        ];
+
+        it.each(dataTypeCases)(
+            'should support creating a bot with a %s tag',
+            (desc, given, expected) => {
+                uuidMock.mockReturnValue('uuid');
+                const bot = library.tagSpecificApi.create(tagContext)({
+                    value: given,
+                }) as RuntimeBot;
+                expect(bot.tags.value).toEqual(given);
+                expect(bot.raw.value).toEqual(given);
+
+                expect(context.actions).toEqual([
+                    botAdded(
+                        createBot('uuid', {
+                            value: expected,
+                        })
+                    ),
+                ]);
+            }
+        );
     });
 
     describe('destroy()', () => {

src/aux-common/runtime/AuxRuntime.spec.ts
@@ -107,6 +107,7 @@ import { flatMap } from 'lodash';
 import { SubscriptionLike } from 'rxjs';
 import { DateTime } from 'luxon';
 import { Vector2, Vector3, Rotation } from '../math';
+import { customDataTypeCases } from './test/RuntimeTestHelpers';
 
 const uuidMock: jest.Mock = <any>uuid;
 jest.mock('uuid');
@@ -6523,6 +6524,36 @@ describe('AuxRuntime', () => {
                     ]);
                 });
             });
+
+            it.each(customDataTypeCases)(
+                'should support creating bots with %s tags',
+                async (desc, given, expected) => {
+                    uuidMock.mockReturnValueOnce('uuid');
+                    runtime.stateUpdated(
+                        stateUpdatedEvent({
+                            test1: createBot('test1', {
+                                create: '@create({ value: that.value })',
+                            }),
+                        })
+                    );
+                    runtime.shout('create', null, {
+                        value: given,
+                    });
+
+                    await waitAsync();
+
+                    expect(events).toEqual([
+                        [
+                            botAdded(
+                                createBot('uuid', {
+                                    creator: 'test1',
+                                    value: expected,
+                                })
+                            ),
+                        ],
+                    ]);
+                }
+            );
         });
 
         describe('bot_removed', () => {

src/aux-common/runtime/Utils.spec.ts
@@ -2,6 +2,7 @@ import {
     convertErrorToCopiableValue,
     convertToCopiableValue,
     embedBase64InPdf,
+    ensureBotIsSerializable,
     formatAuthToken,
     fromHexString,
     getEmbeddedBase64FromPdf,
@@ -12,6 +13,7 @@ import './BlobPolyfill';
 import { createDummyRuntimeBot } from './test/TestScriptBotFactory';
 import { DateTime } from 'luxon';
 import { Vector2, Vector3, Rotation } from '../math';
+import { createBot } from '../bots';
 
 describe('convertErrorToCopiableValue()', () => {
     it('should convert error objects into an object with message and name', () => {
@@ -438,5 +440,45 @@ describe('toHexString()', () => {
             'abcdef1230'
         );
         expect(toHexString(new Uint8Array([255, 254, 253]))).toBe('fffefd');
+    });
+});
+
+describe('ensureBotIsSerializable()', () => {
+    const dataTypeCases = [
+        [
+            'DateTime',
+            DateTime.utc(1999, 11, 19, 5, 42, 8),
+            '📅1999-11-19T05:42:08Z',
+        ] as const,
+        ['Vector2', new Vector2(1, 2), '➡️1,2'] as const,
+        ['Vector3', new Vector3(1, 2, 3), '➡️1,2,3'] as const,
+        ['Rotation', new Rotation(), '🔁0,0,0,1'] as const,
+    ];
+
+    it.each(dataTypeCases)(
+        'should return a new bot with the copiable version for %s values',
+        (desc, given, expected) => {
+            let result = ensureBotIsSerializable(
+                createBot('test', {
+                    value: given,
+                })
+            );
+
+            expect(result).toEqual(
+                createBot('test', {
+                    value: expected,
+                })
+            );
+        }
+    );
+
+    it('should return the given bot if everything is normal', () => {
+        let b = createBot('test', {
+            abc: 123,
+            def: 'ghi',
+        });
+        let result = ensureBotIsSerializable(b);
+
+        expect(result).toBe(b);
     });
 });

src/aux-common/runtime/Utils.ts
@@ -1,5 +1,6 @@
 import { RealtimeEditMode } from './RuntimeBot';
 import {
+    createBot,
     formatBotDate,
     formatBotRotation,
     formatBotVector,
@@ -454,4 +455,32 @@ function fromHexCode(code: number) {
     }
 
     throw new Error('Invalid hex code: ' + code);
+}
+
+/**
+ * Checks each of the tags in the given bot to ensure that the bot is copiable and returns a new bot with serializable values if the given bot is contains non-serializable values.
+ * @param bot The bot to check.
+ */
+export function ensureBotIsSerializable(bot: Bot): Bot {
+    let newBot: Bot;
+    for (let tag in bot.tags) {
+        const value = bot.tags[tag];
+        if (
+            value instanceof DateTime ||
+            value instanceof Vector2 ||
+            value instanceof Vector3 ||
+            value instanceof Rotation
+        ) {
+            updateTag(tag, convertToCopiableValue(value));
+        }
+    }
+
+    return newBot ?? bot;
+
+    function updateTag(tag: string, value: any) {
+        if (!newBot) {
+            newBot = createBot(bot.id, { ...bot.tags }, bot.space);
+        }
+        newBot.tags[tag] = value;
+    }
 }

src/aux-common/runtime/test/RuntimeTestHelpers.ts
@@ -1,0 +1,15 @@
+import { DateTime } from 'luxon';
+import { Vector2 } from '../../math/Vector2';
+import { Vector3 } from '../../math/Vector3';
+import { Rotation } from '../../math/Rotation';
+
+export const customDataTypeCases = [
+    [
+        'DateTime',
+        DateTime.utc(1999, 11, 19, 5, 42, 8),
+        '📅1999-11-19T05:42:08Z',
+    ] as const,
+    ['Vector2', new Vector2(1, 2), '➡️1,2'] as const,
+    ['Vector3', new Vector3(1, 2, 3), '➡️1,2,3'] as const,
+    ['Rotation', new Rotation(), '🔁0,0,0,1'] as const,
+];

commit a5148023ea6abfb80b375e2fa1727e89fa8e15b7

fix: An issue where immediate updates to would be ignored when updating new bots where the new bot was created with a custom data type
- Originally caused by 1e7b5fd9cba410d90a9c06dc29e6bfc39ef81aa3
- Issue was that the event for the new bot was not maintaining the reference that AuxRuntime was expecting.
- The solution is to ensure that the runtime is always given the same bot that is then enqueued to the event list.
- One notable behavior change is that the global context will now serialize custom data types and it is up to the script factory/runtime to ensure that those values then get converted back to the object that is expected of them. This could be considered a little inefficient, but it helps manage the code stink that is caused by sharing the reference between the bot added to the event and the bot that is added to the runtime.

src/aux-common/runtime/AuxGlobalContext.ts
@@ -959,7 +959,8 @@ export class MemoryGlobalContext implements AuxGlobalContext {
     }
 
     createBot(bot: Bot): RuntimeBot {
-        const script = this._scriptFactory.createRuntimeBot(bot) || null;
+        const newBot = ensureBotIsSerializable(bot);
+        const script = this._scriptFactory.createRuntimeBot(newBot) || null;
         if (script) {
             addToContext(this, script);
 
@@ -971,7 +972,7 @@ export class MemoryGlobalContext implements AuxGlobalContext {
                 }
             }
         }
-        this.enqueueAction(botAdded(ensureBotIsSerializable(bot)));
+        this.enqueueAction(botAdded(newBot));
         return script;
     }
 

src/aux-common/runtime/AuxGlobalContext.spec.ts
@@ -266,6 +266,22 @@ describe('AuxGlobalContext', () => {
             ]);
         });
 
+        it('should enqueue the same bot that it adds to the script factory', () => {
+            const mock = (factory.createRuntimeBot = jest.fn(
+                factory.createRuntimeBot
+            ));
+            context.createBot(
+                createBot('test1', {
+                    value: 123,
+                })
+            );
+
+            const actions = context.dequeueActions();
+            expect((actions[0] as any).bot === mock.mock.calls[0][0]).toBe(
+                true
+            );
+        });
+
         it('should return null if the runtime bot was unable to be created', () => {
             const mock = (factory.createRuntimeBot = jest.fn(() => null));
 
@@ -318,8 +334,8 @@ describe('AuxGlobalContext', () => {
                         value: given,
                     })
                 );
-                expect(result.tags.value).toEqual(given);
-                expect(result.raw.value).toEqual(given);
+                expect(result.tags.value).toEqual(expected);
+                expect(result.raw.value).toEqual(expected);
                 const actions = context.dequeueActions();
                 expect(actions).toEqual([
                     botAdded(

src/aux-common/runtime/AuxLibrary.spec.ts
@@ -12448,8 +12448,8 @@ describe('AuxLibrary', () => {
                 const bot = library.tagSpecificApi.create(tagContext)({
                     value: given,
                 }) as RuntimeBot;
-                expect(bot.tags.value).toEqual(given);
-                expect(bot.raw.value).toEqual(given);
+                expect(bot.tags.value).toEqual(expected);
+                expect(bot.raw.value).toEqual(expected);
 
                 expect(context.actions).toEqual([
                     botAdded(

src/aux-common/runtime/AuxRuntime.spec.ts
@@ -6353,6 +6353,43 @@ describe('AuxRuntime', () => {
                 ]);
             });
 
+            it.each(customDataTypeCases)(
+                'should be able to update bots with %s tags',
+                async (desc, value, expected) => {
+                    uuidMock.mockReturnValueOnce('uuid');
+                    runtime.stateUpdated(
+                        stateUpdatedEvent({
+                            test1: createBot('test1', {
+                                create: '@let created = create({ abc: that.value }); created.tags.newTag = 456; created.tags.def = true;',
+                            }),
+                        })
+                    );
+                    runtime.shout('create', undefined, { value });
+
+                    await waitAsync();
+
+                    expect(events).toEqual([
+                        [
+                            botAdded(
+                                createBot('uuid', {
+                                    creator: 'test1',
+                                    abc: expected,
+                                    newTag: 456,
+                                    def: true,
+                                })
+                            ),
+                        ],
+                    ]);
+
+                    expect(runtime.currentState['uuid'].script.tags).toEqual({
+                        creator: 'test1',
+                        abc: value,
+                        newTag: 456,
+                        def: true,
+                    });
+                }
+            );
+
             it('should be able to whisper to a bot that is created in an async shout', async () => {
                 let resolve: Function;
                 const promise = new Promise((r, reject) => {

src/aux-common/runtime/Utils.spec.ts
@@ -448,17 +448,17 @@ describe('ensureBotIsSerializable()', () => {
     it.each(customDataTypeCases)(
         'should return a new bot with the copiable version for %s values',
         (desc, given, expected) => {
-            let result = ensureBotIsSerializable(
-                createBot('test', {
-                    value: given,
-                })
-            );
+            const inputBot = createBot('test', {
+                value: given,
+            });
+            let result = ensureBotIsSerializable(inputBot);
 
             expect(result).toEqual(
                 createBot('test', {
                     value: expected,
                 })
             );
+            expect(result !== inputBot).toBe(true);
         }
     );
 