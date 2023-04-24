9c4dfdcdd6cb0338a7a7af0c5bb0e62ed80fdba2
Kallyn Gowdy <kallyngowdy@gmail.com>
2023-04-24 12:28:17 -0400

    feat: Add a MemoryRateLimiter for easy testing

---
+++ src/aux-records/MemoryRateLimiter.ts
@@ -0,0 +1,61 @@
+import {
+    RateLimiter,
+    RateLimiterIncrementResult,
+    RateLimiterOptions,
+} from '@casual-simulation/rate-limit-redis';
+
+/**
+ * Defines a rate limiter that stores everything in-memory.
+ */
+export class MemoryRateLimiter implements RateLimiter {
+    private _states: Map<string, MemoryState>;
+
+    windowMs: number = 1000;
+
+    init(options: RateLimiterOptions): void {
+        this.windowMs = options.windowMs;
+    }
+
+    async increment(
+        key: string,
+        amount: number = 1
+    ): Promise<RateLimiterIncrementResult> {
+        const state = this._getState(key);
+        state.count += amount;
+
+        return {
+            totalHits: state.count,
+            resetTime: new Date(state.resetTimeMs),
+        };
+    }
+
+    async decrement(key: string, amount?: number): Promise<void> {
+        const state = this._getState(key);
+        state.count += amount;
+    }
+
+    async resetKey(key: string): Promise<void> {
+        this._states.delete(key);
+    }
+
+    private _getState(key: string): MemoryState {
+        let state = this._states.get(key);
+        if (!state) {
+            state = {
+                count: 0,
+                resetTimeMs: Date.now() + this.windowMs,
+            };
+            this._states.set(key, state);
+        }
+        if (state.resetTimeMs > Date.now()) {
+            state.count = 0;
+            state.resetTimeMs = Date.now() + this.windowMs;
+        }
+        return state;
+    }
+}
+
+interface MemoryState {
+    count: number;
+    resetTimeMs: number;
+}
