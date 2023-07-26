772cca824c0989a3796ec17f3ec26615888a46ca
KallynGowdy <kallyngowdy@gmail.com>
2023-07-26 16:39:48 -0400

    feat: Add the ability to return errors when the user is not subscribed  to the correct tier

--- src/aux-records/AIController.spec.ts
+++ src/aux-records/AIController.spec.ts
@@ -13,13 +13,18 @@ describe('AIController', () => {
             [AIChatInterfaceRequest]
         >;
     };
+    let userId: string;
+    let userSubscriptionTier: string;
 
     beforeEach(() => {
+        userId = 'test-user';
+        userSubscriptionTier = 'test-tier';
         chatInterface = {
             chat: jest.fn(),
         };
         controller = new AIController(chatInterface, {
             allowedChatModels: ['test-model1', 'test-model2'],
+            allowedChatSubscriptionTiers: ['test-tier'],
         });
     });
 
@@ -46,7 +51,8 @@ describe('AIController', () => {
                     },
                 ],
                 temperature: 0.5,
-                userId: 'test-user',
+                userId,
+                userSubscriptionTier,
             });
 
             expect(result).toEqual({
@@ -82,7 +88,8 @@ describe('AIController', () => {
                     },
                 ],
                 temperature: 0.5,
-                userId: 'test-user',
+                userId,
+                userSubscriptionTier,
             });
 
             expect(result).toEqual({
@@ -91,6 +98,132 @@ describe('AIController', () => {
                 errorMessage: 'The given model is not allowed for chats.',
             });
             expect(chatInterface.chat).not.toBeCalled();
+        });
+
+        it('should return an not_logged_in result if the given a null userId', async () => {
+            const result = await controller.chat({
+                model: 'test-model1',
+                messages: [
+                    {
+                        role: 'user',
+                        content: 'test',
+                    },
+                ],
+                temperature: 0.5,
+                userId: null as any,
+                userSubscriptionTier,
+            });
+
+            expect(result).toEqual({
+                success: false,
+                errorCode: 'not_logged_in',
+                errorMessage:
+                    'The user must be logged in. Please provide a sessionKey or a recordKey.',
+            });
+            expect(chatInterface.chat).not.toBeCalled();
+        });
+
+        it('should return an not_subscribed result if the given a null userSubscriptionTier', async () => {
+            const result = await controller.chat({
+                model: 'test-model1',
+                messages: [
+                    {
+                        role: 'user',
+                        content: 'test',
+                    },
+                ],
+                temperature: 0.5,
+                userId,
+                userSubscriptionTier: null as any,
+            });
+
+            expect(result).toEqual({
+                success: false,
+                errorCode: 'not_subscribed',
+                errorMessage:
+                    'The user must be subscribed in order to use this operation.',
+                allowedSubscriptionTiers: ['test-tier'],
+            });
+            expect(chatInterface.chat).not.toBeCalled();
+        });
+
+        it('should return an invalid_subscription_tier result if the given a subscription tier that is not allowed', async () => {
+            const result = await controller.chat({
+                model: 'test-model1',
+                messages: [
+                    {
+                        role: 'user',
+                        content: 'test',
+                    },
+                ],
+                temperature: 0.5,
+                userId,
+                userSubscriptionTier: 'wrong-tier',
+            });
+
+            expect(result).toEqual({
+                success: false,
+                errorCode: 'invalid_subscription_tier',
+                errorMessage:
+                    'This operation is not available to the user at their current subscription tier.',
+                currentSubscriptionTier: 'wrong-tier',
+                allowedSubscriptionTiers: ['test-tier'],
+            });
+            expect(chatInterface.chat).not.toBeCalled();
+        });
+
+        it('should work when the controller is configured to allow all subscription tiers and the user does not have a subscription', async () => {
+            chatInterface.chat.mockReturnValueOnce(
+                Promise.resolve({
+                    choices: [
+                        {
+                            role: 'user',
+                            content: 'test',
+                            stopReason: 'stop',
+                        },
+                    ],
+                })
+            );
+
+            controller = new AIController(chatInterface, {
+                allowedChatModels: ['test-model1', 'test-model2'],
+                allowedChatSubscriptionTiers: true,
+            });
+
+            const result = await controller.chat({
+                model: 'test-model1',
+                messages: [
+                    {
+                        role: 'user',
+                        content: 'test',
+                    },
+                ],
+                temperature: 0.5,
+                userId,
+                userSubscriptionTier: null as any,
+            });
+
+            expect(result).toEqual({
+                success: true,
+                choices: [
+                    {
+                        role: 'user',
+                        content: 'test',
+                        stopReason: 'stop',
+                    },
+                ],
+            });
+            expect(chatInterface.chat).toBeCalledWith({
+                model: 'test-model1',
+                messages: [
+                    {
+                        role: 'user',
+                        content: 'test',
+                    },
+                ],
+                temperature: 0.5,
+                userId: 'test-user',
+            });
         });
     });
 });
--- src/aux-records/AIController.ts
+++ src/aux-records/AIController.ts
@@ -1,4 +1,9 @@
-import { ServerError } from './Errors';
+import {
+    InvalidSubscriptionTierError,
+    NotLoggedInError,
+    NotSubscribedError,
+    ServerError,
+} from './Errors';
 import { AIChatInterface, AIChatMessage } from './AIChatInterface';
 
 export interface AIOptions {
@@ -6,6 +11,11 @@ export interface AIOptions {
      * The list of allowed models that are allowed to be used for chat.
      */
     allowedChatModels: string[];
+
+    /**
+     * The list of subscription tiers that are allowed to be used for chat.
+     */
+    allowedChatSubscriptionTiers: true | string[];
 }
 
 /**
@@ -15,14 +25,61 @@ export class AIController {
     private _chat: AIChatInterface;
     private _options: AIOptions;
 
+    private _allowedChatModels: Set<string>;
+    private _allowedChatSubscriptionTiers: true | Set<string>;
+
     constructor(chat: AIChatInterface, options: AIOptions) {
         this._chat = chat;
         this._options = options;
+        this._allowedChatModels = new Set(options.allowedChatModels);
+        this._allowedChatSubscriptionTiers =
+            typeof options.allowedChatSubscriptionTiers === 'boolean'
+                ? options.allowedChatSubscriptionTiers
+                : new Set(options.allowedChatSubscriptionTiers);
     }
 
     async chat(request: AIChatRequest): Promise<AIChatResponse> {
         try {
-            if (!this._options.allowedChatModels.includes(request.model)) {
+            if (!request.userId) {
+                return {
+                    success: false,
+                    errorCode: 'not_logged_in',
+                    errorMessage:
+                        'The user must be logged in. Please provide a sessionKey or a recordKey.',
+                };
+            }
+
+            if (
+                this._allowedChatSubscriptionTiers !== true &&
+                !this._allowedChatSubscriptionTiers.has(
+                    request.userSubscriptionTier
+                )
+            ) {
+                if (!request.userSubscriptionTier) {
+                    return {
+                        success: false,
+                        errorCode: 'not_subscribed',
+                        errorMessage:
+                            'The user must be subscribed in order to use this operation.',
+                        allowedSubscriptionTiers: [
+                            ...this._allowedChatSubscriptionTiers,
+                        ],
+                    };
+                } else {
+                    return {
+                        success: false,
+                        errorCode: 'invalid_subscription_tier',
+                        errorMessage:
+                            'This operation is not available to the user at their current subscription tier.',
+                        allowedSubscriptionTiers: [
+                            ...this._allowedChatSubscriptionTiers,
+                        ],
+                        currentSubscriptionTier: request.userSubscriptionTier,
+                    };
+                }
+            }
+
+            if (!this._allowedChatModels.has(request.model)) {
                 return {
                     success: false,
                     errorCode: 'invalid_model',
@@ -72,6 +129,12 @@ export interface AIChatRequest {
     userId: string;
 
     /**
+     * The subscription tier of the user.
+     * Should be null if the user is not logged in or if they do not have a subscription.
+     */
+    userSubscriptionTier: string;
+
+    /**
      * The temperature of the request.
      */
     temperature?: number;
@@ -86,6 +149,14 @@ export interface AIChatSuccess {
 
 export interface AIChatFailure {
     success: false;
-    errorCode: ServerError | 'invalid_model';
+    errorCode:
+        | ServerError
+        | NotLoggedInError
+        | NotSubscribedError
+        | InvalidSubscriptionTierError
+        | 'invalid_model';
     errorMessage: string;
+
+    allowedSubscriptionTiers?: string[];
+    currentSubscriptionTier?: string;
 }
