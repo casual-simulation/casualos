96815fa0e4c0749392ddc3b6d9b2e1877f57f562
KallynGowdy <kallyngowdy@gmail.com>
2023-07-28 17:18:18 -0400

    feat: Support configuring AI image generation

--- src/aux-server/aux-backend/shared/ServerBuilder.ts
+++ src/aux-server/aux-backend/shared/ServerBuilder.ts
@@ -19,6 +19,8 @@ import {
     OpenAIChatInterface,
     AIChatInterface,
     BlockadeLabsGenerateSkyboxInterface,
+    OpenAIImageInterface,
+    StabilityAIImageInterface,
 } from '@casual-simulation/aux-records';
 import {
     DynamoDBAuthStore,
@@ -80,6 +82,7 @@ import {
 import {
     AIConfiguration,
     AIController,
+    AIGenerateImageConfiguration,
 } from '@casual-simulation/aux-records/AIController';
 
 export class ServerBuilder {
@@ -141,6 +144,7 @@ export class ServerBuilder {
         action: () => Promise<void>;
     }[] = [];
     private _generateSkyboxInterface: BlockadeLabsGenerateSkyboxInterface;
+    private _imagesInterfaces: AIGenerateImageConfiguration['interfaces'];
 
     private get _forceAllowAllSubscriptionFeatures() {
         return !this._stripe;
@@ -557,8 +561,10 @@ export class ServerBuilder {
     }
 
     useAI(
-        options: Pick<BuilderOptions, 'openai' | 'ai' | 'blockadeLabs'> = this
-            ._options
+        options: Pick<
+            BuilderOptions,
+            'openai' | 'ai' | 'blockadeLabs' | 'stabilityai'
+        > = this._options
     ): this {
         console.log('[ServerBuilder] Using AI.');
         if (!options.ai) {
@@ -586,9 +592,36 @@ export class ServerBuilder {
                 });
         }
 
+        if (options.ai.images) {
+            this._imagesInterfaces = {};
+            if (options.ai.images?.allowedModels?.openai && options.openai) {
+                console.log('[ServerBuilder] Using OpenAI Images.');
+                this._imagesInterfaces.openai = new OpenAIImageInterface({
+                    apiKey: options.openai.apiKey,
+                    defaultWidth: options.ai.images.defaultWidth,
+                    defaultHeight: options.ai.images.defaultHeight,
+                });
+            }
+
+            if (
+                options.ai.images?.allowedModels?.stabilityai &&
+                options.stabilityai
+            ) {
+                console.log('[ServerBuilder] Using StabilityAI Images.');
+
+                this._imagesInterfaces.stabilityai =
+                    new StabilityAIImageInterface({
+                        apiKey: options.stabilityai.apiKey,
+                        defaultWidth: options.ai.images.defaultWidth,
+                        defaultHeight: options.ai.images.defaultHeight,
+                    });
+            }
+        }
+
         this._aiConfiguration = {
             chat: null,
             generateSkybox: null,
+            images: null,
         };
 
         if (this._chatInterface && options.ai.chat) {
@@ -608,6 +641,23 @@ export class ServerBuilder {
                 options: {
                     allowedSubscriptionTiers:
                         options.ai.generateSkybox.allowedSubscriptionTiers,
+                },
+            };
+        }
+        if (this._imagesInterfaces && options.ai.images) {
+            const images = options.ai.images;
+            this._aiConfiguration.images = {
+                interfaces: this._imagesInterfaces,
+                options: {
+                    allowedModels: images.allowedModels,
+                    allowedSubscriptionTiers: images.allowedSubscriptionTiers,
+                    defaultHeight: images.defaultHeight,
+                    defaultWidth: images.defaultWidth,
+                    maxHeight: images.maxHeight,
+                    maxWidth: images.maxWidth,
+                    defaultModel: images.defaultModel,
+                    maxImages: images.maxImages,
+                    maxSteps: images.maxSteps,
                 },
             };
         }
@@ -943,6 +993,7 @@ export const optionsSchema = z.object({
     rateLimit: rateLimitSchema.optional(),
     openai: openAiSchema.optional(),
     blockadeLabs: blockadeLabsSchema.optional(),
+    stabilityai: stabilityAiSchema.optional(),
     ai: aiSchema.optional(),
 
     subscriptions: subscriptionConfigSchema.optional(),
