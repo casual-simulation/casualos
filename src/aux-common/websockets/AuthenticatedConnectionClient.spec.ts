ebfe50f1e8d02c275b7d800b0a92b010974ce310
KallynGowdy <kallyngowdy@gmail.com>
2023-10-18 12:51:31 -0400

    feat: Support logging in after recieving a new connection indicator for an origin

--- src/aux-common/websockets/AuthenticatedConnectionClient.spec.ts
+++ src/aux-common/websockets/AuthenticatedConnectionClient.spec.ts
@@ -10,7 +10,7 @@ import { waitAsync } from '../test/TestHelpers';
 import { ClientConnectionState } from './ConnectionClient';
 import { Subject } from 'rxjs';
 import { LoginResultMessage } from './WebsocketEvents';
-import { PartitionAuthSource } from '../partitions';
+import { PartitionAuthRequest, PartitionAuthSource } from '../partitions';
 
 console.log = jest.fn();
 
@@ -31,7 +31,7 @@ describe('AuthenticatedConnectionClient', () => {
             authSource = new PartitionAuthSource(
                 new Map([['http://localhost', indicator]])
             );
-            subject = new AuthenticatedConnectionClient(inner, indicator);
+            subject = new AuthenticatedConnectionClient(inner, authSource);
         });
 
         it('should attempt to login when the inner client connects', async () => {
@@ -75,6 +75,7 @@ describe('AuthenticatedConnectionClient', () => {
 
             loginResult.next({
                 type: 'login_result',
+                success: true,
                 info: {
                     userId: null,
                     sessionId: null,
@@ -151,6 +152,7 @@ describe('AuthenticatedConnectionClient', () => {
 
             loginResult.next({
                 type: 'login_result',
+                success: true,
                 info: {
                     userId: null,
                     sessionId: null,
@@ -179,6 +181,184 @@ describe('AuthenticatedConnectionClient', () => {
                 {
                     connected: false,
                     info: null,
+                },
+            ]);
+        });
+    });
+
+    describe('other scenarios', () => {
+        let indicator: ConnectionIndicator;
+        let loginResult: Subject<LoginResultMessage>;
+        let connectionStates: ClientConnectionState[];
+        beforeEach(() => {
+            indicator = { connectionToken: 'test_connection_token' };
+            inner = new MemoryConnectionClient();
+            inner.origin = 'http://localhost';
+            authSource = new PartitionAuthSource(
+                new Map([['http://localhost', indicator]])
+            );
+            subject = new AuthenticatedConnectionClient(inner, authSource);
+
+            loginResult = new Subject<LoginResultMessage>();
+            inner.events.set('login_result', loginResult);
+
+            connectionStates = [];
+            subject.connectionState.subscribe((state) =>
+                connectionStates.push(state)
+            );
+        });
+
+        it('should attempt to login again when a new indicator is provided for an origin', async () => {
+            inner.connect();
+
+            await waitAsync();
+
+            expect(inner.sentMessages).toEqual([
+                {
+                    type: 'login',
+                    ...indicator,
+                },
+            ]);
+            expect(connectionStates).toEqual([
+                {
+                    connected: false,
+                    info: null,
+                },
+            ]);
+
+            loginResult.next({
+                type: 'login_result',
+                success: true,
+                info: {
+                    userId: null,
+                    sessionId: null,
+                    connectionId: 'test_connection_id',
+                },
+            });
+
+            await waitAsync();
+
+            expect(connectionStates.slice(1)).toEqual([
+                {
+                    connected: true,
+                    info: {
+                        userId: null,
+                        sessionId: null,
+                        connectionId: 'test_connection_id',
+                    },
+                },
+            ]);
+
+            authSource.sendAuthResponse({
+                type: 'response',
+                success: true,
+                origin: subject.origin,
+                indicator: {
+                    connectionToken: 'test_connection_token_2',
+                },
+            });
+
+            await waitAsync();
+
+            expect(inner.sentMessages.slice(1)).toEqual([
+                {
+                    type: 'login',
+                    connectionToken: 'test_connection_token_2',
+                },
+            ]);
+
+            expect(connectionStates.slice(2)).toEqual([]);
+
+            loginResult.next({
+                type: 'login_result',
+                success: true,
+                info: {
+                    userId: null,
+                    sessionId: null,
+                    connectionId: 'test_connection_id_2',
+                },
+            });
+
+            expect(connectionStates.slice(2)).toEqual([
+                {
+                    connected: true,
+                    info: {
+                        userId: null,
+                        sessionId: null,
+                        connectionId: 'test_connection_id_2',
+                    },
+                },
+            ]);
+        });
+
+        it('should work when an indicator is not immediately available', async () => {
+            let requests = [] as PartitionAuthRequest[];
+            authSource.onAuthRequest.subscribe((r) => requests.push(r));
+
+            inner.origin = 'http://different';
+
+            inner.connect();
+
+            await waitAsync();
+
+            expect(inner.sentMessages).toEqual([]);
+            expect(requests).toEqual([
+                {
+                    type: 'request',
+                    origin: 'http://different',
+                },
+            ]);
+            expect(connectionStates).toEqual([
+                {
+                    connected: false,
+                    info: null,
+                },
+            ]);
+
+            authSource.sendAuthResponse({
+                type: 'response',
+                success: true,
+                origin: subject.origin,
+                indicator: {
+                    connectionToken: 'test_connection_token',
+                },
+            });
+
+            await waitAsync();
+
+            expect(inner.sentMessages).toEqual([
+                {
+                    type: 'login',
+                    connectionToken: 'test_connection_token',
+                },
+            ]);
+            expect(connectionStates).toEqual([
+                {
+                    connected: false,
+                    info: null,
+                },
+            ]);
+
+            loginResult.next({
+                type: 'login_result',
+                success: true,
+                info: {
+                    userId: null,
+                    sessionId: null,
+                    connectionId: 'test_connection_id',
+                },
+            });
+
+            await waitAsync();
+
+            expect(connectionStates.slice(1)).toEqual([
+                {
+                    connected: true,
+                    info: {
+                        userId: null,
+                        sessionId: null,
+                        connectionId: 'test_connection_id',
+                    },
                 },
             ]);
         });
