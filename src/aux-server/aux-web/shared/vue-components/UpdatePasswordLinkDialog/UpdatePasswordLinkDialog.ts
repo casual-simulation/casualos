e470eb9d10e360e5d2ca90b5555e2fb895cc903c
KallynGowdy <kallyngowdy@gmail.com>
2023-11-17 11:30:51 -0500

    feat: Add a spearate component for "show_update_password_link" UI pages

---
+++ src/aux-server/aux-web/shared/vue-components/UpdatePasswordLinkDialog/UpdatePasswordLinkDialog.ts
@@ -0,0 +1,57 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Subscription } from 'rxjs';
+import { appManager } from '../../AppManager';
+import {
+    AuthHelperInterface,
+    LoginUIUpdatePasswordLink,
+} from '@casual-simulation/aux-vm';
+import { Prop, Watch } from 'vue-property-decorator';
+
+@Component({
+    components: {},
+})
+export default class UpdatePasswordLinkDialog extends Vue {
+    private _sub: Subscription;
+    private _endpoint: AuthHelperInterface;
+
+    @Prop({ required: true })
+    endpoint: string;
+
+    @Prop()
+    status: LoginUIUpdatePasswordLink;
+
+    showUpdatePassword: boolean;
+
+    get updatePasswordUrl() {
+        return this.status.updatePasswordUrl;
+    }
+
+    @Watch('endpoint')
+    onEndpointChanged() {
+        this._endpoint = appManager.authCoordinator.authEndpoints.get(
+            this.endpoint
+        );
+        this._registerSubs();
+    }
+
+    created() {
+        this._sub = new Subscription();
+        this._endpoint = appManager.authCoordinator.authEndpoints.get(
+            this.endpoint
+        );
+        this.showUpdatePassword = true;
+    }
+
+    beforeDestroy() {
+        this._sub.unsubscribe();
+    }
+
+    private _registerSubs() {
+        if (this._sub) {
+            this._sub.unsubscribe();
+        }
+
+        this._sub = new Subscription();
+    }
+}
