cddaaa0eb2a815faf82f1a2eef1d3bf2ac170581
KallynGowdy <kallyngowdy@gmail.com>
2023-11-17 11:14:37 -0500

    feat: Add a separate component for "enter_address" UI pages

---
+++ src/aux-server/aux-web/shared/vue-components/EnterAddressDialog/EnterAddressDialog.vue
@@ -0,0 +1,58 @@
+<template>
+    <md-dialog
+        :md-active.sync="showEnterAddress"
+        @md-closed="cancelLogin(true)"
+        :md-close-on-esc="true"
+        :md-click-outside-to-close="true"
+        :md-fullscreen="true"
+        class="input-dialog"
+    >
+        <md-dialog-title>Login with {{ loginSiteName }}</md-dialog-title>
+        <md-dialog-content class="input-dialog-content">
+            <div class="md-layout md-gutter">
+                <div class="md-layout-item">
+                    <md-field :class="emailFieldClass">
+                        <label for="email">{{ emailFieldHint }}</label>
+                        <md-input
+                            name="email"
+                            id="email"
+                            autocomplete="email"
+                            v-model="address"
+                            :disabled="processing"
+                        />
+                        <field-errors field="address" :errors="formErrors" />
+                    </md-field>
+                </div>
+            </div>
+            <div class="terms-of-service-container">
+                <field-errors field="termsOfService" :errors="formErrors" />
+                <div class="terms-of-service-wrapper">
+                    <md-checkbox v-model="acceptedTerms" id="terms-of-service"> </md-checkbox>
+                    <label for="terms-of-service">
+                        I accept the
+                        <a target="_blank" :href="termsOfServiceUrl">Terms of Service</a>
+                    </label>
+                </div>
+            </div>
+            <p>
+                <a target="_blank" :href="privacyPolicyUrl">Privacy Policy</a>
+            </p>
+            <field-errors :field="null" :errors="formErrors" />
+        </md-dialog-content>
+        <md-dialog-actions>
+            <md-button type="button" class="md-primary" @click="login()" :disabled="processing">
+                <md-progress-spinner
+                    v-if="processing"
+                    md-mode="indeterminate"
+                    :md-diameter="20"
+                    :md-stroke="2"
+                    >Processing</md-progress-spinner
+                >
+                <span v-else>Login</span>
+            </md-button>
+        </md-dialog-actions>
+    </md-dialog>
+</template>
+<script src="./EnterAddressDialog.ts"></script>
+<style src="./EnterAddressDialog.css" scoped></style>
+<style src="./EnterAddressDialogGlobals.css"></style>
