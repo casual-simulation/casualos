d70eddb3b47cba90bf523179dfa93b99621eebde
KallynGowdy <kallyngowdy@gmail.com>
2024-03-08 12:28:06 -0500

    test: That relative imports are able to be used on grandparent systems

--- src/aux-runtime/runtime/AuxRuntime.spec.ts
+++ src/aux-runtime/runtime/AuxRuntime.spec.ts
@@ -8505,6 +8505,33 @@ describe('AuxRuntime', () => {
                 });
             });
 
+            it('should be able to resolve modules on grandparent systems by relative import', async () => {
+                runtime.stateUpdated(
+                    stateUpdatedEvent({
+                        test2: createBot('test2', {
+                            system: 'module',
+                            library: `📄export const abc = 'def';`,
+                        }),
+                        test1: createBot('test1', {
+                            system: 'module.component.child',
+                            test: 123,
+                        }),
+                    })
+                );
+                await waitAsync();
+
+                const m = await runtime.resolveModule('::library', {
+                    botId: 'test1',
+                    tag: 'test',
+                });
+
+                expect(m).toMatchObject({
+                    id: 'module.library',
+                    botId: 'test2',
+                    tag: 'library',
+                });
+            });
+
             it('should be able to resolve modules on adjacent systems by relative import', async () => {
                 runtime.stateUpdated(
                     stateUpdatedEvent({
