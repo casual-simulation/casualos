c865fbb5090fbebdba8ed82b4ce71d3130f91af4
KallynGowdy <kallyngowdy@gmail.com>
2024-03-19 14:10:53 -0400

    fix: An issue where custom apps would not be shown after the login process
    - Logging in with a code would prompt to add a passkey and then would never mark the endpoint as done, so the custom apps would never be shown.

--- src/aux-server/aux-web/shared/vue-components/LoginEndpointUI/LoginEndpointUI.vue
+++ src/aux-server/aux-web/shared/vue-components/LoginEndpointUI/LoginEndpointUI.vue
@@ -4,23 +4,27 @@
             v-if="page === 'enter_address'"
             :endpoint="endpoint"
             :status="loginUIStatus"
+            @close="closeDialog()"
         ></enter-address-dialog>
         <check-address-dialog
             v-else-if="page === 'check_address'"
             :endpoint="endpoint"
             :status="loginUIStatus"
+            @close="closeDialog()"
         >
         </check-address-dialog>
         <has-account-dialog
             v-else-if="page === 'has_account'"
             :endpoint="endpoint"
             :status="loginUIStatus"
+            @close="closeDialog()"
         >
         </has-account-dialog>
         <enter-account-info-dialog
             v-else-if="page === 'enter_privo_account_info'"
             :endpoint="endpoint"
             :status="loginUIStatus"
+            @close="closeDialog()"
         >
         </enter-account-info-dialog>
         <update-password-link-dialog
