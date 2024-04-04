d6b03794580cb0330d98d13ed773b9e0425c9ae8
KallynGowdy <kallyngowdy@gmail.com>
2024-04-04 10:28:45 -0400

    fix: Add the Node.js shebang to the beginning of the CLI file

--- src/casualos-cli/script/cli-configs.mjs
+++ src/casualos-cli/script/cli-configs.mjs
@@ -1,10 +1,12 @@
 import path from 'path';
+import fs from 'fs';
 import { paths, cleanDirectory } from '../../../script/build-helpers.mjs';
 import { GIT_HASH, GIT_TAG } from '../../../script/git-stats.mjs';
 
 const src = path.resolve(paths.root, 'src');
 const casualosCli = path.resolve(src, 'casualos-cli');
 const dist = path.resolve(casualosCli, 'dist');
+const outputFile = path.resolve(dist, 'cli.js');
 
 export function cleanDirectories() {
     cleanDirectory(dist);
@@ -36,7 +38,37 @@ export function createConfigs(dev, version) {
                 },
                 minify: false,
                 external: ['open'],
+                plugins: [addNodeShebang()],
             },
         ],
     ];
+}
+
+function addNodeShebang() {
+    return {
+        name: 'addNodeShebang',
+        setup: (build) => {
+            build.onEnd((result) => {
+                const addShebang = () => {
+                    const fileData =
+                        '#!/usr/bin/env node\n' +
+                        fs.readFileSync(outputFile, { encoding: 'utf-8' });
+                    fs.writeFileSync(outputFile, fileData, {
+                        encoding: 'utf-8',
+                        flag: 'w+',
+                    });
+                };
+
+                if (fs.existsSync(outputFile)) {
+                    console.log('Output file exists, adding shebang');
+                    addShebang();
+                } else {
+                    console.log(
+                        'Output file does not exist, waiting 5 seconds...'
+                    );
+                    setTimeout(addShebang, 5000);
+                }
+            });
+        },
+    };
 }
