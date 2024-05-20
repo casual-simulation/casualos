af5b444eb8c96b6857ef4b8cfbdf5009190cef89
KallynGowdy <kallyngowdy@gmail.com>
2024-05-20 15:50:42 -0400

    feat: Add the ability to stream AI chat requests in AIController

--- src/aux-records/OpenAIChatInterface.ts
+++ src/aux-records/OpenAIChatInterface.ts
@@ -3,9 +3,13 @@ import {
     AIChatInterface,
     AIChatInterfaceRequest,
     AIChatInterfaceResponse,
+    AIChatInterfaceStreamResponse,
     AIChatMessage,
+    AIChatMessageRole,
+    AIFunctionCall,
 } from './AIChatInterface';
 import axios from 'axios';
+import OpenAI from 'openai';
 
 export interface OpenAIChatOptions {
     /**
@@ -19,9 +23,13 @@ export interface OpenAIChatOptions {
  */
 export class OpenAIChatInterface implements AIChatInterface {
     private _options: OpenAIChatOptions;
+    private _client: OpenAI;
 
     constructor(options: OpenAIChatOptions) {
         this._options = options;
+        this._client = new OpenAI({
+            apiKey: options.apiKey,
+        });
     }
 
     async chat(
@@ -113,6 +121,61 @@ export class OpenAIChatInterface implements AIChatInterface {
                 }
             }
             handleAxiosErrors(err);
+        }
+    }
+
+    async *chatStream(
+        request: AIChatInterfaceRequest
+    ): AsyncIterable<AIChatInterfaceStreamResponse> {
+        const res = await this._client.chat.completions.create({
+            model: request.model,
+            messages: request.messages.map((m) => ({
+                role: m.role,
+                content:
+                    typeof m.content === 'string'
+                        ? m.content
+                        : m.content.map((c) =>
+                              'text' in c
+                                  ? ({
+                                        type: 'text',
+                                        text: c.text,
+                                    } as const)
+                                  : 'url' in c
+                                  ? ({
+                                        type: 'image_url',
+                                        image_url: {
+                                            url: c.url,
+                                        },
+                                    } as const)
+                                  : ({
+                                        type: 'image_url',
+                                        image_url: {
+                                            url: `data:${
+                                                c.mimeType || 'image/png'
+                                            };base64,${c.base64}`,
+                                        },
+                                    } as const)
+                          ),
+                name: m.author,
+                function: m.functionCall,
+            })) as any[],
+            stream: true,
+            stream_options: {
+                include_usage: true,
+            },
+        });
+
+        for await (const chunk of res) {
+            yield {
+                choices: chunk.choices.map((c) => ({
+                    content: c.delta.content,
+                    role: c.delta.role as AIChatMessageRole,
+                    finishReason: c.finish_reason,
+                    functionCall: c.delta
+                        .function_call as unknown as AIFunctionCall,
+                })),
+                totalTokens: chunk.usage.total_tokens,
+            };
         }
     }
 }
