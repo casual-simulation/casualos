eba0736d2466554fb7258f21f80d6c2ba7ffc154
KallynGowdy <kallyngowdy@gmail.com>
2024-06-27 12:32:23 -0400

    feat: Add some extra error handling for Axios errors

--- src/aux-records/SloydInterface.ts
+++ src/aux-records/SloydInterface.ts
@@ -7,6 +7,7 @@ import {
     AISloydInterfaceEditModelResponse,
 } from './AISloydInterface';
 import axios from 'axios';
+import { handleAxiosErrors } from 'Utils';
 
 export interface SloydOptions {
     clientId: string;
@@ -25,89 +26,101 @@ export class SloydInterface implements AISloydInterface {
     async createModel(
         request: AISloydInterfaceCreateModelRequest
     ): Promise<AISloydInterfaceCreateModelResponse> {
-        const result = await axios.post('https://api.sloyd.ai/create', {
-            ClientId: this._clientId,
-            ClientSecret: this._clientSecret,
-            Prompt: request.prompt,
-            ModelOutputType:
-                request.modelOutputType === 'binary-glb'
-                    ? 1
-                    : request.modelOutputType === 'json-gltf'
-                    ? 4
-                    : 4,
-            LOD: request.levelOfDetail,
-            ThumbnailPreviewExportType: request.thumbnailPreviewExportType,
-            ThumbnailPreviewSizeX: request.thumbnailPreviewSizeX,
-            ThumbnailPreviewSizeY: request.thumbnailPreviewSizeY,
-        });
+        try {
+            const result = await axios.post('https://api.sloyd.ai/create', {
+                ClientId: this._clientId,
+                ClientSecret: this._clientSecret,
+                Prompt: request.prompt,
+                ModelOutputType:
+                    request.modelOutputType === 'binary-glb'
+                        ? 1
+                        : request.modelOutputType === 'json-gltf'
+                        ? 4
+                        : 4,
+                LOD: request.levelOfDetail,
+                ThumbnailPreviewExportType: request.thumbnailPreviewExportType,
+                ThumbnailPreviewSizeX: request.thumbnailPreviewSizeX,
+                ThumbnailPreviewSizeY: request.thumbnailPreviewSizeY,
+            });
 
-        const schema = z.object({
-            InteractionId: z.string(),
-            Name: z.string(),
-            ConfidenceScore: z.number(),
-            ModelOutputType: z.number(),
-            Binary: z.array(z.number()).optional().nullable(),
-            GLTFJson: z.object({}).passthrough().optional().nullable(),
-        });
+            const schema = z.object({
+                InteractionId: z.string(),
+                Name: z.string(),
+                ConfidenceScore: z.number(),
+                ModelOutputType: z.number(),
+                Binary: z.array(z.number()).optional().nullable(),
+                GLTFJson: z.object({}).passthrough().optional().nullable(),
+            });
 
-        const data = schema.parse(result.data);
+            const data = schema.parse(result.data);
 
-        return {
-            success: true,
-            interactionId: data.InteractionId,
-            name: data.Name,
-            confidenceScore: data.ConfidenceScore,
-            modelOutputType:
-                data.ModelOutputType === 1
-                    ? 'binary-glb'
-                    : data.ModelOutputType === 4
-                    ? 'json-gltf'
-                    : 'json-gltf',
-            binary: data.Binary,
-            gltfJson: data.GLTFJson ? JSON.stringify(data.GLTFJson) : undefined,
-        };
+            return {
+                success: true,
+                interactionId: data.InteractionId,
+                name: data.Name,
+                confidenceScore: data.ConfidenceScore,
+                modelOutputType:
+                    data.ModelOutputType === 1
+                        ? 'binary-glb'
+                        : data.ModelOutputType === 4
+                        ? 'json-gltf'
+                        : 'json-gltf',
+                binary: data.Binary,
+                gltfJson: data.GLTFJson
+                    ? JSON.stringify(data.GLTFJson)
+                    : undefined,
+            };
+        } catch (err) {
+            handleAxiosErrors(err);
+        }
     }
 
     async editModel(
         request: AISloydInterfaceEditModelRequest
     ): Promise<AISloydInterfaceEditModelResponse> {
-        const result = await axios.post('https://api.sloyd.ai/edit', {
-            ClientId: this._clientId,
-            ClientSecret: this._clientSecret,
-            InteractionId: request.interactionId,
-            Prompt: request.prompt,
-            ModelOutputType:
-                request.modelOutputType === 'binary-glb'
-                    ? 1
-                    : request.modelOutputType === 'json-gltf'
-                    ? 4
-                    : 4,
-            LOD: request.levelOfDetail,
-            ThumbnailPreviewExportType: request.thumbnailPreviewExportType,
-            ThumbnailPreviewSizeX: request.thumbnailPreviewSizeX,
-            ThumbnailPreviewSizeY: request.thumbnailPreviewSizeY,
-        });
+        try {
+            const result = await axios.post('https://api.sloyd.ai/edit', {
+                ClientId: this._clientId,
+                ClientSecret: this._clientSecret,
+                InteractionId: request.interactionId,
+                Prompt: request.prompt,
+                ModelOutputType:
+                    request.modelOutputType === 'binary-glb'
+                        ? 1
+                        : request.modelOutputType === 'json-gltf'
+                        ? 4
+                        : 4,
+                LOD: request.levelOfDetail,
+                ThumbnailPreviewExportType: request.thumbnailPreviewExportType,
+                ThumbnailPreviewSizeX: request.thumbnailPreviewSizeX,
+                ThumbnailPreviewSizeY: request.thumbnailPreviewSizeY,
+            });
 
-        const schema = z.object({
-            InteractionId: z.string(),
-            ModelOutputType: z.number(),
-            Binary: z.array(z.number()).optional().nullable(),
-            GLTFJson: z.object({}).passthrough().optional().nullable(),
-        });
+            const schema = z.object({
+                InteractionId: z.string(),
+                ModelOutputType: z.number(),
+                Binary: z.array(z.number()).optional().nullable(),
+                GLTFJson: z.object({}).passthrough().optional().nullable(),
+            });
 
-        const data = schema.parse(result.data);
+            const data = schema.parse(result.data);
 
-        return {
-            success: true,
-            interactionId: data.InteractionId,
-            modelOutputType:
-                data.ModelOutputType === 1
-                    ? 'binary-glb'
-                    : data.ModelOutputType === 4
-                    ? 'json-gltf'
-                    : 'json-gltf',
-            binary: data.Binary,
-            gltfJson: data.GLTFJson ? JSON.stringify(data.GLTFJson) : undefined,
-        };
+            return {
+                success: true,
+                interactionId: data.InteractionId,
+                modelOutputType:
+                    data.ModelOutputType === 1
+                        ? 'binary-glb'
+                        : data.ModelOutputType === 4
+                        ? 'json-gltf'
+                        : 'json-gltf',
+                binary: data.Binary,
+                gltfJson: data.GLTFJson
+                    ? JSON.stringify(data.GLTFJson)
+                    : undefined,
+            };
+        } catch (err) {
+            handleAxiosErrors(err);
+        }
     }
 }
