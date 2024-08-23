618a04328ae76bc0d8f468809545418b34b3763b
KallynGowdy <kallyngowdy@gmail.com>
2024-08-23 17:34:36 -0400

    wip: Work on supporting storing every webhook run

--- src/aux-records/webhooks/WebhookRecordsStore.ts
+++ src/aux-records/webhooks/WebhookRecordsStore.ts
@@ -1,3 +1,4 @@
+import { GenericHttpRequest } from '@casual-simulation/aux-common';
 import { SubscriptionFilter } from '../MetricsStore';
 import {
     CrudRecord,
@@ -16,6 +17,12 @@ export interface WebhookRecordsStore extends CrudRecordsStore<WebhookRecord> {
     getSubscriptionMetrics(
         filter: SubscriptionFilter
     ): Promise<WebhookSubscriptionMetrics>;
+
+    /**
+     * Records the given webhook run in the store.
+     * @param run The run to record.
+     */
+    createWebhookRun(run: WebhookRunInfo): Promise<void>;
 }
 
 export interface WebhookRecord extends CrudRecord {
@@ -37,6 +44,16 @@ export interface WebhookRecord extends CrudRecord {
      */
     targetAddress: string;
 
+    // TODO:
+    /**
+     * The calling convention of the webhook.
+     * Different calling conventions support different capabilities.
+     *
+     * - `http`: The webhook is called with a HTTP request. This grants the most flexibility for working with HTTP, and doesn't enforce a strict structure for the request and response.
+     * - `rpc`: The webhook is called with a RPC request. This enforces a strict structure for the request and response.
+     */
+    // callingConvention?: 'http' | 'rpc';
+
     /**
      * The ID of the user that represents the webhook.
      * This is used to authenticate the webhook for access to resources.
@@ -44,6 +61,51 @@ export interface WebhookRecord extends CrudRecord {
      * If null, then the webhook does not use any authentication.
      */
     userId?: string | null;
+}
+
+/**
+ * Defines information about a time that a webhook was run.
+ */
+export interface WebhookRunInfo {
+    /**
+     * The ID of the webhook run.
+     */
+    runId: string;
+
+    /**
+     * The name of the record that the webhook is in.
+     */
+    recordName: string;
+
+    /**
+     * The address of the webhook.
+     */
+    webhookAddress: string;
+
+    /**
+     * The unix time in miliseconds when the webhook request was recieved.
+     */
+    requestTimeMs: number;
+
+    /**
+     * The unix time in miliseconds when the webhook response was sent.
+     */
+    responseTimeMs: number;
+
+    /**
+     * The URL to the file that contains the request data.
+     */
+    requestFileUrl: string;
+
+    /**
+     * The URL to the file that contains the response data.
+     */
+    responseFileUrl: string | null;
+
+    /**
+     * The URL to the file that contains the logs for the webhook run.
+     */
+    logsFileUrl: string | null;
 }
 
 export interface WebhookSubscriptionMetrics extends CrudSubscriptionMetrics {
