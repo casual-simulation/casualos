b0b20de3d40adbf0aff502512608d9e44941356a
Landon Siler <landon.siler@yeticgi.com>
2024-08-30 18:44:11 -0400

    feat: Add utility functions for error handling and type manipulation

--- src/xp-api/util/generic/Generic.ts
+++ src/xp-api/util/generic/Generic.ts
@@ -1,5 +1,12 @@
 /**
- *  Throws an error if the response object does not contain success property set to true
+ * @module xp-api/util/generic/Generic
+ * @description Generic utility functions that can be used across the project (not specific to any module)
+ */
+
+// import { DescriptorMapToType } from './TypeUtils';
+
+/**
+ * Throws an error if the response object does not contain success property set to true
  * @param definition A string that describes the response object
  * @param response The response object to check
  */
@@ -14,3 +21,33 @@ export const throwIfNotSuccess = (definition: string, response: unknown) => {
         );
     }
 };
+
+/**
+ * Throws an error if the value is undefined, otherwise returns the value
+ * @param definition A string that describes the value
+ * @param value The value to check
+ */
+export const asDefined = <T = unknown>(
+    definition: string,
+    value: T
+): T extends undefined ? never : T => {
+    if (value === undefined) {
+        throw new Error(
+            `${definition}.\nValue Reference [expected defined]?: ${value}`
+        );
+    }
+    return value as T extends undefined ? never : T;
+};
+
+// /**
+//  * Creates a class-like object with the given prototype and object definition
+//  * Provides an abstraction which adds a layer of type safety to Object.create
+//  * @param proto The prototype object
+//  * @param objectDef The object definition
+//  */
+// export const classLike = <P extends Object, O extends PropertyDescriptorMap>(
+//     proto: P,
+//     objectDef: O
+// ) => {
+//     return Object.create(proto, objectDef) as P & DescriptorMapToType<O>;
+// };
--- src/xp-api/util/generic/TypeUtils.ts
+++ src/xp-api/util/generic/TypeUtils.ts
@@ -1,5 +1,41 @@
 /**
+ * @module xp-api/util/generic/TypeUtils
+ * @description Utility functions for types and type manipulation that can be used across the project
+ */
+
+/**
  * Extracts the type of a promise.
  * Infers the generic type of a promise and returns it.
  */
 export type ParsePromiseGeneric<T> = T extends Promise<infer U> ? U : never;
+
+/**
+ * Extracts a value type from a property descriptor. (e.g. { value: 'value' } => 'value')
+ */
+export type PropertyDescriptorValue<T> = T extends { value: infer V }
+    ? V
+    : never;
+
+/**
+ * Converts a property descriptor map to a type.
+ */
+export type DescriptorMapToType<T> = {
+    [K in keyof T]: PropertyDescriptorValue<T[K]> extends Object
+        ? DescriptorMapToType<PropertyDescriptorValue<T[K]>>
+        : PropertyDescriptorValue<T[K]>;
+};
+
+/**
+ * Provides a type representing a value as a promise or direct value.
+ */
+export type PromiseOrValue<T> = T | Promise<T>;
+
+/**
+ * Type representing a generic class constructor (class type).
+ */
+export type ClassType = new (...args: any) => any;
+
+/**
+ * Type representing a generic function / arrow function.
+ */
+export type FunctionType = (...args: any) => any;
