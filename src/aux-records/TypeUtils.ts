c9d39ca21740525c80f286a77dad7b2c7c531df7
Landon Siler <landon.siler@yeticgi.com>
2024-08-30 18:01:13 -0400

    feat: Add ISO4217 currency code map and ArrayOfKASP utility type

---
+++ src/aux-records/TypeUtils.ts
@@ -0,0 +1,218 @@
+/**
+ * A map of ISO4217 alphabetic currency codes to their numeric codes
+ * and minor units
+ * * Last updated: 2024-08-30 (YYYY-MM-DD)
+ */
+export type ISO4217_Map = {
+    AED: { n: 784; mU: 2 };
+    AFN: { n: 971; mU: 2 };
+    ALL: { n: 8; mU: 2 };
+    AMD: { n: 51; mU: 2 };
+    ANG: { n: 532; mU: 2 };
+    AOA: { n: 973; mU: 2 };
+    ARS: { n: 32; mU: 2 };
+    AUD: { n: 36; mU: 2 };
+    AWG: { n: 533; mU: 2 };
+    AZN: { n: 944; mU: 2 };
+    BAM: { n: 977; mU: 2 };
+    BBD: { n: 52; mU: 2 };
+    BDT: { n: 50; mU: 2 };
+    BGN: { n: 975; mU: 2 };
+    BHD: { n: 48; mU: 3 };
+    BIF: { n: 108; mU: 0 };
+    BMD: { n: 60; mU: 2 };
+    BND: { n: 96; mU: 2 };
+    BOB: { n: 68; mU: 2 };
+    BOV: { n: 984; mU: 2 };
+    BRL: { n: 986; mU: 2 };
+    BSD: { n: 44; mU: 2 };
+    BTN: { n: 64; mU: 2 };
+    BWP: { n: 72; mU: 2 };
+    BYN: { n: 933; mU: 2 };
+    BZD: { n: 84; mU: 2 };
+    CAD: { n: 124; mU: 2 };
+    CDF: { n: 976; mU: 2 };
+    CHE: { n: 947; mU: 2 };
+    CHF: { n: 756; mU: 2 };
+    CHW: { n: 948; mU: 2 };
+    CLF: { n: 990; mU: 4 };
+    CLP: { n: 152; mU: 0 };
+    CNY: { n: 156; mU: 2 };
+    COP: { n: 170; mU: 2 };
+    COU: { n: 970; mU: 2 };
+    CRC: { n: 188; mU: 2 };
+    CUC: { n: 931; mU: 2 };
+    CUP: { n: 192; mU: 2 };
+    CVE: { n: 132; mU: 2 };
+    CZK: { n: 203; mU: 2 };
+    DJF: { n: 262; mU: 0 };
+    DKK: { n: 208; mU: 2 };
+    DOP: { n: 214; mU: 2 };
+    DZD: { n: 12; mU: 2 };
+    EGP: { n: 818; mU: 2 };
+    ERN: { n: 232; mU: 2 };
+    ETB: { n: 230; mU: 2 };
+    EUR: { n: 978; mU: 2 };
+    FJD: { n: 242; mU: 2 };
+    FKP: { n: 238; mU: 2 };
+    GBP: { n: 826; mU: 2 };
+    GEL: { n: 981; mU: 2 };
+    GHS: { n: 936; mU: 2 };
+    GIP: { n: 292; mU: 2 };
+    GMD: { n: 270; mU: 2 };
+    GNF: { n: 324; mU: 0 };
+    GTQ: { n: 320; mU: 2 };
+    GYD: { n: 328; mU: 2 };
+    HKD: { n: 344; mU: 2 };
+    HNL: { n: 340; mU: 2 };
+    HTG: { n: 332; mU: 2 };
+    HUF: { n: 348; mU: 2 };
+    IDR: { n: 360; mU: 2 };
+    ILS: { n: 376; mU: 2 };
+    INR: { n: 356; mU: 2 };
+    IQD: { n: 368; mU: 3 };
+    IRR: { n: 364; mU: 2 };
+    ISK: { n: 352; mU: 0 };
+    JMD: { n: 388; mU: 2 };
+    JOD: { n: 400; mU: 3 };
+    JPY: { n: 392; mU: 0 };
+    KES: { n: 404; mU: 2 };
+    KGS: { n: 417; mU: 2 };
+    KHR: { n: 116; mU: 2 };
+    KMF: { n: 174; mU: 0 };
+    KPW: { n: 408; mU: 2 };
+    KRW: { n: 410; mU: 0 };
+    KWD: { n: 414; mU: 3 };
+    KYD: { n: 136; mU: 2 };
+    KZT: { n: 398; mU: 2 };
+    LAK: { n: 418; mU: 2 };
+    LBP: { n: 422; mU: 2 };
+    LKR: { n: 144; mU: 2 };
+    LRD: { n: 430; mU: 2 };
+    LSL: { n: 426; mU: 2 };
+    LYD: { n: 434; mU: 3 };
+    MAD: { n: 504; mU: 2 };
+    MDL: { n: 498; mU: 2 };
+    MGA: { n: 969; mU: 2 };
+    MKD: { n: 807; mU: 2 };
+    MMK: { n: 104; mU: 2 };
+    MNT: { n: 496; mU: 2 };
+    MOP: { n: 446; mU: 2 };
+    MRU: { n: 929; mU: 2 };
+    MUR: { n: 480; mU: 2 };
+    MVR: { n: 462; mU: 2 };
+    MWK: { n: 454; mU: 2 };
+    MXN: { n: 484; mU: 2 };
+    MXV: { n: 979; mU: 2 };
+    MYR: { n: 458; mU: 2 };
+    MZN: { n: 943; mU: 2 };
+    NAD: { n: 516; mU: 2 };
+    NGN: { n: 566; mU: 2 };
+    NIO: { n: 558; mU: 2 };
+    NOK: { n: 578; mU: 2 };
+    NPR: { n: 524; mU: 2 };
+    NZD: { n: 554; mU: 2 };
+    OMR: { n: 512; mU: 3 };
+    PAB: { n: 590; mU: 2 };
+    PEN: { n: 604; mU: 2 };
+    PGK: { n: 598; mU: 2 };
+    PHP: { n: 608; mU: 2 };
+    PKR: { n: 586; mU: 2 };
+    PLN: { n: 985; mU: 2 };
+    PYG: { n: 600; mU: 0 };
+    QAR: { n: 634; mU: 2 };
+    RON: { n: 946; mU: 2 };
+    RSD: { n: 941; mU: 2 };
+    RUB: { n: 643; mU: 2 };
+    RWF: { n: 646; mU: 0 };
+    SAR: { n: 682; mU: 2 };
+    SBD: { n: 90; mU: 2 };
+    SCR: { n: 690; mU: 2 };
+    SDG: { n: 938; mU: 2 };
+    SEK: { n: 752; mU: 2 };
+    SGD: { n: 702; mU: 2 };
+    SHP: { n: 654; mU: 2 };
+    SLE: { n: 925; mU: 2 };
+    SOS: { n: 706; mU: 2 };
+    SRD: { n: 968; mU: 2 };
+    SSP: { n: 728; mU: 2 };
+    STN: { n: 930; mU: 2 };
+    SVC: { n: 222; mU: 2 };
+    SYP: { n: 760; mU: 2 };
+    SZL: { n: 748; mU: 2 };
+    THB: { n: 764; mU: 2 };
+    TJS: { n: 972; mU: 2 };
+    TMT: { n: 934; mU: 2 };
+    TND: { n: 788; mU: 3 };
+    TOP: { n: 776; mU: 2 };
+    TRY: { n: 949; mU: 2 };
+    TTD: { n: 780; mU: 2 };
+    TWD: { n: 901; mU: 2 };
+    TZS: { n: 834; mU: 2 };
+    UAH: { n: 980; mU: 2 };
+    UGX: { n: 800; mU: 0 };
+    USD: { n: 840; mU: 2 };
+    USN: { n: 997; mU: 2 };
+    UYI: { n: 940; mU: 0 };
+    UYU: { n: 858; mU: 2 };
+    UYW: { n: 927; mU: 4 };
+    UZS: { n: 860; mU: 2 };
+    VED: { n: 926; mU: 2 };
+    VES: { n: 928; mU: 2 };
+    VND: { n: 704; mU: 0 };
+    VUV: { n: 548; mU: 0 };
+    WST: { n: 882; mU: 2 };
+    XAF: { n: 950; mU: 0 };
+    XAG: { n: 961; mU: null };
+    XAU: { n: 959; mU: null };
+    XBA: { n: 955; mU: null };
+    XBB: { n: 956; mU: null };
+    XBC: { n: 957; mU: null };
+    XBD: { n: 958; mU: null };
+    XCD: { n: 951; mU: 2 };
+    XDR: { n: 960; mU: null };
+    XOF: { n: 952; mU: 0 };
+    XPD: { n: 964; mU: null };
+    XPF: { n: 953; mU: 0 };
+    XPT: { n: 962; mU: null };
+    XSU: { n: 994; mU: null };
+    XTS: { n: 963; mU: null };
+    XUA: { n: 965; mU: null };
+    XXX: { n: 999; mU: null };
+    YER: { n: 886; mU: 2 };
+    ZAR: { n: 710; mU: 2 };
+    ZMW: { n: 967; mU: 2 };
+    ZWG: { n: 924; mU: 2 };
+    ZWL: { n: 932; mU: 2 };
+};
+
+/**
+ * A Generic utility type that parses a type's keys and properties from
+ * sub-objects into an array of arrays each containing each key and
+ * its requested properties
+ * @template U The root type
+ * @template A An array of properties to extract from each key (U[T]) in U
+ * @template T Used to force union effects on all keys, please do not override
+ * ? What is KASP? An acronym for Keys And Specified Properties :)
+ * @example
+ * type X = {
+ *    a: { x: 'String literal'; y: number, z: boolean };
+ *    b: { x: string; y: number, z: boolean };
+ * };
+ * type Y = ArrayOfKASP<X, ['x','y']>;
+ * // [['a','String literal','number'],['b','string','number']]
+ */
+export type ArrayOfKASP<
+    U,
+    A extends Array<keyof U[T]>,
+    T extends keyof U = keyof U
+> = Array<
+    T extends keyof U
+        ? [
+              T,
+              ...{
+                  [K in keyof A]: A[K] extends keyof U[T] ? U[T][A[K]] : never;
+              }
+          ]
+        : never
+>;
