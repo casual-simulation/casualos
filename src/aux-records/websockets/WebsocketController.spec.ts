f8576e8b1b435ec8eb8dc32cac0d27ce3e10c6ee
Kallyn Gowdy <kallyngowdy@gmail.com>
2024-12-16 16:27:46 -0500

    chore: Run all websocket tests

--- src/aux-records/websockets/WebsocketController.spec.ts
+++ src/aux-records/websockets/WebsocketController.spec.ts
@@ -7140,7 +7140,7 @@ describe('WebsocketController', () => {
             });
         });
 
-        describe.only('repo/load_package', () => {
+        describe('repo/load_package', () => {
             async function recordPackage(
                 recordName: string,
                 address: string,
