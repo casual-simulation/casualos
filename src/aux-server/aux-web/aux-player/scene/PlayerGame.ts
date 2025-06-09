b2a01e923b4099174048c1436ef7949da4893c16
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-06-09 14:30:35 -0400

    fix: An issue where the VM iframe wasn't interactive even if the gridPortal was hidden

--- src/aux-server/aux-web/aux-player/scene/PlayerGame.ts
+++ src/aux-server/aux-web/aux-player/scene/PlayerGame.ts
@@ -1969,11 +1969,30 @@ export class PlayerGame extends Game {
 
         if (visible && this.gameView.container.style.display !== 'block') {
             this.gameView.container.style.display = 'block';
+
+            // containerId doesn't match gameView.container (bad naming I know)
+            if (this.gameView.containerId) {
+                const gameContainer = document.getElementById(
+                    this.gameView.containerId
+                );
+                if (gameContainer) {
+                    gameContainer.style.display = 'block';
+                }
+            }
         } else if (
             !visible &&
             this.gameView.container.style.display !== 'none'
         ) {
             this.gameView.container.style.display = 'none';
+            // containerId doesn't match gameView.container (bad naming I know)
+            if (this.gameView.containerId) {
+                const gameContainer = document.getElementById(
+                    this.gameView.containerId
+                );
+                if (gameContainer) {
+                    gameContainer.style.display = 'none';
+                }
+            }
         }
     }
 
