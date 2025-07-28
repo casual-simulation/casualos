a6748cad1fd6a801441d563fed629d13a85e0cfe
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-07-28 16:44:50 -0400

    feat: Add methods for the search interface

--- src/aux-records/search/SearchInterface.ts
+++ src/aux-records/search/SearchInterface.ts
@@ -19,4 +19,201 @@
 /**
  * Defines a basic interface that is able to interact with a search engine.
  */
-export interface SearchInterface {}
+export interface SearchInterface {
+    /**
+     * Creates a new collection in the search engine.
+     * @param collection The collection to create.
+     */
+    createCollection(
+        collection: SearchCollection
+    ): Promise<SearchCollectionInfo>;
+
+    /**
+     * Updates an existing collection in the search engine.
+     * @param collection The updated collection.
+     */
+    updateCollection(
+        collection: UpdatedSearchCollection
+    ): Promise<SearchCollectionInfo>;
+
+    /**
+     * Drops a collection from the search engine.
+     * @param collectionName The name of the collection to drop.
+     */
+    dropCollection(collectionName: string): Promise<SearchCollectionInfo>;
+
+    /**
+     * Retrieves a collection from the search engine.
+     * @param collectionName The name of the collection to retrieve.
+     */
+    getCollection(collectionName: string): Promise<SearchCollectionInfo>;
+
+    /**
+     * Creates a new document in the specified collection.
+     * @param collectionName The name of the collection to create the document in.
+     * @param document The document to create.
+     * @param action The action to perform (create, upsert, update, emplace).
+     */
+    createDocument(
+        collectionName: string,
+        document: SearchDocument,
+        action?: 'create' | 'upsert' | 'update' | 'emplace'
+    ): Promise<SearchDocumentInfo>;
+
+    /**
+     * Deletes a document from the specified collection.
+     * @param collectionName The name of the collection to delete the document from.
+     * @param documentId The ID of the document to delete.
+     */
+    deleteDocument(
+        collectionName: string,
+        documentId: string
+    ): Promise<SearchDocumentInfo>;
+
+    /**
+     * Creates a new API key for use with the search engine.
+     * @param apiKey The API key.
+     */
+    createApiKey(apiKey: SearchApiKeyData): Promise<SearchApiKey>;
+}
+
+export interface SearchCollection {
+    name: string;
+    fields: SearchCollectionField[];
+    defaultSortingField?: string;
+}
+
+export interface UpdatedSearchCollection extends SearchCollection {
+    fields: UpdatedSearchCollectionField[];
+}
+
+export interface SearchCollectionInfo
+    extends Omit<SearchCollection, 'defaultSortingField'> {
+    numDocuments: number;
+}
+
+export interface SearchCollectionField {
+    name: string;
+    /**
+     * The type of the field.
+     */
+    type:
+        | 'string'
+        | 'string[]'
+        | 'int32'
+        | 'int32[]'
+        | 'int64'
+        | 'int64[]'
+        | 'float'
+        | 'float[]'
+        | 'bool'
+        | 'bool[]'
+        | 'geopoint'
+        | 'geopoint[]'
+        | 'geopolygon'
+        | 'object'
+        | 'object[]'
+        | 'string*'
+        | 'image'
+        | 'auto';
+
+    /**
+     * Enables faceting on the field.
+     *
+     * Defaults to `false`.
+     */
+    facet?: boolean;
+
+    /**
+     * When set to `true`, the field can have empty, null or missing values. Default: `false`.
+     */
+    optional?: boolean;
+
+    /**
+     * When set to `false`, the field will not be indexed in any in-memory index (e.g. search/sort/filter/facet). Default: `true`.
+     */
+    index?: boolean;
+
+    /**
+     * When set to `false`, the field value will not be stored on disk. Default: `true`.
+     */
+    store?: boolean;
+
+    /**
+     * When set to true, the field will be sortable. Default: `true` for numbers, `false` otherwise.
+     */
+    sort?: boolean;
+
+    /**
+     * When set to `true`, the field value can be infix-searched. Incurs significant memory overhead. Default: `false`.
+     */
+    infix?: boolean;
+
+    /**
+     * For configuring language specific tokenization, e.g. `jp` for Japanese. Default: `en` which also broadly supports most European languages.
+     */
+    locale?: string;
+
+    /**
+     * Not currently supported.
+     *
+     * Set this to a non-zero value to treat a field of type `float[]` as a vector field.
+     */
+    num_dim?: number;
+
+    /**
+     * Not currently supported.
+     *
+     * The distance metric to be used for vector search. Default: `cosine`. You can also use `ip` for inner product.
+     */
+    vec_dist?: boolean;
+
+    /**
+     * Not currently supported.
+     * Reserved for when/if references are supported.
+     * Name of a field in another collection that should be linked to this collection so that it can be joined during query.
+     */
+    reference?: string;
+
+    /**
+     * Not currently supported.
+     *
+     * Enables an index optimized for range filtering on numerical fields (e.g. rating:>3.5). Default: `false`.
+     */
+    range_index?: boolean;
+
+    /**
+     * Not currently supported.
+     *
+     * Values are stemmed before indexing in-memory. Default: `false`.
+     */
+    stem?: boolean;
+}
+
+export interface UpdatedSearchCollectionField extends SearchCollectionField {
+    drop?: boolean;
+}
+
+export interface SearchDocument {
+    id?: string;
+
+    [key: string]: any;
+}
+
+export interface SearchDocumentInfo extends SearchDocument {
+    id: string;
+}
+
+export interface SearchApiKeyData {
+    actions: string[];
+    collections: string[];
+    description: string;
+
+    expiresAt?: number;
+}
+
+export interface SearchApiKey extends SearchApiKeyData {
+    id: number;
+    value: string;
+    expiresAt: number;
+}
