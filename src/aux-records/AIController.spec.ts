0351f587d227b57d9f15383339140eee7cb76c1e
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-08-21 16:05:30 -0400

    fix: Broken test

--- src/aux-records/AIController.spec.ts
+++ src/aux-records/AIController.spec.ts
@@ -1529,7 +1529,6 @@ describe('AIController', () => {
                                 finishReason: 'stop',
                             },
                         ],
-                        totalTokens: 50,
                     },
                 ],
             });
