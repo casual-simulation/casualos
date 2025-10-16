c977c352be525d0a706471b7009f9a200dbe2903
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-10-16 15:55:08 -0400

    chore: Update database migrations

--- src/aux-server/aux-backend/schemas/migrations/20240903145604_make_some_xp_api_properties_optional/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "XpContract" ALTER COLUMN "description" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "XpInvoice" ALTER COLUMN "note" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "XpUser" ALTER COLUMN "accountId" DROP NOT NULL;
-ALTER TABLE "XpUser" ALTER COLUMN "requestedRate" DROP NOT NULL;
--- src/aux-server/aux-backend/schemas/migrations/20241030042205_make_xp_user_account_relation_non_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `accountId` on table `XpUser` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "XpUser" ALTER COLUMN "accountId" SET NOT NULL;
